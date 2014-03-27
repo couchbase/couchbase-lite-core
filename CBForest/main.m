5ed1293df21cd71a3c1e2da5e6219d9b05a5f6c4
Jens Alfke <jens@couchbase.com>
2014-03-26 21:48:42 -0700

    Initial Commit

---
+++ CBForest/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CBForest
+//
+//  Created by Jens Alfke on 3/26/14.
+//  Copyright (c) 2014 Couchbase. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
