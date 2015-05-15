couchbase/couchbase-lite-core

commit eabd5ee93216e3653f682fbf7c92ca4ba441191f

strlcpy() is BSD-only, replace it with a memcpy() three-liner.

CBForest/Geohash.cc
@@ -248,6 +248,12 @@ hash::hash(forestdb::slice bytes) {
     string[n] = '\0';
 }
 
+hash::hash(const char *str) {
+    size_t n = std::min(strlen(str), sizeof(string) - 1);
+    memcpy(string, str, sizeof(n));
+    string[n] = '\0';
+}
+
 
 bool hash::isValid() const {
     const char *p;

CBForest/Geohash.hh
@@ -115,7 +115,7 @@ namespace geohash {
 
         hash()                              {(string)[0] = '\0';}
         hash(forestdb::slice);
-        hash(const char *str)               {strlcpy(string, str, sizeof(string));}
+        hash(const char *str);
         hash(coord, unsigned nChars);       /**< Geohash of the given coord */
 
         /** Returns the length of GeoHash string needed to get a specific accuracy

commit d242cba7524d738f7669ad854a738006ece1b13c

Fixed geohash regression from eabd5ee
Wrong parameter passed to memcpy...

CBForest/Geohash.cc
@@ -250,7 +250,7 @@ hash::hash(forestdb::slice bytes) {
 
 hash::hash(const char *str) {
     size_t n = std::min(strlen(str), sizeof(string) - 1);
-    memcpy(string, str, sizeof(n));
+    memcpy(string, str, n);
     string[n] = '\0';
 }
 