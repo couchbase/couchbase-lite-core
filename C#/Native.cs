768a82dcd22441d69589ffa6e267b2725a7d8d96
Jim Borden <jim.borden@couchbase.com>
2015-09-15 15:13:45 +0900

    Added C# bindings (So far minimally tested on OS X 64-bit mono only)

---
+++ C#/Native.cs
@@ -0,0 +1,270 @@
+﻿//
+//  Native.cs
+//
+//  Author:
+//  	Jim Borden  <jim.borden@couchbase.com>
+//
+//  Copyright (c) 2015 Couchbase, Inc All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace CBForest
+{
+    public static unsafe class Native
+    {
+        private const string DLL_NAME = "CBForest.dll";
+        
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl, CharSet=CharSet.Ansi)]
+        public static extern void c4slice_free(C4Slice slice);
+        
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl, CharSet=CharSet.Ansi)]
+        private static extern C4Database* c4db_open(C4Slice path, byte readOnly, C4Error *outError);
+        
+        public static C4Database *c4db_open(string path, bool readOnly, C4Error *outError)
+        {
+            using(var path_ = new C4String(path)) {
+                return c4db_open(path_.AsC4Slice(), Convert.ToByte(readOnly), outError);
+            }
+        }
+        
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl, CharSet=CharSet.Ansi, EntryPoint="c4db_close")]
+        private static extern byte _c4db_close(C4Database *db, C4Error *outError);
+        
+        public static bool c4db_close(C4Database *db, C4Error *outError)
+        {
+            return Convert.ToBoolean(_c4db_close(db, outError));   
+        }
+        
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl, CharSet=CharSet.Ansi, EntryPoint="c4db_delete")]
+        private static extern byte _c4db_delete(C4Database *db, C4Error *outError);
+        
+        public static bool c4db_delete(C4Database *db, C4Error *outError)
+        {
+            return Convert.ToBoolean(_c4db_delete(db, outError));
+        }
+        
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl, CharSet=CharSet.Ansi)]
+        public static extern ulong c4db_getDocumentCount(C4Database *db);
+        
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl, CharSet=CharSet.Ansi)]
+        public static extern ulong c4db_getLastSequence(C4Database *db);
+     
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl, CharSet=CharSet.Ansi, EntryPoint="c4db_beginTransaction")]
+        private static extern byte _c4db_beginTransaction(C4Database *db, C4Error *outError);
+        
+        public static bool c4db_beginTransaction(C4Database *db, C4Error *outError)
+        {
+            return Convert.ToBoolean(_c4db_beginTransaction(db, outError));
+        }
+        
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl, CharSet=CharSet.Ansi)]
+        private static extern byte c4db_endTransaction(C4Database *db, byte commit, C4Error *outError);
+        
+        public static bool c4db_endTransaction(C4Database *db, bool commit, C4Error *outError)
+        {
+            return Convert.ToBoolean(c4db_endTransaction(db, Convert.ToByte(commit), outError));
+        }
+        
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl, CharSet=CharSet.Ansi, EntryPoint="c4db_isInTransaction")]
+        private static extern byte _c4db_isInTransaction(C4Database *db);
+        
+        public static bool c4db_isInTransaction(C4Database *db)
+        {
+            return Convert.ToBoolean(_c4db_isInTransaction(db));   
+        }
+        
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl, CharSet=CharSet.Ansi)]
+        public static extern void c4raw_free(C4RawDocument *rawDoc);
+        
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl, CharSet=CharSet.Ansi)]
+        private static extern C4RawDocument* c4raw_get(C4Database *db, C4Slice storeName, C4Slice docID, C4Error *outError);
+        
+        public static C4RawDocument* c4raw_get(C4Database *db, string storeName, string docID, C4Error *outError)
+        {
+            using(var storeName_ = new C4String(storeName))
+            using(var docID_ = new C4String(docID)) {
+                return c4raw_get(db, storeName_.AsC4Slice(), docID_.AsC4Slice(), outError);
+            }
+        }
+        
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl, CharSet=CharSet.Ansi)]
+        private static extern byte c4raw_put(C4Database *db, C4Slice storeName, C4Slice key, C4Slice meta,
+            C4Slice body, C4Error *outError);
+        
+        public static bool c4raw_put(C4Database *db, string storeName, string key, string meta, string body, C4Error *outError)
+        {
+            using(var storeName_ = new C4String(storeName))
+            using(var key_ = new C4String(key))
+            using(var meta_ = new C4String(meta))
+            using(var body_ = new C4String(body)) {
+                return Convert.ToBoolean(c4raw_put(db, storeName_.AsC4Slice(), key_.AsC4Slice(), meta_.AsC4Slice(), 
+                                body_.AsC4Slice(), outError));  
+            }
+        }
+        
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl, CharSet=CharSet.Ansi)]
+        public static extern void c4doc_free(C4Document *doc);
+        
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl, CharSet=CharSet.Ansi, EntryPoint="c4doc_getType")]
+        private static extern C4Slice _c4doc_getType(C4Document *doc);
+        
+        public static string c4doc_getType(C4Document *doc)
+        {
+            var rawRetVal = _c4doc_getType(doc);
+            var retVal = (string)rawRetVal;
+            c4slice_free(rawRetVal);
+            return retVal;
+        }
+        
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl, CharSet=CharSet.Ansi)]
+        private static extern byte c4doc_selectRevision(C4Document *doc, C4Slice revID, byte withBody, C4Error *outError);
+        
+        public static bool c4doc_selectRevision(C4Document *doc, string revID, bool withBody, C4Error *outError)
+        {
+            using(var revID_ = new C4String(revID)) {
+                return Convert.ToBoolean(c4doc_selectRevision(doc, revID_.AsC4Slice(), Convert.ToByte(withBody), outError));
+            }
+        }
+        
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl, CharSet=CharSet.Ansi, EntryPoint="c4doc_selectCurrentRevision")]
+        private static extern byte _c4doc_selectCurrentRevision(C4Document *doc);
+        
+        public static bool c4doc_selectCurrentRevision(C4Document *doc)
+        {
+            return Convert.ToBoolean(_c4doc_selectCurrentRevision(doc)); 
+        }
+        
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl, CharSet=CharSet.Ansi, EntryPoint="c4doc_loadRevisionBody")]
+        private static extern byte _c4doc_loadRevisionBody(C4Document *doc, C4Error *outError);
+        
+        public static bool c4doc_loadRevisionBody(C4Document *doc, C4Error *outError)
+        {
+            return Convert.ToBoolean(_c4doc_loadRevisionBody(doc, outError));
+        }
+        
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl, CharSet=CharSet.Ansi, EntryPoint="c4doc_selectParentRevision")]
+        private static extern byte _c4doc_selectParentRevision(C4Document *doc);
+        
+        public static bool c4doc_selectParentRevision(C4Document *doc)
+        {
+            return Convert.ToBoolean(_c4doc_selectParentRevision(doc));
+        }
+        
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl, CharSet=CharSet.Ansi, EntryPoint="c4doc_selectNextRevision")]
+        private static extern byte _c4doc_selectNextRevision(C4Document *doc);
+        
+        public static bool c4doc_selectNextRevision(C4Document *doc)
+        {
+            return Convert.ToBoolean(_c4doc_selectNextRevision(doc));
+        }
+        
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl, CharSet=CharSet.Ansi)]
+        private static extern byte c4doc_selectNextLeafRevision(C4Document *doc, byte includeDeleted, byte withBody, C4Error *outError);
+        
+        public static bool c4doc_selectNextLeafRevision(C4Document *doc, bool includeDeleted, bool withBody, C4Error *outError)
+        {
+            return Convert.ToBoolean(c4doc_selectNextLeafRevision(doc, Convert.ToByte(includeDeleted), Convert.ToByte(withBody),
+                outError));
+        }
+        
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl, CharSet=CharSet.Ansi)]
+        public static extern void c4enum_free(C4DocEnumerator *e);
+        
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl, CharSet=CharSet.Ansi)]
+        private static extern C4DocEnumerator* c4db_enumerateChanges(C4Database *db, ulong since, byte withBodies, C4Error *outError);
+        
+        public static C4DocEnumerator* c4db_enumerateChanges(C4Database *db, ulong since, bool withBodies, C4Error *outError)
+        {
+            return c4db_enumerateChanges(db, since, Convert.ToByte(withBodies), outError);
+        }
+        
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl, CharSet=CharSet.Ansi)]
+        private static extern C4DocEnumerator* c4db_enumerateAllDocs(C4Database *db, C4Slice startDocID, C4Slice endDocID, byte descending,
+            byte inclusiveEnd, uint skip, byte withBodies, C4Error *outError);
+        
+        public static C4DocEnumerator* c4db_enumerateAllDocs(C4Database *db, string startDocID, string endDocID, bool descending,
+            bool inclusiveEnd, uint skip, bool withBodies, C4Error *outError)
+        {
+            using(var startDocID_ = new C4String(startDocID))
+            using(var endDocID_ = new C4String(endDocID)) {
+                return c4db_enumerateAllDocs(db, startDocID_.AsC4Slice(), endDocID_.AsC4Slice(), Convert.ToByte(descending), 
+                    Convert.ToByte(inclusiveEnd), skip, Convert.ToByte(withBodies), outError);
+            }
+        }
+        
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl, CharSet=CharSet.Ansi)]
+        public static extern C4Document* c4enum_nextDocument(C4DocEnumerator *e, C4Error *outError);
+        
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl, CharSet=CharSet.Ansi)]
+        private static extern byte c4doc_insertRevision(C4Document *doc, C4Slice revID, C4Slice body, byte deleted, byte hasAttachments,
+            byte allowConflict, C4Error *outError);
+        
+        public static bool c4doc_insertRevision(C4Document *doc, string revID, string body, bool deleted, bool hasAttachments,
+            bool allowConflict, C4Error *outError)
+        {
+            using(var revID_ = new C4String(revID))
+            using(var body_ = new C4String(body)) {
+                return Convert.ToBoolean(c4doc_insertRevision(doc, revID_.AsC4Slice(), body_.AsC4Slice(), 
+                    Convert.ToByte(deleted), Convert.ToByte(hasAttachments), Convert.ToByte(allowConflict), outError));
+            }
+        }
+        
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl, CharSet=CharSet.Ansi)]
+        private static extern int c4doc_insertRevisionWithHistory(C4Document *doc, C4Slice revID, C4Slice body, byte deleted, 
+            byte hasAttachments, C4Slice[] history, uint historyCount, C4Error *outError);
+        
+        public static int c4doc_insertRevisionWithHistory(C4Document *doc, string revID, string body, bool deleted, 
+            bool hasAttachments, string[] history, uint historyCount, C4Error *outError)
+        {
+            var flattenedStringArray = new C4String[history.Length + 2];
+            flattenedStringArray[0] = new C4String(revID);
+            flattenedStringArray[1] = new C4String(body);
+            for(int i = 0; i < historyCount; i++) {
+                flattenedStringArray[i + 2] = new C4String(history[i]);
+            }
+            
+            var retVal = c4doc_insertRevisionWithHistory(doc, flattenedStringArray[0].AsC4Slice(), flattenedStringArray[1].AsC4Slice(), 
+                Convert.ToByte(deleted), Convert.ToByte(hasAttachments), 
+                flattenedStringArray.Skip(2).Select<C4String, C4Slice>(x => x.AsC4Slice()).ToArray(), historyCount, outError);
+            
+            foreach(var s in flattenedStringArray) {
+                s.Dispose();   
+            }
+            
+            return retVal;
+        }
+        
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl, CharSet=CharSet.Ansi)]
+        private static extern byte c4doc_setType(C4Document *doc, C4Slice docType, C4Error *outError);
+        
+        public static bool c4doc_setType(C4Document *doc, string docType, C4Error *outError)
+        {
+            using(var docType_ = new C4String(docType)) {
+                return Convert.ToBoolean(c4doc_setType(doc, docType_.AsC4Slice(), outError));   
+            }
+        }
+        
+        [DllImport(DLL_NAME, CallingConvention=CallingConvention.Cdecl, CharSet=CharSet.Ansi, EntryPoint="c4doc_save")]
+        private static extern byte _c4doc_save(C4Document *doc, uint maxRevTreeDepth, C4Error *outError);
+        
+        public static bool c4doc_save(C4Document *doc, uint maxRevTreeDepth, C4Error *outError)
+        {
+            return Convert.ToBoolean(_c4doc_save(doc, maxRevTreeDepth, outError));
+        }
+    }
+}
+
---
+++ C#/Types.cs
@@ -0,0 +1,155 @@
+﻿//
+//  Types.cs
+//
+//  Author:
+//  	Jim Borden  <jim.borden@couchbase.com>
+//
+//  Copyright (c) 2015 Couchbase, Inc All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace CBForest
+{
+    public enum C4ErrorDomain 
+    {
+        HTTP,
+        POSIX,
+        ForestDB,
+        C4
+    }
+    
+    public enum C4ErrorCode
+    {
+        InternalException = 1,
+        NotInTransaction,
+        TransactionNotClosed
+    }
+    
+    public struct C4Error
+    {
+        public C4ErrorDomain domain;
+        public int code;
+    }
+    
+    public struct C4String : IDisposable
+    {
+        private GCHandle _handle;
+        
+        public C4String(string s)
+        {
+            _handle = new GCHandle();
+            if(s != null) {
+                var bytes = Encoding.UTF8.GetBytes(s);
+                _handle = GCHandle.Alloc(bytes, GCHandleType.Pinned);  
+            }
+        }
+        
+        public void Dispose()
+        {
+            if(_handle.IsAllocated) {
+                _handle.Free();   
+            }
+        }
+        
+        public unsafe C4Slice AsC4Slice()
+        {
+            if(_handle.Target == null || !_handle.IsAllocated) {
+                return C4Slice.NULL;
+            }
+
+            var bytes = _handle.Target as byte[];
+            return new C4Slice(_handle.AddrOfPinnedObject().ToPointer(), (uint)bytes.Length);
+        }
+    }
+    
+    public unsafe struct C4Slice
+    {
+        public static readonly C4Slice NULL = new C4Slice();
+        
+        public void* buf;
+        public UIntPtr size;
+        
+        public C4Slice()
+        {
+            buf = null;
+            size = UIntPtr.Zero;
+        }
+        
+        public C4Slice(void *buf, uint size)
+        {
+            this.buf = buf;
+            this.size = new UIntPtr(size);
+        }
+        
+        public static explicit operator string(C4Slice slice)
+        {
+            var bytes = (SByte*)slice.buf; 
+            return new string(bytes, 0, (int)slice.size.ToUInt32(), Encoding.UTF8);
+        }
+    }
+    
+    public struct C4RawDocument
+    {
+        public C4Slice key;
+        public C4Slice meta;
+        public C4Slice body;
+    }
+    
+    [Flags]
+    public enum C4DocumentFlags
+    {
+        Deleted = 0x01,
+        Conflicted = 0x02,
+        HasAttachments = 0x04,
+        Exists = 0x1000
+    }
+    
+    [Flags]
+    public enum C4RevisionFlags
+    {
+        RevDeleted = 0x01,
+        RevLeaf = 0x02,
+        RevNew = 0x04,
+        RevHasAttachments = 0x08
+    }
+    
+    public struct C4Document
+    {
+        public C4DocumentFlags flags;
+        public C4Slice docID;
+        public C4Slice revID;
+        
+        public struct rev
+        {
+            public C4Slice revID;
+            public C4RevisionFlags flags;
+            public ulong sequence;
+            public C4Slice body;
+        }
+        
+        public rev selectedRev;
+    }
+    
+    public struct C4Database
+    {
+    }
+    
+    public struct C4DocEnumerator
+    {
+    }
+}
+
