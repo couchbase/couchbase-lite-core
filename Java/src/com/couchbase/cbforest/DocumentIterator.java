bf23eae23e5dd57698da38b7144c005e20c66390
Jens Alfke <jens@couchbase.com>
2015-12-17 14:21:23 -0800

    Java: Added more efficient DocumentIterator accessors
    Instead of calling DocumentIterator.nextDocument(), call
    DocumentIterator.next(), then getCurrentDocID(), getCurrentRevID(),
    etc.

--- Java/src/com/couchbase/cbforest/DocumentIterator.java
+++ Java/src/com/couchbase/cbforest/DocumentIterator.java
@@ -3,6 +3,8 @@ package com.couchbase.cbforest;
 public class DocumentIterator {
     DocumentIterator(long handle, boolean dummy) {
         _handle = handle;
+        _currentIDs = new String[2];
+        _currentNumbers = new long[2];
     }
 
     DocumentIterator(long dbHandle,
@@ -11,41 +13,67 @@ public class DocumentIterator {
                      int skip,
                      int iteratorFlags)
             throws ForestException {
-        _handle = initEnumerateAllDocs(dbHandle, startDocID, endDocID, skip, iteratorFlags);
+        this(initEnumerateAllDocs(dbHandle, startDocID, endDocID, skip, iteratorFlags), false);
     }
 
     DocumentIterator(long dbHandle, String[] docIDs, int iteratorFlags) throws ForestException {
-        _handle = initEnumerateSomeDocs(dbHandle, docIDs, iteratorFlags);
+        this(initEnumerateSomeDocs(dbHandle, docIDs, iteratorFlags), false);
     }
 
     DocumentIterator(long dbHandle, long sinceSequence, int iteratorFlags) throws ForestException {
-        _handle = initEnumerateChanges(dbHandle, sinceSequence, iteratorFlags);
+        this(initEnumerateChanges(dbHandle, sinceSequence, iteratorFlags), false);
     }
 
-    // Returns null at end
+    // Advances iterator. Then call getCurrentDocID(), etc. to get attributes of document. */
+    public boolean next() throws ForestException {
+        _hasCurrentInfo = false;
+        if (next(_handle))
+            return true;
+        _handle = 0; // native iterator is already freed
+        return false;
+    }
+
+    public String getCurrentDocID()  {getCurrentInfo(); return _currentIDs[0];}
+    public String getCurrentRevID()  {getCurrentInfo(); return _currentIDs[1];}
+    public int getCurrentDocFlags()  {getCurrentInfo(); return (int)_currentNumbers[0];}
+    public long getCurrentSequence() {getCurrentInfo(); return _currentNumbers[1];}
+
+    // Returns current Document
+    public Document getDocument() throws ForestException {
+        return new Document(getDocumentHandle(_handle));
+    }
+
+    // Returns next Document, or null at end
     public Document nextDocument() throws ForestException {
-        long docHandle = nextDocumentHandle(_handle);
-        if (docHandle == 0) {
-            _handle = 0; // native iterator is already freed
-            return null;
-        }
-        return new Document(docHandle);
+        return next() ? getDocument() : null;
     }
 
     public synchronized void free() { if (_handle != 0) {free(_handle); _handle = 0;} }
 
     protected void finalize()       { free(); }
 
-    private native long initEnumerateChanges(long dbHandle, long sinceSequence, int optionFlags) throws ForestException;
-    private native long initEnumerateAllDocs(long dbHandle,
+    private void getCurrentInfo() {
+        if (!_hasCurrentInfo) {
+            getDocumentInfo(_handle, _currentIDs, _currentNumbers);
+            _hasCurrentInfo = true;
+        }
+    }
+
+    private static native long initEnumerateChanges(long dbHandle, long sinceSequence, int optionFlags) throws ForestException;
+    private static native long initEnumerateAllDocs(long dbHandle,
                                              String startDocID,
                                              String endDocID,
                                              int skip,
                                              int optionFlags)
             throws ForestException;
-    private native long initEnumerateSomeDocs(long dbHandle, String[] docIDs, int optionFlags) throws ForestException;
-    private native static long nextDocumentHandle(long handle) throws ForestException;
+    private static native long initEnumerateSomeDocs(long dbHandle, String[] docIDs, int optionFlags) throws ForestException;
+    private native static boolean next(long handle) throws ForestException;
+    private native static long getDocumentHandle(long handle) throws ForestException;
+    private native static void getDocumentInfo(long handle, Object[] outIDs, long[] outNumbers);
     private native static void free(long handle);
 
     private long _handle;
+    private boolean _hasCurrentInfo;
+    private String[] _currentIDs;
+    private long[] _currentNumbers;
 }
