480aa7c3af9a92fe8d3619183c1e2f0c400d0d6a
Jim Borden <jim.borden@couchbase.com>
2016-04-06 17:05:11 +0900

    Update Android build script to include Error.cc

--- CSharp/NativeBuild/jni/Android.mk
+++ CSharp/NativeBuild/jni/Android.mk
@@ -162,6 +162,7 @@ LOCAL_SRC_FILES :=	$(SQLITE3_PATH)/fts3_unicode2.c \
 					$(CBFOREST_PATH)/MapReduceIndex.cc \
 					$(CBFOREST_PATH)/Tokenizer.cc \
 					$(CBFOREST_PATH)/sqlite_glue.c \
+					$(CBFOREST_PATH)/Error.cc \
 					$(LOCAL_PATH)/../C/c4Database.cc \
 					$(LOCAL_PATH)/../C/c4View.cc \
 					$(LOCAL_PATH)/../C/c4Key.cc \
