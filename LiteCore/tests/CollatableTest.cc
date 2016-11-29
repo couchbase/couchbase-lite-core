21c10a498ee5fa4d3d2886024f538752997bf290
Jim Borden <jim.borden@couchbase.com>
2016-11-30 07:34:26 +0900

    This call is ambiguous in some cases (e.g. upgrading from GCC 4.x to GCC 5.x for some reason)

--- LiteCore/tests/CollatableTest.cc
+++ LiteCore/tests/CollatableTest.cc
@@ -43,7 +43,7 @@ static double randf() {
     do {
         n.u1 = (uint32_t)random();
         n.u2 = (uint32_t)random();
-    } while (isnan(n.d) || isinf(n.d));
+    } while (std::isnan(n.d) || std::isinf(n.d));
     return n.d;
 }
 
