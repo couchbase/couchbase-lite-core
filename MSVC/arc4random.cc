cef0ae6d30332ff8cf011cfa79881cc908bdbe51
Jim Borden <jim.borden@couchbase.com>
2017-04-06 13:27:40 +0900

    Re-add arc4random_buf implementation for Windows (CppTests uses it)

--- MSVC/arc4random.cc
+++ MSVC/arc4random.cc
@@ -4,9 +4,36 @@
 std::random_device rd;
 std::default_random_engine e(rd());
 
+
+
 extern "C" {
+	typedef union
+	{
+		int i;
+		unsigned char b[4];
+	} int_sandwich;
+
 	uint32_t arc4random()
 	{
 		return e();
+	}
+
+	void arc4random_buf(void *buffer, int size)
+	{
+		unsigned char* buf = (unsigned char *)buffer;
+		int_sandwich sand;
+		int offset = 0;
+		while (offset < size) {
+			sand.i = e();
+			for (int i = 0; i < 4; i++) {
+				if (offset >= size) {
+					break;
+				}
+
+				*buf = sand.b[i];
+				buf++;
+				offset++;
+			}
+		}
 	}
 }
