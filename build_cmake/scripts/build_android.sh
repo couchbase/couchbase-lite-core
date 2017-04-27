136c715e9322dbcda543d5fc914c0faebe703032
Jim Borden <jim.borden@couchbase.com>
2017-04-27 14:13:54 +0900

    Fix Android builds

--- build_cmake/scripts/build_android.sh
+++ build_cmake/scripts/build_android.sh
@@ -6,13 +6,23 @@ pushd $SCRIPT_DIR/..
 core_count=`getconf _NPROCESSORS_ONLN`
 for arch in x86 armeabi-v7a arm64-v8a; do
   mkdir -p android/lib/$arch
+  cp scripts/strip.sh android/lib/$arch
   pushd android/lib/$arch
   version=16
   if [ "$arch" = "arm64-v8a" ]; then
     version=21
   fi
   cmake -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=$version -DCMAKE_ANDROID_NDK_TOOLCHAIN_VERSION=clang -DCMAKE_ANDROID_ARCH_ABI=$arch -DCMAKE_ANDROID_STL_TYPE=c++_static ../../../..
-  make -j `expr $core_count + 1` LiteCore
+  make -j `expr $core_count + 1`
+  if [ "$arch" = "arm64-v8a" ]; then
+    STRIP=`dirname $(find $ANDROID_NDK_ROOT/toolchains -name strip | grep aarch64)`
+  elif [ "$arch" = "armeabi-v7a" ]; then
+    STRIP=`dirname $(find $ANDROID_NDK_ROOT/toolchains -name strip | grep arm)`
+  else
+    STRIP=`dirname $(find $ANDROID_NDK_ROOT/toolchains -name strip | grep x86-)`
+  fi
+
+  ./strip.sh $STRIP/
   popd
 done
 
--- build_cmake/scripts/strip.sh
+++ build_cmake/scripts/strip.sh
@@ -2,7 +2,9 @@
 
 SCRIPT_DIR=`dirname $0`
 PREFIX=""
+TOP=$SCRIPT_DIR/../..
 if [[ $# > 0 ]]; then
+  TOP=$SCRIPT_DIR/../../../..
   PREFIX="${1}"
 fi
 
@@ -11,9 +13,22 @@ while read line; do
   if [[ "$line" != "" && "${line:0:1}" != "#" ]]; then
     echo "-K ${line:1}" >> stripopts
   fi
-done < $SCRIPT_DIR/../../C/c4.exp
+done < $TOP/C/c4.exp
 
 echo "libLiteCore.so" >> stripopts
+
+COMMAND="${PREFIX}strip @stripopts"
+eval ${COMMAND}
+rm stripopts
+
+echo "-s" > stripopts
+while read line; do
+  if [[ "$line" != "" && "${line:0:1}" != "#" ]]; then
+    echo "-K ${line:1}" >> stripopts
+  fi
+done < $TOP/REST/c4REST.exp
+
+echo "libLiteCoreREST.so" >> stripopts
 
 COMMAND="${PREFIX}strip @stripopts"
 eval ${COMMAND}
