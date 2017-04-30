38258090ca0d22dafb30351ddcb1a363d0c92820
Jens Alfke <jens@couchbase.com>
2017-04-29 17:12:16 -0700

    litecp: Added --careful, --limit, --verbose flags
    Also lowered kMaxTransactionSize from 10000 to 1000 because it's faster.

--- tools/litecp/litecp.cc
+++ tools/litecp/litecp.cc
@@ -10,6 +10,7 @@
 #include "c4Document+Fleece.h"
 #include "FleeceCpp.hh"
 #include "StringUtil.hh"
+#include "Benchmark.hh"
 #include <exception>
 #include <iostream>
 #include <fstream>
@@ -23,7 +24,9 @@ using namespace litecore;
 
 int LiteCpMain(vector<string> &args);
 
-bool gVerbose {false};
+int gVerbose {0};
+
+bool gFailOnError {false};
 
 
 
@@ -37,34 +40,40 @@ static inline string to_string(C4String s) {
     return string((const char*)s.buf, s.size);
 }
 
+
 static inline C4Slice c4str(const string &s) {
     return {s.data(), s.size()};
 }
 
 
-[[noreturn]] static void fail(const char *message) {
-    cerr << "Error: " << message << "\n";
-    exit(1);
-}
-
-[[noreturn]] static void fail(const string &message) {
-    fail(message.c_str());
+static void errorOccurred(const string &what) {
+    cerr << "Error " << what << "\n";
+    if (gFailOnError)
+        exit(1);
 }
 
 
-[[noreturn]] static void fail(const char *what, C4Error err) {
+static void errorOccurred(const string &what, C4Error err) {
     alloc_slice message = c4error_getMessage(err);
     cerr << "Error " << what << ": ";
     if (message.buf)
-        cerr << to_string(message);
+        cerr << to_string(message) << ' ';
     cerr << "(" << err.domain << "/" << err.code << ")\n";
+    if (gFailOnError)
+        exit(1);
+}
+
+
+[[noreturn]] static void fail(const string &message) {
+    errorOccurred(message);
     exit(1);
 }
 
-[[noreturn]] static void fail(const string &what, C4Error err) {
-    fail(what.c_str(), err);
-}
 
+[[noreturn]] static void fail(const string &what, C4Error err) {
+    errorOccurred(what, err);
+    exit(1);
+}
 
 
 [[noreturn]] static void failMisuse(const char *message ="Invalid parameters") {
@@ -77,6 +86,7 @@ static inline C4Slice c4str(const string &s) {
 #pragma mark - CLASSES:
 
 
+/** Abstract base class for a source or target of copying/replication. */
 class Endpoint {
 public:
     Endpoint(const string &spec)
@@ -88,16 +98,30 @@ public:
     virtual void prepare(bool readOnly, bool mustExist, slice docIDProperty) {
         _docIDProperty = docIDProperty;
     }
-    virtual void copyTo(Endpoint*) =0;
+    virtual void copyTo(Endpoint*, uint64_t limit) =0;
 
     virtual void writeJSON(slice docID, slice json) = 0;
 
     virtual void finish() { }
 
+    uint64_t docCount() {
+        return _docCount;
+    }
+
 protected:
+
+    void logDocument(slice docID) {
+        ++_docCount;
+        if (gVerbose >= 2)
+            cout << to_string(docID) << '\n';
+        else if (gVerbose == 1 && (_docCount % 1000) == 0)
+            cout << _docCount << '\n';
+    }
+
     const string _spec;
     Encoder _encoder;
     alloc_slice _docIDProperty;
+    uint64_t _docCount {0};
 };
 
 
@@ -108,7 +132,7 @@ public:
     { }
 
     virtual void prepare(bool readOnly, bool mustExist, slice docIDProperty) override;
-    virtual void copyTo(Endpoint*) override;
+    virtual void copyTo(Endpoint*, uint64_t limit) override;
     virtual void writeJSON(slice docID, slice json) override;
 
 private:
@@ -124,7 +148,7 @@ public:
     { }
 
     virtual void prepare(bool readOnly, bool mustExist, slice docIDProperty) override;
-    virtual void copyTo(Endpoint *dst) override;
+    virtual void copyTo(Endpoint *dst, uint64_t limit) override;
     virtual void writeJSON(slice docID, slice json) override;
     virtual void finish() override;
 
@@ -133,11 +157,13 @@ public:
     void importFrom(JSONEndpoint*);
 
 private:
+    void commit();
+
     c4::ref<C4Database> _db;
     unsigned _transactionSize {0};
     unique_ptr<KeyPath> _docIDPath;
 
-    static constexpr unsigned kMaxTransactionSize = 10000;
+    static constexpr unsigned kMaxTransactionSize = 1000;
 };
 
 
@@ -148,7 +174,7 @@ public:
     :Endpoint(spec)
     { }
     virtual void prepare(bool readOnly, bool mustExist, slice docIDProperty) override;
-    virtual void copyTo(Endpoint*) override;
+    virtual void copyTo(Endpoint*, uint64_t limit) override;
 };
 #endif
 
@@ -168,12 +194,12 @@ Endpoint* Endpoint::create(const string &str) {
 }
 
 
-#pragma mark - DATABASE:
+#pragma mark - DATABASE ENDPOINT:
 
 
 void DbEndpoint::prepare(bool readOnly, bool mustExist, slice docIDProperty) {
     Endpoint::prepare(readOnly, mustExist, docIDProperty);
-    C4DatabaseConfig config = {kC4DB_Bundled | kC4DB_SharedKeys};
+    C4DatabaseConfig config = {kC4DB_Bundled | kC4DB_SharedKeys | kc4DB_NonObservable};
     if (readOnly)
         config.flags |= kC4DB_ReadOnly;
     else if (!mustExist)
@@ -195,8 +221,24 @@ void DbEndpoint::prepare(bool readOnly, bool mustExist, slice docIDProperty) {
 }
 
 
+void DbEndpoint::commit() {
+    if (gVerbose > 1) {
+        cout << "[Committing ... ";
+        cout.flush();
+    }
+    Stopwatch st;
+    C4Error err;
+    if (!c4db_endTransaction(_db, true, &err))
+        fail("committing transaction", err);
+    if (gVerbose > 1) {
+        double time = st.elapsed();
+        cout << time << " sec for " << _transactionSize << " docs]\n";
+    }
+    _transactionSize = 0;
+}
 
-void DbEndpoint::copyTo(Endpoint *dst) {
+
+void DbEndpoint::copyTo(Endpoint *dst, uint64_t limit) {
     // Special case: database to database
     auto dstDB = dynamic_cast<DbEndpoint*>(dst);
     if (dstDB)
@@ -207,37 +249,44 @@ void DbEndpoint::copyTo(Endpoint *dst) {
     c4::ref<C4DocEnumerator> e = c4db_enumerateAllDocs(_db, nullslice, nullslice, &options, &err);
     if (!e)
         fail("enumerating source db", err);
-    while (true) {
+    uint64_t line;
+    for (line = 0; line < limit; ++line) {
         c4::ref<C4Document> doc = c4enum_nextDocument(e, &err);
         if (!doc)
             break;
         alloc_slice json = c4doc_bodyAsJSON(doc, &err);
-        if (!json)
-            fail("reading document body", err);
+        if (!json) {
+            errorOccurred("reading document body", err);
+            continue;
+        }
         dst->writeJSON(doc->docID, json);
     }
     if (err.code)
-        fail("enumerating source db", err);
+        errorOccurred("enumerating source db", err);
+    else if (line == limit)
+        cout << "Stopped after " << limit << " documents.\n";
 }
 
 
 void DbEndpoint::writeJSON(slice docID, slice json) {
     _encoder.reset();
-    if (!_encoder.convertJSON(json))
-        fail(format("Couldn't parse JSON: %.*s", SPLAT(json)));
+    if (!_encoder.convertJSON(json)) {
+        errorOccurred(format("Couldn't parse JSON: %.*s", SPLAT(json)));
+        return;
+    }
     alloc_slice body = _encoder.finish();
 
     // Get the JSON's docIDProperty to use as the document ID:
     if (!docID && _docIDProperty) {
         Dict root = Value::fromTrustedData(body).asDict();
         Value docIDProp = root[*_docIDPath];
-        if (!docIDProp) {
-            fail(format("No property \"%.*s\" in JSON: %.*s", SPLAT(_docIDProperty), SPLAT(json)));
-            // ??? Should I just assign a UUID instead of failing?
+        if (docIDProp) {
+            docID = slice(docIDProp.asString());
+            if (!docID)
+                fail(format("Property \"%.*s\" is not a string in JSON: %.*s", SPLAT(_docIDProperty), SPLAT(json)));
+        } else {
+            errorOccurred(format("No property \"%.*s\" in JSON: %.*s", SPLAT(_docIDProperty), SPLAT(json)));
         }
-        docID = slice(docIDProp.asString());
-        if (!docID)
-            fail(format("Property \"%.*s\" is not a string in JSON: %.*s", SPLAT(_docIDProperty), SPLAT(json)));
     }
 
     C4DocPutRequest put { };
@@ -246,35 +295,30 @@ void DbEndpoint::writeJSON(slice docID, slice json) {
     put.save = true;
     C4Error err;
     c4::ref<C4Document> doc = c4doc_put(_db, &put, nullptr, &err);
-    if (!doc) {
+    if (doc) {
+        docID = slice(doc->docID);
+    } else {
         if (docID)
-            fail(format("saving document \"%.*s\"", SPLAT(put.docID)), err);
+            errorOccurred(format("saving document \"%.*s\"", SPLAT(put.docID)), err);
         else
-            fail("saving document", err);
+            errorOccurred("saving document", err);
     }
 
-    if (gVerbose)
-        cout << to_string(doc->docID) << '\n';
+    logDocument(docID);
 
     if (++_transactionSize >= kMaxTransactionSize) {
-        if (gVerbose)
-            cout << "[Committing...";
-        if (!c4db_endTransaction(_db, true, &err))
-            fail("committing transaction", err);
+        commit();
         if (!c4db_beginTransaction(_db, &err))
             fail("starting transaction", err);
-        _transactionSize = 0;
-        if (gVerbose)
-            cout << "]\n";
     }
 }
 
 
 void DbEndpoint::finish() {
+    commit();
     C4Error err;
-    if (!c4db_endTransaction(_db, true, &err))
-        fail("committing transaction", err);
-    c4db_close(_db, &err);
+    if (!c4db_close(_db, &err))
+        errorOccurred("closing database", err);
 }
 
 
@@ -283,7 +327,7 @@ void DbEndpoint::pushTo(DbEndpoint *) {
 }
 
 
-#pragma mark - JSON:
+#pragma mark - JSON ENDPOINT:
 
 
 void JSONEndpoint::prepare(bool readOnly, bool mustExist, slice docIDProperty) {
@@ -306,15 +350,16 @@ void JSONEndpoint::prepare(bool readOnly, bool mustExist, slice docIDProperty) {
 }
 
 
-void JSONEndpoint::copyTo(Endpoint *dst) {
+void JSONEndpoint::copyTo(Endpoint *dst, uint64_t limit) {
     string line;
-    unsigned lineNo = 1;
-    while (getline(*_in, line)) {
+    unsigned lineNo;
+    for (lineNo = 0; lineNo < limit && getline(*_in, line); ++lineNo) {
         dst->writeJSON(nullslice, c4str(line));
-        ++lineNo;
     }
     if (_in->bad())
-        fail("Couldn't read JSON file");
+        errorOccurred("Couldn't read JSON file");
+    else if (lineNo == limit)
+        cout << "Stopped after " << limit << " documents.\n";
 }
 
 
@@ -327,6 +372,7 @@ void JSONEndpoint::writeJSON(slice docID, slice json) {
     }
     _out->write((char*)json.buf, json.size);
     *_out << '\n';
+    logDocument(docID);
 }
 
 
@@ -342,13 +388,15 @@ static void usage() {
     "    * a JSON file path (.json extension) NOTE: Must contain JSON objects separated by \\n\n"
     "    * a remote database URL (blip: or blips: scheme) [NOT YET IMPLEMENTED]\n"
     "Options:\n"
-    "    --existing or -x : Fail if <dst> doesn't already exist\n"
+    "    --existing or -x : Fail if <dst> doesn't already exist.\n"
     "    --id <property>: When <src> is JSON, this is a property name/path whose value will\n"
     "           be used as the docID. (If omitted, documents are given UUIDs.)\n"
     "           When <dst> is JSON, this is a property name that will be added to the JSON, whose\n"
     "           value is the docID. (If omitted, defaults to \"_id\".)\n"
-    "    --verbose or -v: Log every document being imported/exported\n"
-    "    --help: You're looking at it\n"
+    "    --limit <n>: Stop after <n> documents.\n"
+    "    --careful: Abort on any error."
+    "    --verbose or -v: Log every 1000 docs. If given twice, log every docID.\n"
+    "    --help: You're looking at it.\n"
     ;
 }
 
@@ -358,6 +406,7 @@ int LiteCpMain(vector<string> &args) {
         bool createDst = true;
         string docIDPropertyStr;
         C4String docIDProperty {};
+        uint64_t limit = UINT64_MAX;
 
         if (args.empty()) {
             usage();
@@ -376,8 +425,13 @@ int LiteCpMain(vector<string> &args) {
                 docIDPropertyStr = args[0];
                 docIDProperty = c4str(docIDPropertyStr);
                 args.erase(args.begin());
+            } else if (flag == "limit" || flag == "l") {
+                limit = stoull(args[0]);
+                args.erase(args.begin());
+            } else if (flag == "careful") {
+                gFailOnError = true;
             } else if (flag == "verbose" || flag == "v") {
-                gVerbose = true;
+                ++gVerbose;
             } else if (flag == "help" || flag == "h") {
                 usage();
                 return 0;
@@ -389,6 +443,8 @@ int LiteCpMain(vector<string> &args) {
         if (args.size() != 2)
             fail("Missing source or destination path/URL");
 
+        Stopwatch timer;
+
         Endpoint *src = Endpoint::create(args[0]);
         if (!src)
             failMisuse("Unknown source type");
@@ -397,8 +453,16 @@ int LiteCpMain(vector<string> &args) {
             failMisuse("Unknown destination type");
         src->prepare(true, true, docIDProperty);
         dst->prepare(false,!createDst, docIDProperty);
-        src->copyTo(dst);
+
+        if (gVerbose)
+            cout << "Copying...\n";
+        src->copyTo(dst, limit);
         dst->finish();
+
+        double time = timer.elapsed();
+        cout << "Completed " << dst->docCount() << " docs in " << time << " secs; "
+             << int(dst->docCount() / time) << " docs/sec\n";
+
         return 0;
     } catch (const std::exception &x) {
         fail(format("Uncaught C++ exception: %s", x.what()));
