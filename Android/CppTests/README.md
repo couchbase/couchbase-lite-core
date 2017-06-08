ae9fc28334991be69b75e524896698f6a3689cb4
Hideki Itakura <hideki@couchbase.com>
2017-06-07 18:21:00 -0700

    JNI for Listener (#151)
    - Added JNI for Listener
    - Removed Android directory
    - Moved build scripts for Android under Java directory

--- Android/CppTests/README.md
+++
@@ -1,9 +0,0 @@
-# NOTES
-- Building the native library with `clang` causes following compilation error. For this use `GCC`. See app/build.gradle
-
-
-
-```
-arm-linux-androideabi/bin/ld: error: /Users/hideki/github/couchbase-lite-core/Android/CppTests/app/src/main/cpp/../../../../distribution/litecore/lib/armeabi-v7a/libcrypto.a(sha1-armv4-large.o): requires unsupported dynamic reloc R_ARM_REL32; recompile with -fPIC
-arm-linux-androideabi/bin/ld: warning: shared library text segment is not shareable
-```
---
+++ Java/src/com/couchbase/litecore/C4ListenerConfig.java
@@ -0,0 +1,104 @@
+package com.couchbase.litecore;
+
+/** Configuration for a C4Listener. */
+public class C4ListenerConfig {
+    private int port;   ///< TCP port to listen on
+    private int apis;   ///< Which API(s) to enable
+
+    // For REST listeners only:
+    private String directory;       ///< Directory where newly-PUT databases will be created
+    private boolean allowCreateDBs; ///< If true, "PUT /db" is allowed
+    private boolean allowDeleteDBs; ///< If true, "DELETE /db" is allowed
+
+    // For sync listeners only:
+    private boolean allowPush;
+    private boolean allowPull;
+
+    public C4ListenerConfig() {
+    }
+
+    public C4ListenerConfig(int port,
+                            int apis,
+                            String directory,
+                            boolean allowCreateDBs,
+                            boolean allowDeleteDBs,
+                            boolean allowPush,
+                            boolean allowPull) {
+        this.port = port;
+        this.apis = apis;
+        this.directory = directory;
+        this.allowCreateDBs = allowCreateDBs;
+        this.allowDeleteDBs = allowDeleteDBs;
+        this.allowPush = allowPush;
+        this.allowPull = allowPull;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public int getApis() {
+        return apis;
+    }
+
+    public void setApis(int apis) {
+        this.apis = apis;
+    }
+
+    public String getDirectory() {
+        return directory;
+    }
+
+    public void setDirectory(String directory) {
+        this.directory = directory;
+    }
+
+    public boolean isAllowCreateDBs() {
+        return allowCreateDBs;
+    }
+
+    public void setAllowCreateDBs(boolean allowCreateDBs) {
+        this.allowCreateDBs = allowCreateDBs;
+    }
+
+    public boolean isAllowDeleteDBs() {
+        return allowDeleteDBs;
+    }
+
+    public void setAllowDeleteDBs(boolean allowDeleteDBs) {
+        this.allowDeleteDBs = allowDeleteDBs;
+    }
+
+    public boolean isAllowPush() {
+        return allowPush;
+    }
+
+    public void setAllowPush(boolean allowPush) {
+        this.allowPush = allowPush;
+    }
+
+    public boolean isAllowPull() {
+        return allowPull;
+    }
+
+    public void setAllowPull(boolean allowPull) {
+        this.allowPull = allowPull;
+    }
+
+    @Override
+    public String toString() {
+        return "C4ListenerConfig{" +
+                "port=" + port +
+                ", apis=" + apis +
+                ", directory='" + directory + '\'' +
+                ", allowCreateDBs=" + allowCreateDBs +
+                ", allowDeleteDBs=" + allowDeleteDBs +
+                ", allowPush=" + allowPush +
+                ", allowPull=" + allowPull +
+                '}';
+    }
+}
