bfe1d5df93da8b3a20fbc724604b6a5960a76a37
Jim Borden <jim.borden@couchbase.com>
2017-06-09 13:30:18 +0900

    Build iOS with bitcode for .NET

--- build_cmake/scripts/build_ios_fat.sh
+++ build_cmake/scripts/build_ios_fat.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=`dirname $0`
 pushd $SCRIPT_DIR/..
 mkdir ios-fat
 
-xcodebuild -project ../Xcode/LiteCore.xcodeproj -configuration Release -derivedDataPath ios -scheme "LiteCore dylib" -sdk iphoneos
+xcodebuild -project ../Xcode/LiteCore.xcodeproj -configuration Release -derivedDataPath ios -scheme "LiteCore dylib" -sdk iphoneos BITCODE_GENERATION_MODE=bitcode
 xcodebuild -project ../Xcode/LiteCore.xcodeproj -configuration Release -derivedDataPath ios -scheme "LiteCore dylib" -sdk iphonesimulator
 lipo -create ios/Build/Products/Release-iphoneos/libLiteCore.dylib ios/Build/Products/Release-iphonesimulator/libLiteCore.dylib -output ios-fat/libLiteCore.dylib
 rm -rf ios
