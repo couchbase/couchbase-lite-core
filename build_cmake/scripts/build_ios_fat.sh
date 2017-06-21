71dabdb3ca2c9fff7c626fc3415f571fb09437f0
Jim Borden <jim.borden@couchbase.com>
2017-06-21 11:02:47 +0900

    (.NET) dylibs were still getting code signed in an invalid way which blocked the build later, so force it off and add bitcode to tvOS build

--- build_cmake/scripts/build_ios_fat.sh
+++ build_cmake/scripts/build_ios_fat.sh
@@ -5,8 +5,8 @@ SCRIPT_DIR=`dirname $0`
 pushd $SCRIPT_DIR/..
 mkdir ios-fat
 
-xcodebuild -project ../Xcode/LiteCore.xcodeproj -configuration Release -derivedDataPath ios -scheme "LiteCore dylib" -sdk iphoneos BITCODE_GENERATION_MODE=bitcode "CODE_SIGNING_REQUIRED=NO" "CODE_SIGN_IDENTITY="
-xcodebuild -project ../Xcode/LiteCore.xcodeproj -configuration Release -derivedDataPath ios -scheme "LiteCore dylib" -sdk iphonesimulator "CODE_SIGNING_REQUIRED=NO" "CODE_SIGN_IDENTITY="
+xcodebuild -project ../Xcode/LiteCore.xcodeproj -configuration Release -derivedDataPath ios -scheme "LiteCore dylib" -sdk iphoneos BITCODE_GENERATION_MODE=bitcode CODE_SIGNING_ALLOWED=NO
+xcodebuild -project ../Xcode/LiteCore.xcodeproj -configuration Release -derivedDataPath ios -scheme "LiteCore dylib" -sdk iphonesimulator CODE_SIGNING_ALLOWED=NO
 lipo -create ios/Build/Products/Release-iphoneos/libLiteCore.dylib ios/Build/Products/Release-iphonesimulator/libLiteCore.dylib -output ios-fat/libLiteCore.dylib
 rm -rf ios
 popd
