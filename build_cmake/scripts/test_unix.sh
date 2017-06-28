250c748277235e8f9c9a92206afa582c998934f2
Jim Borden <jim.borden@couchbase.com>
2017-06-29 08:39:04 +0900

    Add a test script to call for build validation on Unix platforms

---
+++ build_cmake/scripts/test_unix.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ ! -f LiteCore/tests/CppTests ]; then
+  echo "CppTests not found!"
+  exit 1
+fi
+
+if [ ! -f C/tests/C4Tests ]; then
+  echo "C4Tests not found!"
+  exit 1
+fi
+
+pushd LiteCore/tests
+./CppTests -r list
+popd
+
+pushd C/tests
+./C4Tests -r list
+popd
