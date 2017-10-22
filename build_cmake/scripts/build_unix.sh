cfdf33926ace88f0c89c0a8501bda91d4e53b459
Matias Piipari <matias.piipari@gmail.com>
2017-10-22 23:32:40 +0100

    build_unix.sh script tweaks
    - Adds -p to mkdir command (will fail on subsequent script calls after the first one if folder `unix` already exists).\n- Adds workaround to inability of clang on Ubuntu 16.04 like Linux to find __cxxabi_config.h even when libc++-dev & libc++abi-dev are installed (same issue as noted here: http://lists.alioth.debian.org/pipermail/pkg-llvm-team/2015-September/005208.html, with the same workaround working).

--- build_cmake/scripts/build_unix.sh
+++ build_cmake/scripts/build_unix.sh
@@ -3,10 +3,10 @@
 SCRIPT_DIR=`dirname $0`
 pushd $SCRIPT_DIR/..
 
-mkdir unix
+mkdir -p unix
 pushd unix
 core_count=`getconf _NPROCESSORS_ONLN`
-CC=clang CXX=clang++ cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DLITECORE_BUILD_SQLITE=1 ../..
+CC=clang CXX=clang++ CXXFLAGS="-I/usr/include/libcxxabi" cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DLITECORE_BUILD_SQLITE=1 ../..
 make -j `expr $core_count + 1`
 ../scripts/strip.sh `pwd`
 popd
