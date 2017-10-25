f5844f78d35e325a076095b83e58c5fea8766499
Matias Piipari <matias.piipari@gmail.com>
2017-10-25 10:35:00 +0100

    Addresses pull request feedback for https://github.com/couchbase/couchbase-lite-core/pull/277

--- build_cmake/scripts/build_unix.sh
+++ build_cmake/scripts/build_unix.sh
@@ -6,7 +6,7 @@ pushd $SCRIPT_DIR/..
 mkdir -p unix
 pushd unix
 core_count=`getconf _NPROCESSORS_ONLN`
-CC=clang CXX=clang++ CXXFLAGS="-I/usr/include/libcxxabi" cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DLITECORE_BUILD_SQLITE=1 ../..
+CC=clang CXX=clang++ cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DLITECORE_BUILD_SQLITE=1 ../..
 make -j `expr $core_count + 1`
 ../scripts/strip.sh `pwd`
 popd
