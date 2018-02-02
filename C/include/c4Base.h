3b85d8614a9c2b8aec8203abefa5da732cbc1fde
Jim Borden <jim.borden@couchbase.com>
2018-02-02 14:12:02 +0900

    Apply new file header style (for most things, Xcode and Java folder untouched)

--- C/include/c4Base.h
+++ C/include/c4Base.h
@@ -1,11 +1,19 @@
 //
-//  c4Base.h
-//  Couchbase Lite Core
+// c4Base.h
 //
-//  Basic types and functions for C API.
+// Copyright (c) 2015 Couchbase, Inc All rights reserved.
 //
-//  Created by Jens Alfke on 9/8/15.
-//  Copyright (c) 2015-2016 Couchbase. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 //
 
 #pragma once
--- LiteCore/Query/SQLiteFleeceEach.cc
+++ LiteCore/Query/SQLiteFleeceEach.cc
@@ -1,9 +1,20 @@
 //
-//  SQLiteFleeceEach.cc
-//  LiteCore
+// SQLiteFleeceEach.cc
 //
-//  Created by Jens Alfke on 10/7/16.
-//  Copyright © 2016 Couchbase. All rights reserved.
+// Copyright (c) 2016 Couchbase, Inc All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 //
 //  (Heavily) adapted from ext/misc/json1.c and ext/misc/series.c in the SQLite source code:
 //  http://www.sqlite.org/src/artifact?ci=trunk&filename=ext/misc/series.c
