2f6e3422c58943641241fb002d32d19136ac2b9d
hideki <hideki@couchbase.com>
2018-02-05 18:08:01 -0800

    Update CmakeLists.txt for JNI build, and modified backtrace for Android.

--- LiteCore/Support/Error_android.cc
+++ LiteCore/Support/Error_android.cc
@@ -17,55 +17,58 @@
 //
 
 #ifdef __ANDROID__
+
 #include <unwind.h>
 #include <dlfcn.h>
 #include <android/log.h>
 #include <cxxabi.h>
 #include "Error.hh"
+#include <sstream>
+
+using namespace std;
 
 namespace litecore {
-    
-    struct BacktraceState
-    {
-        void** current;
-        void** end;
+
+    struct BacktraceState {
+        void **current;
+        void **end;
     };
-    
-    static _Unwind_Reason_Code unwindCallback(struct _Unwind_Context* context, void* arg)
-    {
-        BacktraceState* state = static_cast<BacktraceState*>(arg);
+
+    static _Unwind_Reason_Code unwindCallback(struct _Unwind_Context *context, void *arg) {
+        BacktraceState *state = static_cast<BacktraceState *>(arg);
         uintptr_t pc = _Unwind_GetIP(context);
         if (pc) {
             if (state->current == state->end) {
                 return _URC_END_OF_STACK;
             } else {
-                *state->current++ = reinterpret_cast<void*>(pc);
+                *state->current++ = reinterpret_cast<void *>(pc);
             }
         }
         return _URC_NO_REASON;
     }
-    
-    static size_t captureBacktrace(void** buffer, size_t max)
-    {
+
+    static size_t captureBacktrace(void **buffer, size_t max) {
         BacktraceState state = {buffer, buffer + max};
         _Unwind_Backtrace(unwindCallback, &state);
-        
+
         return state.current - buffer;
     }
 
-    /*static*/ void error::logBacktrace(unsigned skip) {
-        void* buffer[50];
+    /*static*/ string error::backtrace(unsigned skip) {
+        stringstream out;
+
+        void *buffer[50];
         size_t captured = captureBacktrace(buffer, 50);
-        char* unmangled = nullptr;
+        char *unmangled = nullptr;
         size_t unmangledLen = 0;
-        
-        for(size_t i = skip + 1; i < captured; i++) {
-            const void* addr = buffer[i];
-            const char* symbol = "";
-            const char* file = "???";
-            
+
+        for (size_t i = skip + 1; i < captured; i++) {
+            const void *addr = buffer[i];
+            const char *symbol = "";
+            const char *file = "???";
+
             Dl_info info;
-            if(dladdr(addr, &info) && info.dli_sname) {
+            if (dladdr(addr, &info) && info.dli_sname) {
                 int status;
                 symbol = info.dli_sname;
                 file = info.dli_fname;
@@ -74,9 +77,13 @@ namespace litecore {
                     symbol = unmangled;
                 }
             }
-            
-            __android_log_print(ANDROID_LOG_ERROR, "libLiteCore", "%s %s", file, symbol);
+
+            char *cstr = nullptr;
+            asprintf(&cstr, "%s %s", file, symbol);
+            out << cstr;
+            free(cstr);
         }
+        return out.str();
     }
 }
 
