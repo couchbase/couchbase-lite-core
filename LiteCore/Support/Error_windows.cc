b6c19f80048bc1e8040b4a261006d77928749733
Jim Borden <jim.borden@couchbase.com>
2018-02-08 12:39:13 +0900

    Fix Error_windows.cc to account for new backtrace signature

--- LiteCore/Support/Error_windows.cc
+++ LiteCore/Support/Error_windows.cc
@@ -21,35 +21,44 @@
 #include <Windows.h>
 #include <Dbghelp.h>
 #include "Error.hh"
-#include "Logging.hh"
+#include "asprintf.h"
+#include <sstream>
+using namespace std;
 
 namespace litecore {
-    /*static*/ void error::logBacktrace(unsigned skip) {
+    /*static*/ string error::backtrace(unsigned skip) {
         #if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
         void* stack[50];
-        HANDLE process = GetCurrentProcess();
-        SymInitialize(process, NULL, TRUE);
-        DWORD captured = CaptureStackBackTrace(0, 50, stack, NULL);
-        SYMBOL_INFO* symbol = (SYMBOL_INFO*)malloc(sizeof(SYMBOL_INFO)+1023 * sizeof(TCHAR));
+        const auto process = GetCurrentProcess();
+        SymInitialize(process, nullptr, TRUE);
+        const auto captured = CaptureStackBackTrace(0, 50, stack, nullptr);
+        const auto symbol = (SYMBOL_INFO*)malloc(sizeof(SYMBOL_INFO)+1023 * sizeof(TCHAR));
         symbol->MaxNameLen = 1024;
         symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
         DWORD displacement;
-        IMAGEHLP_LINE64 *line = (IMAGEHLP_LINE64*)malloc(sizeof(IMAGEHLP_LINE64));
+        const auto line = (IMAGEHLP_LINE64*)malloc(sizeof(IMAGEHLP_LINE64));
         line->SizeOfStruct = sizeof(IMAGEHLP_LINE64);
         
+		stringstream out;
         for(unsigned i = skip + 1; i < captured - skip; i++) {
-            DWORD64 address = (DWORD64)stack[i];
-            SymFromAddr(process, address, NULL, symbol);
+            const auto address = (DWORD64)stack[i];
+            SymFromAddr(process, address, nullptr, symbol);
+			char* cstr = nullptr;
             if(SymGetLineFromAddr64(process, address, &displacement, line)) {
-                WarnError("\tat %s in %s: line: %lu: address: 0x%0X\n", symbol->Name, line->FileName, line->LineNumber, symbol->Address);
+				asprintf(&cstr, "\tat %s in %s: line: %lu: address: 0x%0llX\r\n", symbol->Name, line->FileName, line->LineNumber, symbol->Address);
             } else {
-                WarnError("\tat %s, address 0x%0X.\n", symbol->Name, symbol->Address);
+                asprintf(&cstr, "\tat %s, address 0x%0llX.\r\n", symbol->Name, symbol->Address);
             }
+
+			out << cstr;
+			free(cstr);
         }
         
         free(symbol);
         free(line);
         SymCleanup(process);
+
+		return out.str();
         #endif
     }
 }
