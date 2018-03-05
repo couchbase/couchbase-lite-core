af3d7889ea4f5f78f77c3ece2d467f96baed6674
Jens Alfke <jens@couchbase.com>
2018-03-05 12:17:41 -0800

    Fixed stuck replicator, in a super-fast continuous push case
    Too many failed revisions sent (rev obsolete by the time the peer requests
    it) could cause the Pusher to stop sending `rev` messages.
    
    For #446

--- Replicator/Pusher.cc
+++ Replicator/Pusher.cc
@@ -383,6 +383,7 @@ namespace litecore { namespace repl {
     void Pusher::_couldntSendRevision(RevRequest rev) {
         decrement(_revisionsInFlight);
         doneWithRev(rev, false);
+        maybeSendMoreRevs();
     }
 
 
