couchbase/couchbase-lite-core

commit c9969f52458da91bb44cf791cedc069ea4e909ae

Don't mark locally-created revs with kRevConflict
In c4doc_put, only add the kRevConflict flag to an inserted revision if
the remoteDB field is nonzero, i.e. if this revision comes from a remote
server.
This won't have any effect on clients, but allows tests to create
conflicting rev trees; see the upcoming "Server Conflict Branch-Switch".

LiteCore/Database/TreeDocument.cc
@@ -316,7 +316,8 @@ namespace c4Internal {
                 revIDBuffers[i].parse(rq.history[i]);
             commonAncestor = _versionedDoc.insertHistory(revIDBuffers,
                                                          rq.body,
-                                                         (Rev::Flags)rq.revFlags);
+                                                         (Rev::Flags)rq.revFlags,
+                                                         (rq.remoteDBID != 0));
             if (commonAncestor < 0)
                 error::_throw(error::BadRevisionID); // must be invalid revision IDs
             auto newRev = _versionedDoc[revidBuffer(rq.history[0])];
@@ -332,6 +333,8 @@ namespace c4Internal {
 
 
         bool putNewRevision(const C4DocPutRequest &rq) override {
+            if (rq.remoteDBID != 0)
+                error::_throw(error::InvalidParameter, "remoteDBID cannot be used when existing=false");
             bool deletion = (rq.revFlags & kRevDeleted) != 0;
             revidBuffer encodedNewRevID = generateDocRevID(rq.body, selectedRev.revID, deletion);
             slice body = rq.body;
@@ -343,6 +346,7 @@ namespace c4Internal {
                                                (Rev::Flags)rq.revFlags,
                                                _selectedRev,
                                                rq.allowConflict,
+                                               false,
                                                httpStatus);
             if (newRev) {
                 return saveNewRev(rq, newRev);

LiteCore/RevTrees/RevTree.cc
@@ -193,7 +193,8 @@ namespace litecore {
     Rev* RevTree::_insert(revid unownedRevID,
                           slice body,
                           Rev *parentRev,
-                          Rev::Flags revFlags)
+                          Rev::Flags revFlags,
+                          bool markConflict)
     {
         revFlags = Rev::Flags(revFlags & (Rev::kDeleted | Rev::kClosed | Rev::kHasAttachments | Rev::kKeepBody));
         Assert(!((revFlags & Rev::kClosed) && !(revFlags & Rev::kDeleted)));
@@ -217,14 +218,14 @@ namespace litecore {
         newRev->parent = parentRev;
 
         if (parentRev) {
-            if (!parentRev->isLeaf() || parentRev->isConflict())
+            if (markConflict && (!parentRev->isLeaf() || parentRev->isConflict()))
                 newRev->addFlag(Rev::kIsConflict);      // Creating or extending a branch
             parentRev->clearFlag(Rev::kLeaf);
             if (revFlags & Rev::kKeepBody)
                 keepBody(newRev);
         } else {
             // Root revision:
-            if (!_revs.empty())
+            if (markConflict && !_revs.empty())
                 newRev->addFlag(Rev::kIsConflict);      // Creating a 2nd root
         }
 
@@ -236,7 +237,7 @@ namespace litecore {
     }
 
     const Rev* RevTree::insert(revid revID, slice data, Rev::Flags revFlags,
-                               const Rev* parent, bool allowConflict,
+                               const Rev* parent, bool allowConflict, bool markConflict,
                                int &httpStatus)
     {
         // Make sure the given revID is valid:
@@ -275,11 +276,11 @@ namespace litecore {
         
         // Finally, insert:
         httpStatus = (revFlags & Rev::kDeleted) ? 200 : 201;
-        return _insert(revID, data, (Rev*)parent, revFlags);
+        return _insert(revID, data, (Rev*)parent, revFlags, markConflict);
     }
 
     const Rev* RevTree::insert(revid revID, slice body, Rev::Flags revFlags,
-                               revid parentRevID, bool allowConflict,
+                               revid parentRevID, bool allowConflict, bool markConflict,
                                int &httpStatus)
     {
         const Rev* parent = nullptr;
@@ -290,11 +291,13 @@ namespace litecore {
                 return nullptr; // parent doesn't exist
             }
         }
-        return insert(revID, body, revFlags, parent, allowConflict, httpStatus);
+        return insert(revID, body, revFlags, parent, allowConflict, markConflict, httpStatus);
     }
 
-    int RevTree::insertHistory(const std::vector<revidBuffer> history, slice data,
-                               Rev::Flags revFlags) {
+    int RevTree::insertHistory(const std::vector<revidBuffer> history,
+                               slice data,
+                               Rev::Flags revFlags,
+                               bool markConflict) {
         Assert(history.size() > 0);
         // Find the common ancestor, if any. Along the way, preflight revision IDs:
         int i;
@@ -316,8 +319,8 @@ namespace litecore {
         if (i > 0) {
             // Insert all the new revisions in chronological order:
             while (--i > 0)
-                parent = _insert(history[i], slice(), parent, Rev::kNoFlags);
-            _insert(history[0], data, parent, revFlags);
+                parent = _insert(history[i], slice(), parent, Rev::kNoFlags, markConflict);
+            _insert(history[0], data, parent, revFlags, markConflict);
         }
         return commonAncestorIndex;
     }

LiteCore/RevTrees/RevTree.hh
@@ -114,6 +114,7 @@ namespace litecore {
                           Rev::Flags,
                           revid parentRevID,
                           bool allowConflict,
+                          bool markConflict,
                           int &httpStatus);
 
         // Adds a new leaf revision, given a pointer to the parent Rev
@@ -122,13 +123,15 @@ namespace litecore {
                           Rev::Flags,
                           const Rev* parent,
                           bool allowConflict,
+                          bool markConflict,
                           int &httpStatus);
 
         // Adds a new leaf revision along with any new ancestor revs in its history.
         // (history[0] is the new rev's ID, history[1] is its parent's, etc.)
         int insertHistory(const std::vector<revidBuffer> history,
                           slice body,
-                          Rev::Flags);
+                          Rev::Flags,
+                          bool markConflict);
 
         // Sets/clears the kIsConflict flag for a Rev and its ancestors.
         void markBranchAsConflict(const Rev*, bool);
@@ -175,7 +178,7 @@ namespace litecore {
         friend class Rev;
         friend class RawRevision;
         void initRevs();
-        Rev* _insert(revid, slice body, Rev *parentRev, Rev::Flags);
+        Rev* _insert(revid, slice body, Rev *parentRev, Rev::Flags, bool markConflicts);
         bool confirmLeaf(Rev* testRev NONNULL);
         void compact();
         void checkForResolvedConflict();

commit 0cd3b3fe0903cb6b7bca4ec4ea6c6ee35446cb99

Oops, fixed two more unit tests that broke with commit c9969f52 :(

LiteCore/Database/TreeDocument.cc
@@ -32,11 +32,6 @@
 #include <algorithm>
 
 
-namespace litecore {
-    extern LogDomain SyncLog;   // Defined in Worker.cc
-}
-
-
 namespace c4Internal {
 
     static const uint32_t kDefaultMaxRevTreeDepth = 20;
@@ -350,7 +345,7 @@ namespace c4Internal {
                     } else {
                         effect = "doing nothing";
                     }
-                    LogTo(SyncLog, "c4doc_put detected server-side branch-switch: \"%.*s\" %.*s to %.*s; %s",
+                    LogTo(DBLog, "c4doc_put detected server-side branch-switch: \"%.*s\" %.*s to %.*s; %s",
                           SPLAT(docID), SPLAT(oldRev->revID.expanded()),
                           SPLAT(newRev->revID.expanded()), effect);
                 }

C/tests/c4DatabaseInternalTest.cc
@@ -53,6 +53,8 @@ static C4Document* c4enum_nextDocument(C4DocEnumerator *e, C4Error *outError) no
 ////////////////////////////////////////////////////////////////////////////////
 class C4DatabaseInternalTest : public C4Test {
 public:
+    C4RemoteID _remoteID {0};
+
     
     C4DatabaseInternalTest(int testOption) :C4Test(testOption) { }
 
@@ -108,6 +110,7 @@ public:
         rq.body = body;
         rq.revFlags = flags;
         rq.save = true;
+        rq.remoteDBID = _remoteID;
         return c4doc_put(db, &rq, nullptr, error);
     }
     
@@ -144,6 +147,7 @@ public:
         rq.body = body;
         rq.revFlags = flags;
         rq.save = true;
+        rq.remoteDBID = _remoteID;
         size_t commonAncestorIndex;
         return c4doc_put(db, &rq, &commonAncestorIndex, error);
     }
@@ -581,7 +585,7 @@ N_WAY_TEST_CASE_METHOD(C4DatabaseInternalTest, "DeleteAndRecreate", "[Database][
 // test06_RevTree
 N_WAY_TEST_CASE_METHOD(C4DatabaseInternalTest, "RevTree", "[Database][C]") {
     if(!isRevTrees()) return;
-    
+
     // TODO: Observer
     
     C4String docID = C4STR("MyDocID");
@@ -601,13 +605,16 @@ N_WAY_TEST_CASE_METHOD(C4DatabaseInternalTest, "RevTree", "[Database][C]") {
     C4SequenceNumber lastSeq = c4db_getLastSequence(db);
     forceInsert(docID, history, historyCount, body);
     REQUIRE(c4db_getLastSequence(db) == lastSeq);
-    
+
+    // Insert a conflict:
+    _remoteID = 1;  // Treat insertions as coming from a remote db by the replicator
     const size_t conflictHistoryCount = 5;
     const C4String conflictHistory[conflictHistoryCount] =
     {C4STR("5-5555"), C4STR("4-4545"), C4STR("3-3030"),
         C4STR("2-2222"), C4STR("1-1111")};
     C4String conflictBody = C4STR("{\"message\":\"yo\"}");
     forceInsert(docID, conflictHistory, conflictHistoryCount, conflictBody);
+    _remoteID = 0;
 
     // We handle conflicts somewhat differently now than in CBL 1. When a conflict is created the
     // new revision(s) are marked with kRevConflict, and such revisions can never be current. So

C/tests/c4DocumentTest.cc
@@ -451,6 +451,7 @@ N_WAY_TEST_CASE_METHOD(C4Test, "Document Put", "[Database][C]") {
     // Insert existing rev that conflicts:
     rq.body = C4STR("{\"from\":\"elsewhere\"}");
     rq.existingRevision = true;
+    rq.remoteDBID = 1;
     C4Slice kConflictRevID = isRevTrees() ? C4STR("2-deadbeef")
                                           : C4STR("1@binky");
     C4Slice history[2] = {kConflictRevID, kExpectedRevID};