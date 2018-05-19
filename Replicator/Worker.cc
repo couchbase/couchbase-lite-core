48019af65ebd48d56b993d4da7c4e35e0c73df39
Jim Borden <jim.borden@couchbase.com>
2018-05-19 12:02:46 +0900

    Add additional keys for redaction after audit

--- Replicator/Worker.cc
+++ Replicator/Worker.cc
@@ -24,6 +24,7 @@
 #include "PlatformCompat.hh"
 #include "BLIP.hh"
 #include <sstream>
+#include <unordered_set>
 
 #if defined(__clang__) && !defined(__ANDROID__)
 #include <cxxabi.h>
@@ -41,6 +42,7 @@ namespace litecore {
 namespace litecore { namespace repl {
 
     LogDomain SyncBusyLog("SyncBusy", LogLevel::Warning);
+    static unordered_set<string> kRedactionKeys { kC4ReplicatorAuthPassword, kC4ReplicatorOptionCookies, kC4ReplicatorOptionExtraHeaders };
 
 
     static void writeRedacted(Dict dict, stringstream &s) {
@@ -51,7 +53,7 @@ namespace litecore { namespace repl {
                 s << ", ";
             slice key = i.keyString();
             s << key << ":";
-            if (key == slice(C4STR(kC4ReplicatorAuthPassword))) {
+            if (kRedactionKeys.find(key.asString()) != kRedactionKeys.end()) {
                 s << "\"********\"";
             } else if (i.value().asDict()) {
                 writeRedacted(i.value().asDict(), s);
