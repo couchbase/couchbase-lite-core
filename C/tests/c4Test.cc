c93064fba51679f80b9f16796cd5786889b998d1
Jim Borden <jim.borden@couchbase.com>
2018-05-25 06:46:49 +0900

    Try increasing the timeout for leak detection to get rid of an odd Jenkins failure

--- C/tests/c4Test.cc
+++ C/tests/c4Test.cc
@@ -252,7 +252,7 @@ C4Test::~C4Test() {
         int leaks;
         int attempt = 0;
         while ((leaks = c4_getObjectCount() - objectCount) > 0 && attempt++ < 10) {
-            this_thread::sleep_for(chrono::microseconds(200000)); // wait up to 2 seconds for bg threads to free objects
+            this_thread::sleep_for(chrono::microseconds(500000)); // wait up to 5 seconds for bg threads to free objects
         }
         if (leaks > 0) {
             fprintf(stderr, "*** LEAKED LITECORE OBJECTS: \n");
