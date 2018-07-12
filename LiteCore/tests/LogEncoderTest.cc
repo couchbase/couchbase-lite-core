9f53884c9f4685f67ab81fe0dc4b2194f37e8510
Jim Borden <jim.borden@couchbase.com>
2018-07-12 12:58:32 -0400

    Fix Windows log prune test
    Apparently closing the stream to a file also changes the modified date on Windows, so put the sleep at the beginning of the loop for the 2nd and 3rd iteration instead at the end of 1st and 2nd

--- LiteCore/tests/LogEncoderTest.cc
+++ LiteCore/tests/LogEncoderTest.cc
@@ -148,19 +148,18 @@ TEST_CASE("Logging prune old files", "[Log]") {
     FilePath tmpLogDir = FilePath::tempDirectory()["Log_Prune/"];
     tmpLogDir.mkdir();
     for(int i = 0; i < 11; i++) {
+        if(i == 1 || i == 2) {
+            // Need some of the logs to be later because otherwise it is
+            // impossible to test which logs should disappear
+            this_thread::sleep_for(chrono::seconds(2));
+        }
+        
         stringstream s;
         s << "log" << 11 - i;
         FilePath f(tmpLogDir[s.str()]);
         ofstream fout(tmpLogDir[s.str()].canonicalPath(), ofstream::trunc|ofstream::binary|ofstream::out);
         LogEncoder e(fout);
         e.log(0, nullptr, LogEncoder::None, "Hi");
-
-        
-        if(i <= 1) {
-            // Need some of the logs to be later because otherwise it is
-            // impossible to test which logs should disappear
-            this_thread::sleep_for(chrono::seconds(2));
-        }
     }
 
     LogDomain::writeEncodedLogsTo(tmpLogDir["log_last"].canonicalPath(), LogLevel::Info, "Hello");
