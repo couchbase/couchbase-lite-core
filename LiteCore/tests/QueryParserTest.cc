couchbase/couchbase-lite-core

commit d4028aac9d76f89bf12a60432db6a917db4ef5e5

‘ANY’ query now calls fl_contains() when possible
An ‘ANY’ expression in a query now compiles to a call to fl_contains()
if its predicate is a simple equality check.

LiteCore/Query/QueryParser.cc
@@ -46,6 +46,7 @@ namespace litecore {
     static constexpr slice kCountFnName = "fl_count"_sl;
     static constexpr slice kExistsFnName= "fl_exists"_sl;
     static constexpr slice kResultFnName= "fl_result"_sl;
+    static constexpr slice kContainsFnName = "fl_contains"_sl;
 
     // Existing SQLite FTS rank function:
     static constexpr slice kRankFnName  = "rank"_sl;
@@ -141,7 +142,7 @@ namespace litecore {
 
     
     static string propertyFromOperands(Array::iterator &operands);
-    static string propertyFromNode(const Value *node);
+    static string propertyFromNode(const Value *node, char prefix ='.');
 
 
 #pragma mark - QUERY PARSER TOP LEVEL:
@@ -380,7 +381,7 @@ namespace litecore {
         if (from) {
             for (Array::iterator i(requiredArray(from, "FROM value")); i; ++i) {
                 if (first)
-                _propertiesUseAliases = true;
+                    _propertiesUseAliases = true;
                 auto entry = requiredDict(i.value(), "FROM item");
                 string alias = requiredString(getCaseInsensitive(entry, "AS"_sl),
                                               "AS in FROM item").asString();
@@ -801,10 +802,24 @@ namespace litecore {
         _variables.insert(var);
 
         string property = propertyFromNode(operands[1]);
-        //OPT: If expr is `var = value`, can generate `fl_contains(array, value)` instead
+        auto predicate = requiredArray(operands[2], "ANY/EVERY third parameter");
+
 
         bool every = !op.caseEquivalent("ANY"_sl);
         bool anyAndEvery = op.caseEquivalent("ANY AND EVERY"_sl);
+
+        if (op.caseEquivalent("ANY"_sl) && predicate->count() == 3
+                                        && predicate->get(0)->asString() == "="_sl
+                                        && propertyFromNode(predicate->get(1), '?') == var) {
+            // If predicate is `var = value`, generate `fl_contains(array, value)` instead
+            _sql << kContainsFnName << '(';
+            if (_propertiesUseAliases)
+                _sql << _dbAlias << ".";
+            _sql << "body, '" << property << "', ";
+            parseNode(predicate->get(2));
+            _sql << ')';
+            return;
+        }
 
         if (anyAndEvery) {
             _sql << '(';
@@ -819,7 +834,7 @@ namespace litecore {
         _sql << " AS _" << var << " WHERE ";
         if (every)
             _sql << "NOT (";
-        parseNode(operands[2]);
+        parseNode(predicate);
         if (every)
             _sql << ')';
         _sql << ')';
@@ -1048,17 +1063,20 @@ namespace litecore {
 
 
     // Returns the property represented by a node, or "" if it's not a property node
-    static string propertyFromNode(const Value *node) {
+    static string propertyFromNode(const Value *node, char prefix) {
         Array::iterator i(node->asArray());
         if (i.count() >= 1) {
             auto op = i[0]->asString();
-            if (op && op[0] == '.') {
+            if (op && op[0] == prefix) {
+                ++i;  // skip "." item
                 if (op.size == 1) {
-                    ++i;  // skip "." item
                     return propertyFromOperands(i);
                 } else {
                     op.moveStart(1);
-                    return (string)op;
+                    string result = (string)op;
+                    if (i)
+                        result += '.' + propertyFromOperands(i);
+                    return result;
                 }
             }
         }

LiteCore/tests/QueryParserTest.cc
@@ -147,7 +147,11 @@ TEST_CASE_METHOD(QueryParserTest, "QueryParser property contexts", "[Query]") {
 
 TEST_CASE_METHOD(QueryParserTest, "QueryParser ANY", "[Query]") {
     CHECK(parseWhere("['ANY', 'X', ['.', 'names'], ['=', ['?', 'X'], 'Smith']]")
-          == "EXISTS (SELECT 1 FROM fl_each(body, 'names') AS _X WHERE _X.value = 'Smith')");
+          == "fl_contains(body, 'names', 'Smith')");
+    CHECK(parseWhere("['ANY', 'X', ['.', 'names'], ['=', ['?X'], 'Smith']]")
+          == "fl_contains(body, 'names', 'Smith')");
+    CHECK(parseWhere("['ANY', 'X', ['.', 'names'], ['>', ['?', 'X'], 3.14]]")
+          == "EXISTS (SELECT 1 FROM fl_each(body, 'names') AS _X WHERE _X.value > 3.14)");
     CHECK(parseWhere("['EVERY', 'X', ['.', 'names'], ['=', ['?', 'X'], 'Smith']]")
           == "NOT EXISTS (SELECT 1 FROM fl_each(body, 'names') AS _X WHERE NOT (_X.value = 'Smith'))");
     CHECK(parseWhere("['ANY AND EVERY', 'X', ['.', 'names'], ['=', ['?', 'X'], 'Smith']]")

LiteCore/Query/SQLiteFleeceFunctions.cc
@@ -125,84 +125,89 @@ namespace litecore {
     }
 
 
-    // fl_contains(body, propertyPath, all?, value1, ...) -> 0/1
+    // fl_contains(body, propertyPath, value) -> 0/1
     static void fl_contains(sqlite3_context* ctx, int argc, sqlite3_value **argv) noexcept {
-        if (argc < 4) {
-            sqlite3_result_error(ctx, "fl_contains: too few arguments", -1);
-            return;
-        }
-        const Value *val;
-        if (!evaluatePathFromArgs(ctx, argv, true, &val))
-            return;
-        const Array *array = val ? val->asArray() : nullptr;
-        if (!array) {
-            sqlite3_result_int(ctx, 0);
+        const Value *collection;
+        if (evaluatePathFromArgs(ctx, argv, true, &collection))
+            collectionContainsImpl(ctx, collection, argv[2]);
+    }
+
+
+    void collectionContainsImpl(sqlite3_context* ctx, const Value *collection, sqlite3_value *arg) {
+        if (!collection || collection->type() < kArray) {
+            sqlite3_result_zeroblob(ctx, 0); // JSON null
             return;
         }
 
-        int found = 0, needed = 1;
-        if (sqlite3_value_int(argv[2]) != 0)    // 'all' flag
-            needed = (argc - 3);
+        // Set up a predicate callback that will match the desired value:
+        union target_t {
+            int64_t i;
+            double d;
+            FLSlice s;
+        };
+        target_t target;
+        valueType targetType;
+        bool (*predicate)(const Value*, const target_t&);
 
-        for (int i = 3; i < argc; ++i) {
-            auto arg = argv[i];
-            auto argType = sqlite3_value_type(arg);
-            switch (argType) {
-                case SQLITE_INTEGER: {
-                    int64_t n = sqlite3_value_int64(arg);
-                    for (Array::iterator j(array); j; ++j) {
-                        if (j->type() == kNumber && j->isInteger() && j->asInt() == n) {
-                            ++found;
-                            break;
-                        }
-                    }
-                    break;
-                }
-                case SQLITE_FLOAT: {
-                    double n = sqlite3_value_double(arg);
-                    for (Array::iterator j(array); j; ++j) {
-                        if (j->type() == kNumber && j->asDouble() == n) {   //TODO: Approx equal?
-                            ++found;
-                            break;
-                        }
-                    }
-                    break;
-                }
-                case SQLITE_BLOB:
-                    if (sqlite3_value_bytes(arg) == 0) {
-                        // A zero-length blob represents a Fleece/JSON 'null'.
-                        for (Array::iterator j(array); j; ++j) {
-                            if (j->type() == kNull) {
-                                ++found;
-                                break;
-                            }
-                        }
-                        break;
-                    }
-                    // ... else fall through to match blobs:
-                case SQLITE_TEXT: {
-                    valueType type = (argType == SQLITE_TEXT) ? kString : kData;
-                    const void *blob = sqlite3_value_blob(arg);
-                    slice blobVal(blob, sqlite3_value_bytes(arg));
-                    for (Array::iterator j(array); j; ++j) {
-                        if (j->type() == type && j->asString() == blobVal) {
-                            ++found;
-                            break;
-                        }
-                    }
-                    break;
-                }
-                case SQLITE_NULL: {
-                    // A SQL null doesn't match anything
-                    break;
-                }
+        switch (sqlite3_value_type(arg)) {
+            case SQLITE_INTEGER: {
+                targetType = kNumber;
+                target.i = sqlite3_value_int64(arg);
+                predicate = [](const Value *v, const target_t &t) {return v->asInt() == t.i;};
+                break;
             }
-            if (found >= needed) {
-                sqlite3_result_int(ctx, 1);
+            case SQLITE_FLOAT: {
+                targetType = kNumber;
+                target.d = sqlite3_value_double(arg);
+                predicate = [](const Value *v, const target_t &t) {return v->asDouble() == t.d;};
+                break;
+            }
+            case SQLITE_TEXT: {
+                targetType = kString;
+                target.s = slice(sqlite3_value_blob(arg), sqlite3_value_bytes(arg));
+                predicate = [](const Value *v, const target_t &t) {return v->asString() == slice(t.s);};
+                break;
+            }
+            case SQLITE_BLOB: {
+                if (sqlite3_value_bytes(arg) == 0) {
+                    // A zero-length blob represents a Fleece/JSON 'null'.
+                    sqlite3_result_zeroblob(ctx, 0); // JSON null
+                    return;
+                } else {
+                    targetType = kData;
+                    target.s = slice(sqlite3_value_blob(arg), sqlite3_value_bytes(arg));
+                    predicate = [](const Value *v, const target_t &t) {return v->asData() == slice(t.s);};
+                }
+                break;
+            }
+            case SQLITE_NULL:
+            default:{
+                // A SQL null (MISSING) doesn't match anything
+                sqlite3_result_null(ctx);
                 return;
             }
         }
-        sqlite3_result_int(ctx, 0);
+
+        // Now iterate the array/dict:
+        bool found = false;
+        if (collection->type() == kArray) {
+            for (Array::iterator j(collection->asArray()); j; ++j) {
+                auto val = j.value();
+                if (val->type() == targetType && predicate(val, target)) {
+                    found = true;
+                    break;
+                }
+            }
+        } else {
+            for (Dict::iterator j(collection->asDict()); j; ++j) {
+                auto val = j.value();
+                if (val->type() == targetType && predicate(val, target)) {
+                    found = true;
+                    break;
+                }
+            }
+        }
+        sqlite3_result_int(ctx, found);
     }
 
 
@@ -255,7 +260,7 @@ namespace litecore {
         { "fl_nested_value",   2, fl_nested_value },
         { "fl_exists",         2, fl_exists },
         { "fl_count",          2, fl_count },
-        { "fl_contains",      -1, fl_contains },
+        { "fl_contains",       3, fl_contains },
         { "fl_result",         1, fl_result },
         { }
     };

LiteCore/Query/SQLiteFleeceUtil.hh
@@ -100,6 +100,10 @@ namespace litecore {
     // Sets the function result to be a Fleece/JSON null (an empty blob with kFleeceNullSubtype)
     void setResultFleeceNull(sqlite3_context*);
 
+    // Common implementation of fl_contains and array_contains
+    void collectionContainsImpl(sqlite3_context*, const fleece::Value *collection, sqlite3_value *arg);
+
+
     //// Registering SQLite functions:
 
     struct SQLiteFunctionSpec {

LiteCore/tests/QueryTest.cc
@@ -964,57 +964,74 @@ TEST_CASE_METHOD(QueryTest, "Query JOINs", "[Query]") {
 }
 
 
-static void checkUnnestQuery(Query *query, int docNo, int expectedRowCount) {
-    unique_ptr<QueryEnumerator> e(query->createEnumerator());
-    CHECK(e->getRowCount() == expectedRowCount);
-    while (e->next()) {
-        auto cols = e->columns();
-        slice docID = cols[0]->asString();
-        string expectedDocID = stringWithFormat("rec-%03d", docNo);
-        CHECK(docID == slice(expectedDocID));
-        ++docNo;
+class ArrayQueryTest : public QueryTest {
+protected:
+    Retained<Query> query;
+
+    void checkQuery(int docNo, int expectedRowCount) {
+        unique_ptr<QueryEnumerator> e(query->createEnumerator());
+        CHECK(e->getRowCount() == expectedRowCount);
+        while (e->next()) {
+            auto cols = e->columns();
+            slice docID = cols[0]->asString();
+            string expectedDocID = stringWithFormat("rec-%03d", docNo);
+            CHECK(docID == slice(expectedDocID));
+            ++docNo;
+        }
     }
+
+    void testArrayQuery(const string &json, bool checkOptimization) {
+        addArrayDocs(1, 90);
+
+        query = store->compileQuery(json);
+        string explanation = query->explain();
+        Log("%s", explanation.c_str());
+        checkQuery(88, 3);
+
+        Log("-------- Repeating with index --------");
+        store->createIndex("numbersIndex"_sl,
+                           "[[\".numbers\"]]"_sl,
+                           KeyStore::kArrayIndex);
+        query = store->compileQuery(json);
+        explanation = query->explain();
+        Log("%s", explanation.c_str());
+        if (checkOptimization)
+            CHECK(explanation.find("SCAN") == string::npos);    // should be no linear table scans
+        checkQuery(88, 3);
+
+        Log("-------- Adding a doc --------");
+        addArrayDocs(91, 1);
+        checkQuery(88, 4);
+
+        Log("-------- Purging a doc --------");
+        deleteDoc("rec-091"_sl, true);
+        checkQuery(88, 3);
+
+        Log("-------- Soft-deleting a doc --------");
+        deleteDoc("rec-090"_sl, false);
+        checkQuery(88, 2);
+
+        Log("-------- Un-deleting a doc --------");
+        undeleteDoc("rec-090"_sl);
+        checkQuery(88, 3);
+    }
+};
+
+
+TEST_CASE_METHOD(ArrayQueryTest, "Query ANY", "[Query]") {
+    testArrayQuery(json5("['SELECT', {\
+                             WHERE: ['ANY', 'num', ['.numbers'],\
+                                            ['=', ['?num'], 'eight-eight']]}]"),
+                   false);
 }
 
 
-TEST_CASE_METHOD(QueryTest, "Query UNNEST", "[Query]") {
-    addArrayDocs(1, 90);
-
-    auto queryJSON = json5("['SELECT', {\
+TEST_CASE_METHOD(ArrayQueryTest, "Query UNNEST", "[Query]") {
+    testArrayQuery(json5("['SELECT', {\
                               FROM: [{as: 'doc'}, \
                                      {as: 'num', 'unnest': ['.doc.numbers']}],\
-                              WHERE: ['=', ['.num'], 'eight-eight']}]");
-    Retained<Query> query;
-    query = store->compileQuery(queryJSON);
-    string explanation = query->explain();
-    Log("%s", explanation.c_str());
-    checkUnnestQuery(query, 88, 3);
-
-    Log("-------- Repeating with index --------");
-    store->createIndex("numbersIndex"_sl,
-                       "[[\".numbers\"]]"_sl,
-                       KeyStore::kArrayIndex);
-    query = store->compileQuery(queryJSON);
-    explanation = query->explain();
-    Log("%s", explanation.c_str());
-    CHECK(explanation.find("SCAN") == string::npos);    // should be no linear table scans
-    checkUnnestQuery(query, 88, 3);
-
-    Log("-------- Adding a doc --------");
-    addArrayDocs(91, 1);
-    checkUnnestQuery(query, 88, 4);
-
-    Log("-------- Purging a doc --------");
-    deleteDoc("rec-091"_sl, true);
-    checkUnnestQuery(query, 88, 3);
-
-    Log("-------- Soft-deleting a doc --------");
-    deleteDoc("rec-090"_sl, false);
-    checkUnnestQuery(query, 88, 2);
-
-    Log("-------- Un-deleting a doc --------");
-    undeleteDoc("rec-090"_sl);
-    checkUnnestQuery(query, 88, 3);
+                              WHERE: ['=', ['.num'], 'eight-eight']}]"),
+                   true);
 }
 
 

commit cb575bef4fe05e10f456b80ad23cae7e35d91a68

Fixed regression in recent fl_contains optimization
Regression introduced in d4028aa -- could use the wrong table alias
and/or property path in the fl_contains call.

LiteCore/Query/QueryParser.cc
@@ -821,12 +821,7 @@ namespace litecore {
                                         && predicate->get(0)->asString() == "="_sl
                                         && propertyFromNode(predicate->get(1), '?') == var) {
             // If predicate is `var = value`, generate `fl_contains(array, value)` instead
-            _sql << kContainsFnName << '(';
-            if (_propertiesUseAliases)
-                _sql << _dbAlias << ".";
-            _sql << "body, '" << property << "', ";
-            parseNode(predicate->get(2));
-            _sql << ')';
+            writePropertyGetter(kContainsFnName, property, predicate->get(2));
             return;
         }
 
@@ -1113,7 +1108,7 @@ namespace litecore {
 
 
     // Writes a call to a Fleece SQL function, including the closing ")".
-    void QueryParser::writePropertyGetter(slice fn, string property) {
+    void QueryParser::writePropertyGetter(slice fn, string property, const fleece::Value *param) {
         string alias, tablePrefix;
         if (_propertiesUseAliases) {
             // Interpret the first component of the property as a db alias:
@@ -1165,6 +1160,10 @@ namespace litecore {
             if(!property.empty()) {
                 _sql << ", ";
                 writeSQLString(_sql, slice(property));
+            }
+            if (param) {
+                _sql << ", ";
+                parseNode(param);
             }
             _sql << ")";
         }

LiteCore/Query/QueryParser.hh
@@ -152,7 +152,7 @@ namespace litecore {
         void functionOp(slice, fleece::Array::iterator&);
 
         bool writeNestedPropertyOpIfAny(fleece::slice fnName, fleece::Array::iterator &operands);
-        void writePropertyGetter(slice fn, std::string property);
+        void writePropertyGetter(slice fn, std::string property, const fleece::Value *param =nullptr);
         void writeUnnestPropertyGetter(slice fn, const std::string &property,
                                        const std::string &alias, aliasType);
         void writeEachExpression(const std::string &property);

LiteCore/tests/QueryParserTest.cc
@@ -156,6 +156,13 @@ TEST_CASE_METHOD(QueryParserTest, "QueryParser ANY", "[Query]") {
           == "NOT EXISTS (SELECT 1 FROM fl_each(body, 'names') AS _X WHERE NOT (_X.value = 'Smith'))");
     CHECK(parseWhere("['ANY AND EVERY', 'X', ['.', 'names'], ['=', ['?', 'X'], 'Smith']]")
           == "(fl_count(body, 'names') > 0 AND NOT EXISTS (SELECT 1 FROM fl_each(body, 'names') AS _X WHERE NOT (_X.value = 'Smith')))");
+
+    CHECK(parseWhere("['SELECT', {FROM: [{AS: 'person'}],\
+                                 WHERE: ['ANY', 'X', ['.', 'person', 'names'], ['=', ['?', 'X'], 'Smith']]}]")
+          == "SELECT person.key, person.sequence FROM kv_default AS \"person\" WHERE ((fl_contains(\"person\".body, 'names', 'Smith'))) AND (\"person\".flags & 1) = 0");
+    CHECK(parseWhere("['SELECT', {FROM: [{AS: 'person'}, {AS: 'book', 'ON': 1}],\
+                                 WHERE: ['ANY', 'X', ['.', 'book', 'keywords'], ['=', ['?', 'X'], 'horror']]}]")
+          == "SELECT person.key, person.sequence FROM kv_default AS \"person\" CROSS JOIN kv_default AS \"book\" ON (1) AND (\"book\".flags & 1) = 0 WHERE ((fl_contains(\"book\".body, 'keywords', 'horror'))) AND (\"person\".flags & 1) = 0");
 }
 
 