f993b03b617015f2749d7ded1e905f8370d4a617
Jens Alfke <jens@couchbase.com>
2018-10-30 15:14:01 -0700

    CMake: Added build_macos_ee.sh script

---
+++ build_cmake/scripts/build_macos_ee.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+SCRIPT_DIR=`dirname $0`
+pushd $SCRIPT_DIR/..
+
+mkdir -p macos
+pushd macos
+core_count=`getconf _NPROCESSORS_ONLN`
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_ENTERPRISE=ON ../..
+make -j `expr $core_count + 1`
+if [ -d "couchbase-lite-core" ]; then
+    dysumutil couchbase-lite-core/libLiteCore.dylib couchbase-lite-core/libLiteCore.dylib.dSYM
+    strip -x couchbase-lite-core/libLiteCore.dylib
+else
+    dsymutil libLiteCore.dylib -o libLiteCore.dylib.dSYM
+    strip -x libLiteCore.dylib
+fi
+popd
+popd
