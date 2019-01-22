9970d989847ec2faf6f78346d4a331ae8ac8d473
Jim Borden <jim.borden@couchbase.com>
2019-01-22 10:44:14 +0900

    Fix dynamic destructor issue and correct invalid behavior
    Two fixes are in this commit.  One change `sRegistry` to be a pointer so that it won't be automatically freed upon process exit (the reasoning for this is in the comments).  The other is that `unordered_map` does not automatically replace items and so the insert behavior was incorrect (it would ignore any subsequent insert with the same key).

--- LiteCore/Query/PredictiveModel.cc
+++ LiteCore/Query/PredictiveModel.cc
@@ -27,24 +27,29 @@ namespace litecore {
     using namespace std;
     using namespace fleece;
 
-    static unordered_map<string, Retained<PredictiveModel>> sRegistry;
+    // HACK: Making this a pointer to avoid the dynamic atexit destructor
+    // Since the "unregister" callback potentially calls into managed code
+    // (i.e. C#, etc) it will cause errors if the runtime has already been
+    // unloaded (which is the case during atexit)
+    static unordered_map<string, Retained<PredictiveModel>> *sRegistry 
+        = new unordered_map<string, Retained<PredictiveModel>>;
     static mutex sRegistryMutex;
-
 
     void PredictiveModel::registerAs(const std::string &name) {
         lock_guard<mutex> lock(sRegistryMutex);
-        sRegistry.insert({name, this});
+        sRegistry->erase(name);
+        sRegistry->insert({name, this});
     }
 
     bool PredictiveModel::unregister(const std::string &name) {
         lock_guard<mutex> lock(sRegistryMutex);
-        return sRegistry.erase(name) > 0;
+        return sRegistry->erase(name) > 0;
     }
 
     Retained<PredictiveModel> PredictiveModel::named(const std::string &name) {
         lock_guard<mutex> lock(sRegistryMutex);
-        auto i = sRegistry.find(name);
-        if (i == sRegistry.end())
+        auto i = sRegistry->find(name);
+        if (i == sRegistry->end())
             return nullptr;
         return i->second;
     }
