e2456597d00bbed68d9b93cf01656c18885029fb
Jens Alfke <jens@couchbase.com>
2019-05-22 18:09:18 -0700

    Improved build process for libwebsockets
    * CMake build directory is now located in Xcode object-file dir, not
      in the source tree. Final static lib is copied to Xcode build output
      dir for the linker to find.
    * Builds with Debug or RelWithDebugInfo, depending on Xcode config.
    * Builds with Address Sanitizer if it’s enabled in Xcode.

--- Xcode/build_libwebsockets.sh
+++ Xcode/build_libwebsockets.sh
@@ -2,24 +2,55 @@
 # This script builds the libwebsockets submodule.
 # It's run by Xcode when building the target LiteCoreWebSocket.
 
-cd "$SRCROOT/../vendor/libwebsockets"
-mkdir -p build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Debug  -DLWS_WITH_ASAN=1 \
-         -DLWS_WITHOUT_SERVER=0 \
-         -DLWS_WITH_HTTP2=0 \
-         -DLWS_WITH_SHARED=0 \
-         -DLWS_WITH_LEJP=0 \
-         -DLWS_WITH_STRUCT_JSON=0 \
-         -DLWS_WITHOUT_TESTAPPS=1 \
-         -DLWS_WITHOUT_TEST_PING=1 \
-         -DLWS_WITHOUT_TEST_CLIENT=1 \
-         -DLWS_WITHOUT_TEST_SERVER=1 \
-         -DLWS_WITHOUT_TEST_SERVER_EXTPOLL=1 \
-         -DLWS_WITH_MBEDTLS=1 \
-         -DLWS_MBEDTLS_INCLUDE_DIRS=../../mbedtls/build/include \
-         -DMBEDCRYPTO_LIBRARY=../../mbedtls/build/library/libmbedcrypto.a \
-         -DMBEDTLS_LIBRARY=../../mbedtls/build/library/libmbedtls.a \
-         -DMBEDX509_LIBRARY=../../mbedtls/build/library/libmbedx509.a
+# Create the CMake build directory:
+CMAKE_BUILD_DIR="$PER_VARIANT_OBJECT_FILE_DIR/libwebsockets"
+mkdir -p "$CMAKE_BUILD_DIR"
+cd "$CMAKE_BUILD_DIR"
 
+# Set up the CMake build options:
+CMAKE_OPTS="-DLWS_WITHOUT_SERVER=0 \
+           -DLWS_WITH_HTTP2=0 \
+           -DLWS_WITH_SHARED=0 \
+           -DLWS_WITH_LEJP=0 \
+           -DLWS_WITH_STRUCT_JSON=0 \
+           -DLWS_WITHOUT_TESTAPPS=1 \
+           -DLWS_WITHOUT_TEST_PING=1 \
+           -DLWS_WITHOUT_TEST_CLIENT=1 \
+           -DLWS_WITHOUT_TEST_SERVER=1 \
+           -DLWS_WITHOUT_TEST_SERVER_EXTPOLL=1 \
+           -DLWS_WITH_MBEDTLS=1 \
+           -DLWS_MBEDTLS_INCLUDE_DIRS=../mbedtls/include \
+           -DMBEDCRYPTO_LIBRARY=../mbedtls/library/libmbedcrypto.a \
+           -DMBEDTLS_LIBRARY=../mbedtls/library/libmbedtls.a \
+           -DMBEDX509_LIBRARY=../mbedtls/library/libmbedx509.a"
+
+if [[ "$CONFIGURATION" == "Release*" ]]
+then
+    CMAKE_OPTS="$CMAKE_OPTS -DCMAKE_BUILD_TYPE=RelWithDebugInfo"
+else
+    CMAKE_OPTS="$CMAKE_OPTS -DCMAKE_BUILD_TYPE=Debug"
+    if [ "$ENABLE_ADDRESS_SANITIZER" == "YES" ]
+    then
+        CMAKE_OPTS="$CMAKE_OPTS -DLWS_WITH_ASAN=1"
+    fi
+fi
+
+echo "CMake options: $CMAKE_OPTS"
+
+# Build!
+cmake "$SRCROOT/../vendor/libwebsockets" $CMAKE_OPTS
 make
+
+# Copy the resulting static library to the Xcode build dir where the linker will find it:
+mkdir -p "$TARGET_BUILD_DIR"
+cp lib/libwebsockets.a "$TARGET_BUILD_DIR/"
+
+# Copy the generated lws_config.h to the derived sources dir where #include will find it,
+# but only if it's changed, to avoid triggering a bunch of recompilation:
+if cmp --quiet include/lws_config.h "$DERIVED_SOURCES_DIR/lws_config.h"
+then
+    echo "No change in lws_config.h"
+else
+    echo "Copying lws_config.h to $DERIVED_SOURCES_DIR"
+    cp include/lws_config.h "$DERIVED_SOURCES_DIR/lws_config.h"
+fi
--- Xcode/build_mbedtls.sh
+++ Xcode/build_mbedtls.sh
@@ -2,8 +2,18 @@
 # This script builds the mbedTLS submodule.
 # It's run by Xcode when building the target LiteCoreWebSocket.
 
-cd "$SRCROOT/../vendor/mbedtls"
-mkdir -p build
-cd build
-cmake .. -DENABLE_PROGRAMS=0 -DENABLE_TESTING=0
+# Create the CMake build directory:
+CMAKE_BUILD_DIR="$PER_VARIANT_OBJECT_FILE_DIR/mbedtls"
+mkdir -p "$CMAKE_BUILD_DIR"
+cd "$CMAKE_BUILD_DIR"
+
+# Set up the CMake build options:
+CMAKE_OPTS="-DCMAKE_BUILD_TYPE=RelWithDebugInfo -DENABLE_PROGRAMS=0 -DENABLE_TESTING=0"
+
+# Build!
+cmake "$SRCROOT/../vendor/mbedtls" $CMAKE_OPTS
 make
+
+# Copy the resulting static libraries to the Xcode build dir where the linker will find them:
+mkdir -p "$TARGET_BUILD_DIR"
+cp library/libmbed*.a "$TARGET_BUILD_DIR/"
