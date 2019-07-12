b2bb1326cc10a64112e892c76f78f224c12f7e5b
Jens Alfke <jens@couchbase.com>
2019-07-11 17:34:50 -0700

    CMake: Two small tweaks
    * platform_linux_desktop.cmake now recognizes 'AppleClang' name
    * removed use of obsolete LITECORE_BUILD_SQLITE flag in build_unix.sh

--- build_cmake/scripts/build_unix.sh
+++ build_cmake/scripts/build_unix.sh
@@ -8,7 +8,7 @@ pushd $SCRIPT_DIR/..
 mkdir -p unix
 pushd unix
 core_count=`getconf _NPROCESSORS_ONLN`
-CC=clang CXX=clang++ cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DLITECORE_BUILD_SQLITE=1 ../..
+CC=clang CXX=clang++ cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ../..
 make -j `expr $core_count + 1`
 if [ -d "couchbase-lite-core" ]; then
     # Enterprise Edition
