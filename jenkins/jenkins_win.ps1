bff6b4f1374d163d00026c5c3ba1fd7b34845c8a
Jim Borden <jim.borden@couchbase.com>
2019-07-23 09:59:37 -0400

    More tweaks (squash me later)

--- jenkins/jenkins_win.ps1
+++ jenkins/jenkins_win.ps1
@@ -1,9 +1,21 @@
+$ErrorActionPreference = "Stop";
+
 if($env:CHANGE_TARGET) {
     $env:BRANCH = $env:CHANGE_TARGET
 }
 
-if(Test-Path $PSScriptRoot\..\..\couchbase-lite-core-EE) {
-    Push-Location $PSScriptRoot\..\..\couchbase-lite-core-EE
+if(! $env:WORKSPACE) {
+    $env:WORKSPACE = "$PSScriptRoot\.."
+}
+
+# Jenkins is a pain because it doesn't give the option for no source
+# so instead just put a junction to the current directory in
+if(!(Test-Path $env:WORKSPACE\couchbase-lite-core)) {
+    New-Item -Type Junction -Target $env:WORKSPACE $env:WORKSPACE\couchbase-lite-core
+}
+
+if(Test-Path $env:WORKSPACE\couchbase-lite-core-EE) {
+    Push-Location $env:WORKSPACE\couchbase-lite-core-EE
     & 'C:\Program Files\Git\bin\git.exe' fetch origin
     & 'C:\Program Files\Git\bin\git.exe' reset --hard
     & 'C:\Program Files\Git\bin\git.exe' checkout $env:BRANCH
@@ -11,31 +23,38 @@ if(Test-Path $PSScriptRoot\..\..\couchbase-lite-core-EE) {
     & 'C:\Program Files\Git\bin\git.exe' pull origin $env:BRANCH
     Pop-Location
 } else {
-    & 'C:\Program Files\Git\bin\git.exe' clone ssh://git@github.com/couchbase/couchbase-lite-core-EE --branch $env:BRANCH --recursive $PSScriptRoot\..\..\couchbase-lite-core-EE
+    & 'C:\Program Files\Git\bin\git.exe' clone ssh://git@github.com/couchbase/couchbase-lite-core-EE --branch $env:BRANCH --recursive $env:WORKSPACE\couchbase-lite-core-EE
 }
 
-New-Item -Type Directory build_cmake/x64
-Push-Location build_cmake/x64
+New-Item -Type Directory $env:WORKSPACE\couchbase-lite-core\build_cmake\x64
+Push-Location $env:WORKSPACE\couchbase-lite-core\build_cmake\x64
 & 'C:\Program Files\CMake\bin\cmake.exe' -G "Visual Studio 14 2015 Win64" -DBUILD_ENTERPRISE=ON ..\..
 if($LASTEXITCODE -ne 0) {
-    exit $LASTEXITCODE
+    Write-Host "Failed to run CMake!" -ForegroundColor Red
+    exit 1
 }
 
 & 'C:\Program Files\CMake\bin\cmake.exe' --build .
 if($LASTEXITCODE -ne 0) {
-    exit $LASTEXITCODE
+    Write-Host "Failed to build!" -ForegroundColor Red
+    exit 1
 }
 
 $env:LiteCoreTestsQuiet=1
 Push-Location LiteCore\tests\Debug
 .\CppTests -r list
 if($LASTEXITCODE -ne 0) {
-    exit $LASTEXITCODE
+    Write-Host "C++ tests failed!" -ForegroundColor Red
+    exit 1
 }
 
 Pop-Location
 Push-Location C\tests\Debug
 .\C4Tests -r list
 if($LASTEXITCODE -ne 0) {
-    exit $LASTEXITCODE
+    Write-Host "C tests failed!" -ForegroundColor Red
+    exit 1
 }
+
+Pop-Location
+Pop-Location
