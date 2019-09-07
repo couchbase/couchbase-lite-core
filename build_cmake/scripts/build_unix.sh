69902992a2d416b2eb25de34cf3624bf5462f6ef
Jim Borden <jim.borden@couchbase.com>
2019-09-07 09:20:19 +0900

    More environmental tweaks

--- build_cmake/scripts/build_unix.sh
+++ build_cmake/scripts/build_unix.sh
@@ -8,5 +8,5 @@ pushd $SCRIPT_DIR/..
 mkdir -p unix
 pushd unix
 core_count=`getconf _NPROCESSORS_ONLN`
-cmake -DCMAKE_BUILD_TYPE=MinSizeRel ../..
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ../..
 make -j `expr $core_count + 1`
