dfacbf4c24ff6e3e0df8dc336b75a258f16088d5
Jim Borden <jim.borden@couchbase.com>
2019-09-07 01:01:35 +0000

    CBL-347 Build with both GCC and clang (#837)

--- build_cmake/scripts/build_unix.sh
+++ build_cmake/scripts/build_unix.sh
@@ -8,13 +8,5 @@ pushd $SCRIPT_DIR/..
 mkdir -p unix
 pushd unix
 core_count=`getconf _NPROCESSORS_ONLN`
-CC=clang CXX=clang++ cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ../..
-make -j `expr $core_count + 1`
-if [ -d "couchbase-lite-core" ]; then
-    # Enterprise Edition
-    ../scripts/strip.sh `pwd`/couchbase-lite-core
-else
-    ../scripts/strip.sh `pwd`
-fi
-popd
-popd
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ../..
+make -j `expr $core_count + 1` LiteCore
