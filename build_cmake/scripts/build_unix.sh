9acad98b123e4f768ff7e2cb86cc34a932b84658
Jim Borden <jim.borden@couchbase.com>
2019-09-11 14:24:25 +0900

    Forgot that I used this in PR validation...
    Have it build the tests after all too, because that's also an important part of LiteCore (having the proper things exported)

---
+++ build_cmake/scripts/build_unix.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR=`dirname $0`
+pushd $SCRIPT_DIR/..
+
+mkdir -p unix
+pushd unix
+core_count=`getconf _NPROCESSORS_ONLN`
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo ../..
+make -j `expr $core_count + 1`
