5bf3854f032eeb93fe278598febba7bef41204e0
Jim Borden <jim.borden@couchbase.com>
2019-10-17 18:38:40 +0000

    CBL-430 Hide non-exported symbols on Linux (#853)

--- C/tests/cmake/platform_linux.cmake
+++ C/tests/cmake/platform_linux.cmake
@@ -24,6 +24,11 @@ function(setup_build)
     target_link_libraries(
         C4Tests PRIVATE
         pthread
+        FleeceBase
+        Support
+        BLIPStatic
+        CivetWeb
+        mbedcrypto
         ${LIBCXX_LIB}
         ${LIBCXXABI_LIB}
         dl
