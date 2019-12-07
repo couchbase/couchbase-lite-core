cc80e9ad5394aa43ec02a8042133fb6d78911d7d
Jim Borden <jim.borden@couchbase.com>
2019-12-07 11:07:48 +0900

    Implement SetThreadName on Windows

--- src/util/ThreadUtil.hh
+++ src/util/ThreadUtil.hh
@@ -21,10 +21,31 @@
 
 #ifndef _MSC_VER
 #include <pthread.h>
+#else
+#include <Windows.h>
 #endif
 
 
 namespace litecore {
+
+#ifdef _MSC_VER
+    // This is sickening, but it is the only way to set thread names in MSVC.
+    // By raising an SEH exception (Windows equivalent to Unix signal)
+    // then catching and ignoring it
+    // https://stackoverflow.com/a/10364541/1155387
+
+    const DWORD MS_VC_EXCEPTION = 0x406D1388;
+
+#pragma pack(push, 8)
+    typedef struct
+    {
+        DWORD dwType; // Must be 0x1000.
+        LPCSTR szName; // Pointer to name (in user addr space).
+        DWORD dwThreadID; // Thread ID (-1=caller thread).
+        DWORD dwFlags; // Reserved for future use, must be zero.
+    } THREADNAME_INFO;
+#pragma pack(pop)
+#endif
 
     static inline void SetThreadName(const char *name) {
 #ifndef _MSC_VER
@@ -34,6 +55,18 @@ namespace litecore {
 #else
             pthread_setname_np(pthread_self(), name);
 #endif
+        }
+#else
+        THREADNAME_INFO info;
+        info.dwType = 0x1000;
+        info.szName = name;
+        info.dwThreadID = -1;
+        info.dwFlags = 0;
+
+        __try {
+            RaiseException(MS_VC_EXCEPTION, 0, sizeof(info)/sizeof(ULONG_PTR), (ULONG_PTR*)&info);
+        } __except(EXCEPTION_EXECUTE_HANDLER) {
+
         }
 #endif
     }
