19b91e1ee3d39cb7903b23aa9a5fe70b17d39074
Jim Borden <jim.borden@couchbase.com>
2020-04-17 07:15:52 +0900

    Fix a flaky macOS test check

--- Replicator/tests/ReplicatorAPITest.cc
+++ Replicator/tests/ReplicatorAPITest.cc
@@ -184,7 +184,7 @@ TEST_CASE_METHOD(ReplicatorAPITest, "API Connection Failure", "[C][Push]") {
     _mayGoOffline = true;
     replicate(kC4Disabled, kC4OneShot, false);
     CHECK(_callbackStatus.error.domain == POSIXDomain);
-    CHECK(_callbackStatus.error.code == ECONNREFUSED);
+    CHECK((_callbackStatus.error.code == ECONNREFUSED || _callbackStatus.error.code == ETIMEDOUT));
     CHECK(_callbackStatus.progress.unitsCompleted == 0);
     CHECK(_callbackStatus.progress.unitsTotal == 0);
     CHECK(_wentOffline);
