484c584707bf7c7146ddef9bd46e61b5d1182a0e
Jens Alfke <jens@couchbase.com>
2020-04-21 12:06:32 -0700

    Xcode: Restore iOS 9 and 32-bit iOS support
    * Changed *_DEPLOYMENT_TARGET definitions accordingly.
    * Changed VALID_ARCHS; there is no armv7a but there is armv7s.
    * Added `-fno-aligned-allocation` compiler flag on armv7/armv7s builds
      to avoid an annoying build error (see the issue)
    * #ifdef-d out the experimental Async class from Actor; this didn't
      turn out to be needed to fix the above errors, but it's a good idea
      anyway.
    
    Fixes CBL-734

--- Networking/BLIP/cmake/platform_base.cmake
+++ Networking/BLIP/cmake/platform_base.cmake
@@ -16,7 +16,7 @@ function(set_source_files_base)
         ${WEBSOCKETS_LOCATION}/WebSocketInterface.cc
         ${SUPPORT_LOCATION}/Actor.cc
         ${SUPPORT_LOCATION}/ActorProperty.cc
-        ${SUPPORT_LOCATION}/Async.cc
+#       ${SUPPORT_LOCATION}/Async.cc
         ${SUPPORT_LOCATION}/Channel.cc
         ${SUPPORT_LOCATION}/Codec.cc
         ${SUPPORT_LOCATION}/Timer.cc
