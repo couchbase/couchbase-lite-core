7840ab9da5b5da4e5286303f07323ef24fc88fcb
Jens Alfke <jens@couchbase.com>
2020-05-27 17:48:36 -0700

    Retry revs that couldn't be inserted due to delta failures
    * Inserter flags a delta error as 'transient'.
    * Pusher detects a delta error received from the peer and retries
      sending the revision, this time without a delta.
    NOTE: This will probably require a change in SG corresponding to the
          Pusher change above.
    
    For CBL-986

--- Replicator/tests/ReplicatorLoopbackTest.hh
+++ Replicator/tests/ReplicatorLoopbackTest.hh
@@ -232,10 +232,12 @@ public:
                             (rev->errorIsTransient ? "transient " : ""),
                             (dir == Dir::kPushing ? "pushing" : "pulling"),
                             SPLAT(rev->docID), SPLAT(rev->revID), message);
-                        if (dir == Dir::kPushing)
-                            _docPushErrors.emplace(rev->docID);
-                        else
-                            _docPullErrors.emplace(rev->docID);
+                        if (!rev->errorIsTransient || !_ignoreTransientErrors) {
+                            if (dir == Dir::kPushing)
+                                _docPushErrors.emplace(rev->docID);
+                            else
+                                _docPullErrors.emplace(rev->docID);
+                        }
                     }
                 } else {
                     Log(">> Replicator %s '%.*s' #%.*s",
@@ -533,6 +535,7 @@ public:
     C4Error _expectedError {};
     set<string> _docPushErrors, _docPullErrors;
     set<string> _expectedDocPushErrors, _expectedDocPullErrors;
+    bool _ignoreTransientErrors = false;
     bool _checkDocsFinished {true};
     multiset<string> _docsFinished, _expectedDocsFinished;
     unsigned _blobPushProgressCallbacks {0}, _blobPullProgressCallbacks {0};
