0e1d79d856291b8386f2ba733b87e883a4582dce
Jim Borden <jim.borden@couchbase.com>
2020-06-24 13:54:13 +0900

    Remove the SANITIZE_FOR_DEBUG_ENABLED option
    It was causing too many issues with consuming environments (Rust, Java, and .NET all had issues)

--- cmake/platform_apple.cmake
+++ cmake/platform_apple.cmake
@@ -2,25 +2,6 @@ include("${CMAKE_CURRENT_LIST_DIR}/platform_unix.cmake")
 
 function(setup_globals)
     setup_globals_unix()
-
-    if((CMAKE_BUILD_TYPE STREQUAL "Debug") AND ${SANITIZE_FOR_DEBUG_ENABLED})
-        add_compile_options(
-            -fsanitize=address 
-            -fsanitize=undefined 
-            -fsanitize=nullability
-            -fno-sanitize=enum,return,float-divide-by-zero,function,vptr
-        )
-
-        set_property(
-            DIRECTORY 
-            PROPERTY LINK_OPTIONS
-            ${LINK_OPTIONS}
-            -fsanitize=address 
-            -fsanitize=undefined 
-            -fsanitize=nullability
-            -fno-sanitize=enum,return,float-divide-by-zero,function,vptr
-        )
-    endif()
 endfunction()
 
 function(set_litecore_source)
