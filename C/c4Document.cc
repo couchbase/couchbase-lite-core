couchbase/couchbase-lite-core

commit 4c3662efbc0bbe599c3e0f38785d1bc49e1705dc

Fixed incorrect commonAncestorIndex from c4doc_put
A call to c4doc_put, with rq.existingRevision=true and where the
document didn't already exist, would incorrectly set the
*outCommonAncestorIndex to 0 -- it should be equal to the length of
the `history` array. Fixed.

C/c4Document.cc
@@ -386,21 +386,22 @@ static bool isNewDocPutRequest(C4Database *database, const C4DocPutRequest *rq) 
 
 
 // Tries to fulfil a PutRequest by creating a new Record. Returns null if one already exists.
-static Document* putNewDoc(C4Database *database, const C4DocPutRequest *rq)
+static pair<Document*,int> putNewDoc(C4Database *database,
+                                     const C4DocPutRequest *rq)
 {
     DebugAssert(rq->save, "putNewDoc optimization works only if rq->save is true");
     Record record(rq->docID);
     if (!rq->docID.buf)
         record.setKey(createDocUUID());
     Retained<Document> idoc = database->documentFactory().newDocumentInstance(record);
-    bool ok;
+    int commonAncestorIndex;
     if (rq->existingRevision)
-        ok = (idoc->putExistingRevision(*rq, nullptr) >= 0);
+        commonAncestorIndex = idoc->putExistingRevision(*rq, nullptr);
     else
-        ok = idoc->putNewRevision(*rq);
-    if (!ok)
+        commonAncestorIndex = idoc->putNewRevision(*rq) ? 0 : -1;
+    if (commonAncestorIndex < 0)
         idoc = nullptr;
-    return retain(idoc.get());
+    return {retain(idoc.get()), commonAncestorIndex};
 }
 
 
@@ -484,7 +485,7 @@ C4Document* c4doc_put(C4Database *database,
     try {
         if (rq->save && isNewDocPutRequest(database, rq)) {
             // As an optimization, write the doc assuming there is no prior record in the db:
-            doc = putNewDoc(database, rq);
+            tie(doc, commonAncestorIndex) = putNewDoc(database, rq);
             // If there's already a record, doc will be null, so we'll continue down regular path.
         }
         if (!doc) {

C/tests/c4DocumentTest.cc
@@ -694,6 +694,29 @@ N_WAY_TEST_CASE_METHOD(C4Test, "Document Put", "[Database][C]") {
 }
 
 
+N_WAY_TEST_CASE_METHOD(C4Test, "Document create from existing rev", "[Database][C]") {
+    C4Error error;
+    TransactionHelper t(db);
+
+    // Creating doc given ID:
+    C4DocPutRequest rq = {};
+    rq.docID = kDocID;
+    rq.body = kFleeceBody;
+    rq.existingRevision = true;
+    C4String history[1] = {"1-31415926"_sl};
+    rq.history = history;
+    rq.historyCount = 1;
+    rq.save = true;
+    size_t commonAncestor = 9999;
+    auto doc = c4doc_put(db, &rq, &commonAncestor, &error);
+    REQUIRE(doc != nullptr);
+    CHECK(commonAncestor == 1);
+    CHECK(doc->docID == kDocID);
+    CHECK(doc->revID == "1-31415926"_sl);
+    c4doc_release(doc);
+}
+
+
 N_WAY_TEST_CASE_METHOD(C4Test, "Document Update", "[Database][C]") {
     C4Log("Begin test");
     C4Error error;

commit cb07f6c99587577382e21443dc4cd18ebb8d7dc7

Fixed regression from 4c3662ef ("incorrect commonAncestorIndex...")
Didn't notice that it broke the test "DeleteWithProperties", oops

C/c4Document.cc
@@ -510,8 +510,7 @@ C4Document* c4doc_put(C4Database *database,
                                       outError);
                 if (!doc)
                     return nullptr;
-                if (!asInternal(doc)->putNewRevision(*rq))
-                    commonAncestorIndex = -1;
+                commonAncestorIndex = asInternal(doc)->putNewRevision(*rq) ? 0 : -1;
             }
         }
 