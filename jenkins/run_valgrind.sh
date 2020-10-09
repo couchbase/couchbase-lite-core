4664312c3cc13df24853cc32702a4d49e2cfb8da
Jim Borden <jim.borden@couchbase.com>
2020-10-09 10:38:43 +0900

    Need to run C4Tests from the correct directory

--- jenkins/run_valgrind.sh
+++ jenkins/run_valgrind.sh
@@ -4,10 +4,13 @@ script_dir=`dirname $0`
 cmake_dir=$script_dir/../build_cmake/valgrind
 mkdir -p $cmake_dir
 
+if [[ -z "${WORKSPACE}" ]]; then
+    WORKSPACE=`pwd`
+fi
+
 pushd $cmake_dir
 cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_ENTERPRISE=ON ../..
 make -j8 C4Tests
-popd
 
-valgrind --leak-check=yes $cmake_dir/C/tests/C4Tests -r list 2>&1 | tee memcheck.txt
-valgrind --tool=drd $cmake_dir/C/tests/C4Tests -r list 2>&1 | tee drd.txt
+valgrind --leak-check=yes C/tests/C4Tests -r list 2>&1 | tee $WORKSPACE/memcheck.txt
+valgrind --tool=drd C/tests/C4Tests -r list 2>&1 | tee $WORKSPACE/drd.txt
