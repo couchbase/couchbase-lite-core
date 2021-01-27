7c1f35bfe6a211aa0abe9c4f39e4d2b50a7eebed
Jayahari Vavachan <10448770+jayahariv@users.noreply.github.com>
2021-01-26 18:29:15 -0800

    Catalyst / Apple Silicon changes (master -> 2.8.4) (#1112)
    * Make catalyst compatible (#1097) - 14a4d6c21a6e582f481beed71659297079ad0a44
    * Xcode: Build arm64 for macOS (Apple Silicon) - d4d80960b8333ad13f6a944fbd027c36767e64ba
    * Fixed mbedTLS build for Xcode 12 GM release - c726b7d814ab3c8e025dbf25f27da83845c8b4b7
    * Update the mbedTLS for speed optimization. - d45b74b0ee93b0ec84ceb6ccfe053d7a2a329bdc

--- Xcode/build_setup.sh
+++ Xcode/build_setup.sh
@@ -13,6 +13,14 @@ fi
 mkdir -p "$CMAKE_BUILD_DIR"
 cd "$CMAKE_BUILD_DIR"
 
+# What architectures to build for Mac?
+if [[ "$ARCHS" =~ "arm64" ]]
+then
+  MAC_ARCHS="x86_64;arm64"
+else
+  MAC_ARCHS="x86_64"
+fi
+
 # Define an initial CMAKE_OPTS including compiler options for iOS.
 # The main build script should then append to this and pass it to cmake.
 if [[ "$PLATFORM_NAME" == "iphoneos" ]]
@@ -23,8 +31,14 @@ then
                 -DDEPLOYMENT_TARGET=$IPHONEOS_DEPLOYMENT_TARGET"
 elif [[ "$PLATFORM_NAME" == "iphonesimulator" ]]
 then
+    # CMake options for iOS Simulator -- use ios.toolchain.cmake
     CMAKE_OPTS="$CMAKE_OPTS \
                 -DCMAKE_TOOLCHAIN_FILE=$SRCROOT/../vendor/ios-cmake/ios.toolchain.cmake \
-                -DPLATFORM=SIMULATOR64 -DARCHS=i386;x86_64\
+                -DPLATFORM=SIMULATOR -DARCHS=$MAC_ARCHS;i386\
                 -DDEPLOYMENT_TARGET=$IPHONEOS_DEPLOYMENT_TARGET"
-fi
+elif [[ "$PLATFORM_NAME" == "macosx" ]]
+then
+    # CMake options for macOS
+    CMAKE_OPTS="$CMAKE_OPTS \
+                -DCMAKE_OSX_ARCHITECTURES=$MAC_ARCHS"
+ fi
