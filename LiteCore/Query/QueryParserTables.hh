9a2eb107353d111e4577fde795e12b0c1f979984
jianminzhao <76990468+jianminzhao@users.noreply.github.com>
2021-04-06 13:24:33 -0700

    Make JSON query to look the same as the N1QL in terms of operator vs function. #CBL-1823 (#1154)
    ['IS_VALUED()', ...] --> ['IS VALUED', ...]
    ['META', ...] --> ['META()', ...]

--- LiteCore/Query/QueryParserTables.hh
+++ LiteCore/Query/QueryParserTables.hh
@@ -73,6 +73,7 @@ namespace litecore {
         {"NOT IN",          2, 9,  3,  &QueryParser::inOp},
         {"BETWEEN",         3, 3,  3,  &QueryParser::betweenOp},
         {"EXISTS",          1, 1,  8,  &QueryParser::existsOp},
+        {"IS VALUED",       1, 1,  3,  &QueryParser::functionOp},
 
         {"COLLATE",         2, 2, 10,  &QueryParser::collateOp},
 
@@ -91,7 +92,7 @@ namespace litecore {
         {"ASC",             1, 1,  2,  &QueryParser::postfixOp},
         {"DESC",            1, 1,  2,  &QueryParser::postfixOp},
 
-        {"META",            0, 1, 99,  &QueryParser::metaOp},
+        {"META()",          0, 1, 99,  &QueryParser::metaOp},
 
         {nullslice,         0, 0, 99,  &QueryParser::fallbackOp} // fallback; must come last in list
     };
@@ -222,7 +223,7 @@ namespace litecore {
         {"to_object",        1, 1, "toobject"},
         {"tostring",         1, 1},
         {"to_string",        1, 1, "tostring"},
-        {"is_valued",        1, 1, "isvalued"},
+        {"is valued",        1, 1, "isvalued"},
 
         // FTS (not standard N1QL):
         {"match",            2, 2},
