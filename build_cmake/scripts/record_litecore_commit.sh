7807c75a947aed0f9193c6a5a2233d4530f5ce8d
Jim Borden <jim.borden@couchbase.com>
2021-09-01 08:31:52 +0900

    Update record LiteCore script to use SCP instead of FTP

--- build_cmake/scripts/record_litecore_commit.sh
+++ build_cmake/scripts/record_litecore_commit.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -ex
 
 # Copyright 2019-Present Couchbase, Inc.
 #
@@ -13,7 +13,6 @@
      echo
      echo "ARGUMENTS"
      echo "--user       |-u    FTP username for upload"
-     echo "--pass       |-p    FTP password for upload"
      echo "--lite-branch|-b    The branch of Couchbase Lite to record the commit for"
  }
 
@@ -25,10 +24,6 @@ while (( "$#" )); do
   case "$1" in
     -u|--user)
       USER=$2
-      shift 2
-      ;;
-    -p|--pass)
-      PASS=$2
       shift 2
       ;;
     -b|--lite-branch)
@@ -62,12 +57,6 @@ if [[ -z $USER ]]; then
     exit 1
 fi
 
-if [[ -z $PASS ]]; then
-    echo "No password provided..."
-    print_help
-    exit 2
-fi
-
 if [[ -z $LITE_BRANCH ]]; then
     echo "No Couchbase Lite branch provided..."
     print_help
@@ -77,12 +66,5 @@ fi
 SUBMODULE_SHA=`git rev-parse HEAD`
 FILENAME_PREFIX=`echo $LITE_BRANCH | sed -e 's/\//_/'`
 echo $SUBMODULE_SHA > $FILENAME_PREFIX.txt
-ftp -n latestbuilds.service.couchbase.com <<END_SCRIPT
-quote USER $USER
-quote PASS $PASS
-binary
-cd builds/latestbuilds/couchbase-lite-core
-put $FILENAME_PREFIX.txt
-quit
-END_SCRIPT
+scp $FILENAME_PREFIX.txt $USER@latestbuilds.service.couchbase.com:/data/builds/latestbuilds/couchbase-lite-core
 rm $FILENAME_PREFIX.txt
