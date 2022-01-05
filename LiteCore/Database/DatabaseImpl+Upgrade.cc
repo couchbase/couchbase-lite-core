9c88c0f90a73f1ac437b9d40b93d912212340bda
jianminzhao <76990468+jianminzhao@users.noreply.github.com>
2022-01-05 10:03:47 -0800

    CBL-2639: add a test case for upgrading db with index to v3. (#1342)
    * Revert "Upgrade db with indexes. CBL-2155 (#1236)"
    This reverts commit f1f28c2bad1d42e530d1e909cd75a0b0b2562216.
    
    Commit ea82fb755d5cac938812a38e1f6bafee694a58ae renders the above commit unnecessary.
    
    * CBL-2639: add a test case for upgrading db with index to v3.

--- LiteCore/Database/DatabaseImpl+Upgrade.cc
+++ LiteCore/Database/DatabaseImpl+Upgrade.cc
@@ -59,16 +59,9 @@ namespace litecore {
         if (_config.flags & (kC4DB_ReadOnly |kC4DB_NoUpgrade))
             error::_throw(error::CantUpgradeDatabase, "Document versioning needs upgrade");
 
-        // Drop indexes (then restore them afterwards.) This is faster than letting SQLite
-        // update indexes incrementally, when large numbers of docs are modified.
-        vector<IndexSpec> indexSpecs{defaultKeyStore().getIndexes()};
-        for (IndexSpec& spec: indexSpecs) {
-            defaultKeyStore().deleteIndex(spec.name, t);
-        }
-
         alloc_slice path = getPath();
-        LogTo(DBLog, "SCHEMA UPGRADE: Upgrading database <%.*s> from %s to %s ...",
-              SPLAT(path), kNameOfVersioning[curVersioning], kNameOfVersioning[newVersioning]);
+        LogTo(DBLog, "*** Upgrading stored documents from %s to %s ***",
+              kNameOfVersioning[curVersioning], kNameOfVersioning[newVersioning]);
         uint64_t docCount = 0;
 
         // Iterate over all documents:
@@ -97,15 +90,6 @@ namespace litecore {
 
             ++docCount;
         }
-        
-        // Restore all the indexes
-        if (!indexSpecs.empty()) {
-            LogTo(DBLog, "\tRebuilding %u indexes...", unsigned(indexSpecs.size()));
-            for (IndexSpec& spec: indexSpecs) {
-                defaultKeyStore().createIndex(spec, t);
-            }
-        }
-
         LogTo(DBLog, "\t%" PRIu64 " documents upgraded, now committing changes...", docCount);
     }
 
