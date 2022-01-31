aaa23c559b0bf9f78597214a4253ad2d0c5f4d83
Jens Alfke <jens@mooseyard.com>
2022-01-31 14:38:22 -0800

    Move RSA signing code from PublicKey to Signing
    Backs out the changes made to PublicKey.

--- Crypto/SignatureTest.cc
+++ Crypto/SignatureTest.cc
@@ -25,54 +25,60 @@ using namespace std;
 using namespace fleece;
 
 
-TEST_CASE("RSA Signatures", "[Signatures]") {
+TEST_CASE("Signatures", "[Signatures]") {
     static constexpr slice kDataToSign = "The only thing we learn from history"
                                          " is that people do not learn from history. --Hegel";
-    Retained<PrivateKey> key = PrivateKey::generateTemporaryRSA(2048);
-    alloc_slice signature = key->sign(kDataToSign);
+
+    const char *alg = GENERATE(kRSAAlgorithmName, kEd25519AlgorithmName);
+    cerr << "\t---- " << alg << endl;
+    
+    auto signingKey = SigningKey::generate(alg);
+    alloc_slice signature = signingKey->sign(kDataToSign);
     cout << "Signature is " << signature.size << " bytes: " << base64::encode(signature) << endl;
 
     // Verify:
-    CHECK(key->publicKey()->verifySignature(kDataToSign, signature));
+    auto verifyingKey = signingKey->verifyingKey();
+    CHECK(verifyingKey->verifySignature(kDataToSign, signature));
 
     // Verification fails with wrong public key:
-    auto key2 = PrivateKey::generateTemporaryRSA(2048);
-    CHECK(!key2->publicKey()->verifySignature(kDataToSign, signature));
+    auto key2 = SigningKey::generate(kRSAAlgorithmName);
+    CHECK(!key2->verifyingKey()->verifySignature(kDataToSign, signature));
 
     // Verification fails with incorrect digest:
     auto badDigest = SHA256(kDataToSign);
     ((uint8_t*)&badDigest)[10]++;
-    CHECK(!key->publicKey()->verifySignature(badDigest, signature));
+    CHECK(!verifyingKey->verifySignature(badDigest, signature));
 
     // Verification fails with altered signature:
-    ((uint8_t&)signature[100])++;
-    CHECK(!key->publicKey()->verifySignature(kDataToSign, signature));
+    ((uint8_t&)signature[30])++;
+    CHECK(!verifyingKey->verifySignature(kDataToSign, signature));
 }
 
 
 TEST_CASE("Signed Document", "[Signatures]") {
+    const char *algorithm = GENERATE(kRSAAlgorithmName, kEd25519AlgorithmName);
     bool embedKey = GENERATE(false, true);
-    cout << "---- Embed key in signature = " << embedKey << endl;
+    cerr << "\t---- " << algorithm << "; embed key in signature = " << embedKey << endl;
 
     // Create a signed doc and convert to JSON:
     alloc_slice publicKeyData;
     string json;
     {
-        auto priv = Ed25519SigningKey::generate();
-        auto pub  = priv.publicKey();
-        publicKeyData = pub.data();
+        auto priv = SigningKey::generate(algorithm);
+        auto pub  = priv->verifyingKey();
+        publicKeyData = pub->data();
 
         MutableDict doc = MutableDict::newDict();
         doc["name"] = "Oliver Bolliver Butz";
         doc["age"]  = 6;
         cout << "Document: " << doc.toJSONString() << endl;
 
-        MutableDict sig = makeSignature(doc, priv, 5 /*minutes*/, embedKey);
+        MutableDict sig = makeSignature(doc, *priv, 5 /*minutes*/, embedKey);
         REQUIRE(sig);
         string sigJson = sig.toJSONString();
         cout << "Signature, " << sigJson.size() << " bytes: " << sigJson << endl;
 
-        CHECK(verifySignature(doc, sig, &pub) == VerifyResult::Valid);
+        CHECK(verifySignature(doc, sig, pub.get()) == VerifyResult::Valid);
 
         doc["(sig)"] = sig;             // <-- add signature to doc, in "(sig)" property
         json = doc.toJSONString();
@@ -86,13 +92,13 @@ TEST_CASE("Signed Document", "[Signatures]") {
         Dict sig = doc["(sig)"].asDict();
         REQUIRE(sig);
 
-        auto parsedKey = getSignaturePublicKey(sig, "Ed25519");
+        auto parsedKey = getSignaturePublicKey(sig, algorithm);
         if (embedKey) {
             REQUIRE(parsedKey);
             CHECK(parsedKey->data() == publicKeyData);
         } else {
             CHECK(!parsedKey);
-            parsedKey = make_unique<Ed25519VerifyingKey>(publicKeyData);
+            parsedKey = VerifyingKey::instantiate(publicKeyData, algorithm);
         }
 
         MutableDict unsignedDoc = doc.mutableCopy();
--- Crypto/Signing.cc
+++ Crypto/Signing.cc
@@ -19,11 +19,28 @@
 #include "Signing.hh"
 #include "Error.hh"
 #include "SecureRandomize.hh"
+#include "mbedUtils.hh"
 #include "monocypher.h"
 #include "monocypher-ed25519.h"
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdocumentation-deprecated-sync"
+#include "mbedtls/pk.h"
+#pragma clang diagnostic pop
+
 namespace litecore::crypto {
     using namespace std;
+
+
+    std::unique_ptr<SigningKey> SigningKey::generate(const char *algorithm) {
+        if (0 == strcmp(algorithm, kRSAAlgorithmName)) {
+            return make_unique<RSASigningKey>(PrivateKey::generateTemporaryRSA(2048));
+        } else if (0 == strcmp(algorithm, kEd25519AlgorithmName)) {
+            return make_unique<Ed25519SigningKey>();
+        } else {
+            error::_throw(error::CryptoError, "Unknown signature algorithm '%s'", algorithm);
+        }
+    }
 
 
     unique_ptr<VerifyingKey> VerifyingKey::instantiate(slice data, const char *algorithm) {
@@ -37,9 +54,54 @@ namespace litecore::crypto {
     }
 
 
+#pragma mark - RSA:
+
+
+    static int rngFunction(void *ctx, unsigned char *dst, size_t size) {
+        SecureRandomize({dst, size});
+        return 0;
+    }
+
+
+    alloc_slice RSASigningKey::sign(slice data) const {
+        SHA256 inputDigest(data);
+        alloc_slice signature(MBEDTLS_PK_SIGNATURE_MAX_SIZE);
+        size_t sigLen = 0;
+        TRY(mbedtls_pk_sign(_key->context(),
+                            MBEDTLS_MD_SHA256,  // declares that input is a SHA256 digest.
+                            (const uint8_t*)inputDigest.asSlice().buf, inputDigest.asSlice().size,
+                            (uint8_t*)signature.buf, &sigLen,
+                            rngFunction, nullptr));
+        signature.shorten(sigLen);
+        return signature;
+    }
+
+    
+    unique_ptr<VerifyingKey> RSASigningKey::verifyingKey() const {
+        return make_unique<RSAVerifyingKey>(_key->publicKey());
+    }
+
+
+    bool RSAVerifyingKey::verifySignature(slice data, slice signature) const {
+        SHA256 inputDigest(data);
+        int result = mbedtls_pk_verify(_key->context(),
+                                       MBEDTLS_MD_SHA256, // declares that input is a SHA256 digest.
+                                       (const uint8_t*)inputDigest.asSlice().buf,
+                                       inputDigest.asSlice().size,
+                                       (const uint8_t*)signature.buf, signature.size);
+        if (result == MBEDTLS_ERR_RSA_VERIFY_FAILED)
+            return false;
+        TRY(result);        // other error codes throw exceptions
+        return true;
+    }
+
+
+#pragma mark - Ed25519:
+
+
     Ed25519Base::Ed25519Base(slice bytes) {
         if (bytes.size != sizeof(_bytes))
-            error::_throw(error::CryptoError, "Invalid data for Ed25519 key");
+            error::_throw(error::CryptoError, "Invalid data size for Ed25519 key");
         bytes.copyTo(_bytes.data());
     }
 
@@ -56,13 +118,20 @@ namespace litecore::crypto {
     }
 
 
+    unique_ptr<VerifyingKey> Ed25519SigningKey::verifyingKey() const {
+        unique_ptr<Ed25519VerifyingKey> pub(new Ed25519VerifyingKey());
+        crypto_ed25519_public_key(pub->_bytes.data(), _bytes.data());
+        return pub;
+    }
+
+
     alloc_slice Ed25519SigningKey::verifyingKeyData() const {
         return publicKey().data();
     }
 
 
     alloc_slice Ed25519SigningKey::sign(slice data) const {
-        alloc_slice signature(64);
+        alloc_slice signature(kSignatureSize);
         crypto_ed25519_sign((uint8_t*)signature.buf,
                             (const uint8_t*)_bytes.data(), nullptr,
                             (const uint8_t*)data.buf, data.size);
@@ -76,7 +145,8 @@ namespace litecore::crypto {
     }
 
     bool Ed25519VerifyingKey::verifySignature(slice inputData, slice signature) const {
-        return 0 == crypto_ed25519_check((const uint8_t*)signature.buf,
+        return signature.size == kSignatureSize
+            && 0 == crypto_ed25519_check((const uint8_t*)signature.buf,
                                          (const uint8_t*)_bytes.data(),
                                          (const uint8_t*)inputData.buf,
                                          inputData.size);
