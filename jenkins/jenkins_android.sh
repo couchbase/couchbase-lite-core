f6be00efd4fde4be1451070af33d9ff855bfdb32
Jim Borden <jim.borden@couchbase.com>
2022-04-09 08:47:26 +0900

    Update Android NDK to 23b (#1429)
    In the process it was determined that we need to move to a stock CMake and Ninja, so fetch those from cbdeps

--- jenkins/jenkins_android.sh
+++ jenkins/jenkins_android.sh
@@ -11,8 +11,9 @@
 set -e
 shopt -s extglob dotglob
 
-CMAKE_VER="3.18.1"
-NDK_VER="21.2.6472646"
+NDK_VER="23.1.7779620"
+CMAKE_VER="3.23.0"
+NINJA_VER="1.10.2"
 
 function usage() {
     echo "Usage: $0 <sdk path>"
@@ -30,47 +31,62 @@ if [ -z "$SDK_HOME" ]; then
 fi
 
 SDK_MGR="${SDK_HOME}/cmdline-tools/latest/bin/sdkmanager"
-CMAKE_PATH="${SDK_HOME}/cmake/${CMAKE_VER}/bin"
 yes | ${SDK_MGR} --licenses > /dev/null 2>&1
-${SDK_MGR} --install "cmake;${CMAKE_VER}"
 ${SDK_MGR} --install "ndk;${NDK_VER}"
+
+mkdir -p .tools
+if [ ! -f .tools/cbdep ]; then 
+    curl -o .tools/cbdep http://downloads.build.couchbase.com/cbdep/cbdep.$(uname -s | tr "[:upper:]" "[:lower:]")-$(uname -m)
+    chmod +x .tools/cbdep
+fi 
+
+CMAKE="$(pwd)/.tools/cmake-${CMAKE_VER}/bin/cmake"
+NINJA="$(pwd)/.tools/ninja-${NINJA_VER}/bin/ninja"
+if [ ! -f ${CMAKE} ]; then
+    .tools/cbdep install -d .tools cmake ${CMAKE_VER}
+fi
+
+if [ ! -f ${NINJA} ]; then
+    .tools/cbdep install -d .tools ninja ${NINJA_VER}
+fi
 
 mkdir "couchbase-lite-core"
 git submodule update --init --recursive
-mv !(couchbase-lite-core) couchbase-lite-core
+mv !(couchbase-lite-core|.tools|.|..) couchbase-lite-core
 
 # Sometimes a PR depends on a PR in the EE repo as well.  This needs to be convention based, so if there is a branch with the name PR-###
 # (with the GH PR number) in the EE repo then use that, otherwise use the name of the target branch (master, release/XXX etc)
 git clone ssh://git@github.com/couchbase/couchbase-lite-core-EE --branch $BRANCH_NAME --recursive --depth 1 couchbase-lite-core-EE || \
     git clone ssh://git@github.com/couchbase/couchbase-lite-core-EE --branch $CHANGE_TARGET --recursive --depth 1 couchbase-lite-core-EE 
 
+
 # Just build one of each 32 and 64-bit for validation
 mkdir -p "couchbase-lite-core/build_cmake/android/armeabi-v7a"
 pushd "couchbase-lite-core/build_cmake/android/armeabi-v7a"
-${CMAKE_PATH}/cmake \
+${CMAKE} \
     -G Ninja \
     -DCMAKE_TOOLCHAIN_FILE="${SDK_HOME}/ndk/${NDK_VER}/build/cmake/android.toolchain.cmake" \
-    -DCMAKE_MAKE_PROGRAM="${CMAKE_PATH}/ninja" \
-    -DANDROID_NATIVE_API_LEVEL=19 \
+    -DANDROID_PLATFORM=19 \
     -DANDROID_ABI=armeabi-v7a \
     -DBUILD_ENTERPRISE=ON \
     -DCMAKE_BUILD_TYPE=MinSizeRel \
+    -DCMAKE_MAKE_PROGRAM="$NINJA" \
     ../../..
 
-${CMAKE_PATH}/ninja
+${NINJA} LiteCore
 popd
 
 mkdir -p "couchbase-lite-core/build_cmake/android/arm64-v8a"
 pushd "couchbase-lite-core/build_cmake/android/arm64-v8a"
-${CMAKE_PATH}/cmake \
+${CMAKE} \
     -G Ninja \
     -DCMAKE_TOOLCHAIN_FILE="${SDK_HOME}/ndk/${NDK_VER}/build/cmake/android.toolchain.cmake" \
-    -DCMAKE_MAKE_PROGRAM="${CMAKE_PATH}/ninja" \
-    -DANDROID_NATIVE_API_LEVEL=21 \
+    -DANDROID_PLATFORM=21 \
     -DANDROID_ABI=arm64-v8a \
     -DBUILD_ENTERPRISE=ON \
     -DCMAKE_BUILD_TYPE=MinSizeRel \
+    -DCMAKE_MAKE_PROGRAM="$NINJA" \
     ../../..
 
-${CMAKE_PATH}/ninja
+${NINJA} LiteCore
 popd
