1a8017fc973a07193fc78db8b101febcdeb2b077
Jim Borden <jim.borden@couchbase.com>
2022-08-05 12:49:03 -0400

    Add last remaining files from couchbase/build

---
+++ jenkins/manifest.cmake
@@ -0,0 +1,7 @@
+#
+# Keep the list sorted alphabetically, and the platforms alphabetically.
+# The syntax is:
+#
+# DECLARE_DEP (name VERSION version-revision PLATFORMS platform1 platform2)
+#
+DECLARE_DEP (icu4c VERSION 54.1-cb10 PLATFORMS centos7)
