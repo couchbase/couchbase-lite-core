719fab4cd19795901998f101b88c82edebfc6650
Jianmin Zhao <jianmin.zhao@couchbase.com>
2022-08-29 10:36:22 -0700

    Update the Doxygen docs

--- docs/C/html/search/classes_1.js
+++ docs/C/html/search/classes_1.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['flarrayiterator_1184',['FLArrayIterator',['../struct_f_l_array_iterator.html',1,'']]],
-  ['fldictiterator_1185',['FLDictIterator',['../struct_f_l_dict_iterator.html',1,'']]],
-  ['fldictkey_1186',['FLDictKey',['../struct_f_l_dict_key.html',1,'']]],
-  ['flpathcomponent_1187',['FLPathComponent',['../struct_f_l_path_component.html',1,'']]],
-  ['flslice_1188',['FLSlice',['../struct_f_l_slice.html',1,'']]],
-  ['flsliceresult_1189',['FLSliceResult',['../struct_f_l_slice_result.html',1,'']]]
+  ['flarrayiterator_1181',['FLArrayIterator',['../struct_f_l_array_iterator.html',1,'']]],
+  ['fldictiterator_1182',['FLDictIterator',['../struct_f_l_dict_iterator.html',1,'']]],
+  ['fldictkey_1183',['FLDictKey',['../struct_f_l_dict_key.html',1,'']]],
+  ['flpathcomponent_1184',['FLPathComponent',['../struct_f_l_path_component.html',1,'']]],
+  ['flslice_1185',['FLSlice',['../struct_f_l_slice.html',1,'']]],
+  ['flsliceresult_1186',['FLSliceResult',['../struct_f_l_slice_result.html',1,'']]]
 ];
--- docs/C/html/search/defines_4.js
+++ docs/C/html/search/defines_4.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['litecore_5fapi_5fversion_2196',['LITECORE_API_VERSION',['../c4_base_8h.html#a906bded72f22a59134ce4d2db77a0c53',1,'c4Base.h']]],
-  ['litecore_5fcpp_5fapi_2197',['LITECORE_CPP_API',['../c4_document_struct_8h.html#a517d6fd1817ca661e3ca335c16839913',1,'c4DocumentStruct.h']]],
-  ['litecore_5fversion_2198',['LITECORE_VERSION',['../c4_base_8h.html#abd4a83720173b87bd04686206a65d6b4',1,'c4Base.h']]]
+  ['litecore_5fapi_5fversion_2190',['LITECORE_API_VERSION',['../c4_base_8h.html#a906bded72f22a59134ce4d2db77a0c53',1,'c4Base.h']]],
+  ['litecore_5fcpp_5fapi_2191',['LITECORE_CPP_API',['../c4_document_struct_8h.html#a517d6fd1817ca661e3ca335c16839913',1,'c4DocumentStruct.h']]],
+  ['litecore_5fversion_2192',['LITECORE_VERSION',['../c4_base_8h.html#abd4a83720173b87bd04686206a65d6b4',1,'c4Base.h']]]
 ];
