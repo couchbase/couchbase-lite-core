57f1083afa0c4342766b61c94373e4d9333c7f84
Callum Birks <callumbirks@protonmail.com>
2022-09-09 16:26:09 +0100

    Change `and` to `&&` to try and fix compiler errors on certain builds

--- Networking/HTTP/HTTPLogic.cc
+++ Networking/HTTP/HTTPLogic.cc
@@ -111,7 +111,7 @@ namespace litecore { namespace net {
         rq << " HTTP/1.1\r\n"
               "Host: " << string(slice(_address.hostname));
         // Omit port from header if using standard ports
-        if(_address.port != 80 and _address.port != 443) {
+        if(_address.port != 80 && _address.port != 443) {
             rq << ':' << _address.port;
         }
 
