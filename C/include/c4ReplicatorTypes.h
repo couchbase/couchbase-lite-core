42743b94616274e45229975cc671747b461af5a3
Pasin Suriyentrakorn <pasin@couchbase.com>
2022-12-14 18:26:00 -0800

    CBL-4024 : Update BuiltInWebSocket to do preemptive auth by default
    * Updated BuiltInWebSocket and HTTPLogic to do preemptive auth by default with an option for doing challenge based auth.
    
    * This change is required by CBL-C to do basic auth preemptively to avoid the issue that users can get authenticated as a GUEST user when they don't specify credentials.

--- C/include/c4ReplicatorTypes.h
+++ C/include/c4ReplicatorTypes.h
@@ -240,9 +240,11 @@ C4API_BEGIN_DECLS
     #define kC4ReplicatorCompressionLevel       "BLIPCompressionLevel" ///< Data compression level, 0..9
 
     // [1]: Auth dictionary keys:
-    #define kC4ReplicatorAuthType       "type"           ///< Auth type; see [2] (string)
-    #define kC4ReplicatorAuthUserName   "username"       ///< User name for basic auth (string)
-    #define kC4ReplicatorAuthPassword   "password"       ///< Password for basic auth (string)
+    #define kC4ReplicatorAuthType                   "type"           ///< Auth type; see [2] (string)
+    #define kC4ReplicatorAuthUserName               "username"       ///< User name for basic auth (string)
+    #define kC4ReplicatorAuthPassword               "password"       ///< Password for basic auth (string)
+    #define kC4ReplicatorAuthEnableChallengeAuth    "challengeAuth"  ///< Use challenge auth instead of preemptive auth for basic auth, default is false (bool); Implemented by BuiltInWebSocket.
+///<
     #define kC4ReplicatorAuthClientCert "clientCert"     ///< TLS client certificate (value platform-dependent)
     #define kC4ReplicatorAuthClientCertKey "clientCertKey" ///< Client cert's private key (data)
     #define kC4ReplicatorAuthToken      "token"          ///< Session cookie or auth token (string)
