e66fbbc708f428c809efeb96abe86ba898bc5dac
Callum Birks <callumbirks@protonmail.com>
2023-03-09 13:27:16 +0000

    Add scheme to Xcode for running Clang-Format.

--- build_cmake/scripts/run-clang-format.sh
+++ build_cmake/scripts/run-clang-format.sh
@@ -2,8 +2,18 @@
 # Runs clang-format on each of the below LiteCore directories
 # Must be ran from Core root directory ('couchbase-lite-core/')
 # Formatting is based off the .clang-format file in the root of Core
+# clang-format should either be installed by homebrew, or available to /bin/sh's path
+PATH=$PATH:/opt/homebrew/bin
 
 DIRS=("C" "Crypto" "LiteCore" "Networking" "Replicator" "REST")
+
+CURDIR=$(pwd)
+
+# Move back to Core's root if this is being ran from Xcode
+if [[ ${CURDIR} == */Xcode ]]; then
+    cd ..
+fi
+
 
 for dir in ${DIRS[@]}; do
   find $dir/. -iname '*.hh' -o -iname '*.cc' -o -iname '*.h' -o -iname '*.c' | xargs clang-format -i
