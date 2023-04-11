665204b2e60c1a368518d2cefbce9e0826c6fe3d
callumbirks <73551885+callumbirks@users.noreply.github.com>
2023-04-11 11:09:48 +0100

    CBL-4379: CertHelper leaves persistent certs in the keychain

--- REST/tests/CertHelper.hh
+++ REST/tests/CertHelper.hh
@@ -12,6 +12,8 @@
 
 #pragma once
 #include "c4Certificate.h"
+#include "c4Certificate.hh"
+#include "Certificate.hh"
 #include "c4Private.h"
 #include "PublicKey.hh"  // just for PERSISTENT_PRIVATE_KEY_AVAILABLE
 
@@ -34,6 +36,13 @@ class CertHelper {
     CertHelper()
         : temporaryServerIdentity(createIdentity(false, kC4CertUsage_TLSServer, "LiteCore Listener Test"))
         , temporaryClientIdentity(createIdentity(false, kC4CertUsage_TLSClient, "LiteCore Client Test")) {}
+
+#    ifdef PERSISTENT_PRIVATE_KEY_AVAILABLE
+    ~CertHelper() {
+        if ( _serverPersistentIdentity.key ) { _serverPersistentIdentity.key->removePersistent(); }
+        if ( _clientPersistentIdentity.key ) { _clientPersistentIdentity.key->removePersistent(); }
+    }
+#    endif
 
     Identity const temporaryServerIdentity;
 
