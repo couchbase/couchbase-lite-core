3dbae8ed08cae520ed41e66a10034982ab7d8229
Jianmin Zhao <jianmin.zhao@couchbase.com>
2023-06-28 11:13:15 -0700

    address review feedback.

--- C/include/c4Replicator.h
+++ C/include/c4Replicator.h
@@ -77,8 +77,7 @@ CBL_CORE_API C4Replicator* c4repl_newWithSocket(C4Database* db, C4Socket* openSo
 
 /** Tells a replicator to start. Ignored if it's not in the Stopped state.
         \note This function is thread-safe.
-        \note This function may perform action in the calling thread that requires the transaction from the database
-              that was passed to the replicator instance, \p repl.
+        \note Do not call this function while a transaction is open on the same database as it can deadlock when \p repl tries to acquire the database.
 
         @param repl  The C4Replicator instance.
         @param reset If true, the replicator will reset its checkpoint and start replication from the beginning
