c5b2acec2e6a0a0d83b89386d59613a6b17f74e5
Jens Alfke <jens@couchbase.com>
2024-06-06 13:32:43 -0700

    Use same vector-index options struct as vectorsearch repo
    Replaced IndexSpec::VectorOptions with vectorsearch::IndexSpec.

--- LiteCore/Query/SQLiteKeyStore+VectorIndex.cc
+++ LiteCore/Query/SQLiteKeyStore+VectorIndex.cc
@@ -22,6 +22,7 @@
 #    include "StringUtil.hh"
 #    include "Array.hh"
 #    include "Error.hh"
+#    include "SQLiteCpp/Statement.h"
 #    include "SQLiteCpp/Exception.h"
 #    include <sstream>
 
@@ -34,70 +35,26 @@ namespace litecore {
     // Vector search index for ML / predictive query, using the vectorsearch extension.
     // https://github.com/couchbaselabs/mobile-vector-search/blob/main/README_Extension.md
 
-    static constexpr const char* kMetricNames[] = {nullptr, "euclidean2", "cosine"};
-
-    /// Returns the SQL expression to create a vectorsearch virtual table.
-    static string createVectorSearchTableSQL(string_view vectorTableName, const IndexSpec& spec) {
-        stringstream stmt;
-        stmt << "CREATE VIRTUAL TABLE " << sqlIdentifier(vectorTableName) << " USING vectorsearch(";
-        Assert(spec.vectorOptions() != nullptr);
-        IndexSpec::VectorOptions const& options = *spec.vectorOptions();
-        stmt << "dimensions=" << options.dimensions << ',';
-        if ( options.metric != IndexSpec::VectorOptions::DefaultMetric ) {
-            stmt << "metric=" << kMetricNames[options.metric] << ',';
-        }
-        switch ( options.clustering.type ) {
-            case IndexSpec::VectorOptions::Flat:
-                stmt << "clustering=flat" << options.clustering.flat_centroids << ',';
-                break;
-            case IndexSpec::VectorOptions::Multi:
-                stmt << "clustering=multi" << options.clustering.multi_subquantizers << 'x'
-                     << options.clustering.multi_bits << ',';
-                break;
-            default:
-                error::_throw(error::InvalidParameter, "invalid vector clustering type");
-        }
-        switch ( options.encoding.type ) {
-            case IndexSpec::VectorOptions::DefaultEncoding:
-                break;
-            case IndexSpec::VectorOptions::NoEncoding:
-                stmt << "encoding=none,";
-                break;
-            case IndexSpec::VectorOptions::PQ:
-                stmt << "encoding=PQ" << options.encoding.pq_subquantizers << 'x' << options.encoding.bits << ',';
-                break;
-            case IndexSpec::VectorOptions::SQ:
-                stmt << "encoding=SQ" << options.encoding.bits << ',';
-                break;
-            default:
-                error::_throw(error::InvalidParameter, "invalid vector encoding type");
-        }
-        if ( options.numProbes > 0 ) stmt << "probes=" << options.numProbes << ',';
-        if ( options.maxTrainingSize > 0 ) stmt << "maxToTrain=" << options.maxTrainingSize << ',';
-        stmt << "minToTrain=" << options.minTrainingSize;
-        if ( QueryLog.effectiveLevel() <= LogLevel::Verbose )
-            stmt << ",verbose";  // Enable vectorsearch verbose logging (via printf, for now)
-        stmt << ")";
-        return stmt.str();
-    }
-
     // Creates a vector-similarity index.
     bool SQLiteKeyStore::createVectorIndex(const IndexSpec& spec) {
         auto vectorTableName = db().auxiliaryTableName(tableName(), KeyStore::kVectorSeparator, spec.name);
+        auto vectorOptions   = spec.vectorOptions();
+        Assert(vectorOptions);
 
         // Generate a SQL expression to get the vector:
         QueryParser qp(db(), collectionName(), tableName());
         qp.setBodyColumnName("new.body");
         string vectorExpr;
         if ( auto what = spec.what(); what && what->count() == 1 )
-            vectorExpr = qp.vectorToIndexExpressionSQL(what->get(0), spec.vectorOptions()->dimensions);
+            vectorExpr = qp.vectorToIndexExpressionSQL(what->get(0), vectorOptions->dimensions);
         else
             error::_throw(error::Unimplemented, "Vector index doesn't support multiple properties");
 
         // Create the virtual table:
         try {
-            if ( !db().createIndex(spec, this, vectorTableName, createVectorSearchTableSQL(vectorTableName, spec)) )
-                return false;
+            string sql = CONCAT("CREATE VIRTUAL TABLE " << sqlIdentifier(vectorTableName) << " USING vectorsearch("
+                                                        << *vectorOptions << ")");
+            if ( !db().createIndex(spec, this, vectorTableName, sql) ) return false;
         } catch ( SQLite::Exception const& x ) {
             string_view what(x.what());
             if ( hasPrefix(what, "no such module") ) {
@@ -123,7 +80,7 @@ namespace litecore {
         createTrigger(vectorTableName, "preupdate", "BEFORE UPDATE OF body", whereOldSQL, deleteOldSQL);
         createTrigger(vectorTableName, "del", "AFTER DELETE", whereOldSQL, deleteOldSQL);
 
-        bool lazy = spec.vectorOptions()->lazy;
+        bool lazy = vectorOptions->lazyEmbedding;
         if ( lazy ) {
             // Lazy index: Mark as lazy by initializing lastSeq. Vectors will not be computed
             // automatically; app updates them via the LazyIndex class.
@@ -165,58 +122,19 @@ namespace litecore {
         return "";  // no index found
     }
 
-    static inline unsigned asUInt(string_view sv) {
-        string str(sv);
-        return unsigned(strtoul(str.c_str(), nullptr, 10));
-    }
-
     // The opposite of createVectorSearchTableSQL
     optional<IndexSpec::VectorOptions> SQLiteKeyStore::parseVectorSearchTableSQL(string_view sql) {
-        optional<IndexSpec::VectorOptions> opts;
         // Find the virtual-table arguments in the CREATE TABLE statement:
         auto start = sql.find("vectorsearch(");
-        if ( start == string::npos ) return opts;
+        if ( start == string::npos ) return nullopt;
         start += strlen("vectorsearch(");
         auto end = sql.find(')', start);
-        if ( end == string::npos ) return opts;
+        if ( end == string::npos ) return nullopt;
 
         // Parse each comma-delimited key-value pair:
-        string_view args(&sql[start], end - start);
-        opts.emplace(0);
-        split(args, ",", [&](string_view key) {
-            string_view value;
-            if ( auto eq = key.find('='); eq != string::npos ) {
-                value = key.substr(eq + 1);
-                key   = key.substr(0, eq);
-                if ( value.empty() || key.empty() ) return;
-            }
-            if ( key == "dimensions" ) {
-                opts->dimensions = asUInt(value);
-            } else if ( key == "metric" ) {
-                if ( value == "euclidean2" ) opts->metric = IndexSpec::VectorOptions::Euclidean;
-                else if ( value == "cosine" )
-                    opts->metric = IndexSpec::VectorOptions::Cosine;
-            } else if ( key == "minToTrain" ) {
-                opts->minTrainingSize = asUInt(value);
-            } else if ( key == "maxToTrain" ) {
-                opts->maxTrainingSize = asUInt(value);
-            } else if ( key == "probes" ) {
-                opts->numProbes = asUInt(value);
-            } else if ( key == "lazyindex" ) {
-                opts->lazy = (value != "false" && value != "0");
-            } else if ( key == "clustering" ) {
-                if ( hasPrefix(value, "multi") ) opts->clustering = {IndexSpec::VectorOptions::Multi};
-                //TODO: Parse centroid count & other params; see vectorsearch::IndexSpec::setParam()
-            } else if ( key == "encoding" ) {
-                if ( value == "none" ) opts->encoding = {IndexSpec::VectorOptions::NoEncoding};
-                else if ( hasPrefix(value, "PQ") ) {
-                    opts->encoding = {IndexSpec::VectorOptions::PQ};
-                } else if ( hasPrefix(value, "SQ") ) {
-                    opts->encoding = {IndexSpec::VectorOptions::SQ};
-                }
-                //TODO: Parse encoding params; see vectorsearch::IndexSpec::setParam()
-            }
-        });
+        string_view              args(&sql[start], end - start);
+        IndexSpec::VectorOptions opts;
+        split(args, ",", [&](string_view arg) { (void)opts.readArg(arg); });
         return opts;
     }
 
