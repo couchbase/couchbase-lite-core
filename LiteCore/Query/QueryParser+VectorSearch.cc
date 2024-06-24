ab37982e2508a7b09ab8cb2f3b3801f8a559f1a8
Jens Alfke <jens@couchbase.com>
2024-06-24 12:30:08 -0700

    Removed limit param from vector_match()
    vector_match() no longer takes an optional 3rd parameter.
    The limit should now be passed as the overall LIMIT of the SELECT.
    
    Also switched to using MATCH instead of LIKE in the generated SQL,
    because I found MATCH is slightly more efficient.

--- LiteCore/Query/QueryParser+VectorSearch.cc
+++ LiteCore/Query/QueryParser+VectorSearch.cc
@@ -29,54 +29,40 @@ using namespace litecore::qp;
 
 namespace litecore {
 
-#    ifndef REQUIRE_LIMIT
     static constexpr unsigned kDefaultMaxResults = 3;
-#    endif
-    static constexpr unsigned kMaxMaxResults = 10000;
+    static constexpr unsigned kMaxMaxResults     = 10000;
 
     // Scans the entire query for vector_match() calls, and adds join tables for ones that are
     // indexed.
     void QueryParser::addVectorSearchJoins(const Dict* select) {
-        auto whereClause = getCaseInsensitive(select, "WHERE");
         findNodes(select, kVectorMatchFnNameWithParens, 1, [&](const Array* matchExpr) {
-            // Arguments to vector_match are index name, target vector, and optional max-results.
-            string         tableName = FTSTableName(matchExpr->get(1), true).first;
-            const Value*   limitVal  = matchExpr->get(3);
-            indexJoinInfo* info      = indexJoinTable(tableName, "vector");
-            if ( matchExpr == whereClause || limitVal ) {
+            // Arguments to vector_match are index name and target vector.
+            string         tableName         = FTSTableName(matchExpr->get(1), true).first;
+            auto           targetVectorParam = matchExpr->get(2);
+            indexJoinInfo* info              = indexJoinTable(tableName, "vector");
+            if ( matchExpr == getCaseInsensitive(select, "WHERE") ) {
                 // If vector_match is the entire WHERE clause, this is a simple non-hybrid query.
                 // This is implemented by a nested SELECT that finds the nearest vectors in
                 // the entire collection. Isolating this in a nested SELECT ensures SQLite doesn't
                 // see the outer JOIN against the collection; if it did, the vectorsearch extension's
                 // planner would see a constraint against `rowid` and interpret it as a hybrid search.
                 // https://github.com/couchbaselabs/mobile-vector-search/blob/main/docs/Extension.md
-                auto targetVectorParam = matchExpr->get(2);
 
                 // Figure out the limit to use in the vector query:
-                const char* limitName = "3rd max_results argument";
-                if ( !limitVal ) {
-                    // If no limit param is given, check the LIMIT on the SELECT itself:
-                    limitVal  = getCaseInsensitive(select, "LIMIT");
-                    limitName = "LIMIT";
-                }
                 int64_t maxResults;
-                if ( limitVal ) {
+                if ( auto limitVal = getCaseInsensitive(select, "LIMIT") ) {
                     maxResults = limitVal->asInt();
-                    require(limitVal->isInteger() && maxResults > 0, "vector_match()'s %s must be a positive integer",
-                            limitName);
-                    require(maxResults <= kMaxMaxResults, "vector_match()'s %s must not exceed %u", limitName,
+                    require(limitVal->isInteger() && maxResults > 0,
+                            "LIMIT must be a positive integer when using vector_match()");
+                    require(maxResults <= kMaxMaxResults, "LIMIT must not exceed %u when using vector_match()",
                             kMaxMaxResults);
                 } else {
-#    ifdef REQUIRE_LIMIT
-                    fail("vector_match() requires a 3rd max_results argument or a LIMIT");
-#    else
                     maxResults = kDefaultMaxResults;
-#    endif
                 }
 
                 // Register a callback to write the nested SELECT in place of a table name:
                 info->writeTableSQL = [=] {
-                    _sql << "(SELECT rowid, distance FROM \"" << tableName << "\" WHERE vector LIKE encode_vector(";
+                    _sql << "(SELECT rowid, distance FROM \"" << tableName << "\" WHERE vector MATCH encode_vector(";
                     parseNode(targetVectorParam);
                     _sql << ") LIMIT " << maxResults << ")";
                 };
@@ -87,20 +73,20 @@ namespace litecore {
     // Writes a `vector_match()` expression.
     void QueryParser::writeVectorMatchFn(ArrayIterator& params) {
         requireTopLevelConjunction("VECTOR_MATCH");
-        auto         parentCtx = _context.rbegin() + 1;
-        auto         parentOp  = (*parentCtx)->op;
-        const Value* limitVal  = params[2];
-        if ( parentOp == "SELECT"_sl || parentOp == nullslice || limitVal ) {
+        auto parentCtx = _context.rbegin() + 1;
+        auto parentOp  = (*parentCtx)->op;
+        if ( parentOp == "SELECT"_sl || parentOp == nullslice ) {
             // In a simple query the work of `vector_match` is done by the JOIN, which limits the results to the
             // rowids produced by the nested query of the vector table.
             // Since there's nothing to do here, replace the call with a `true`.
             _sql << "true";
         } else {
             // In a hybrid query we do write the LIKE test at the point of the match call:
-            string        tableName         = FTSTableName(params[0], true).first;
-            const string& alias             = indexJoinTableAlias(tableName);
-            auto          targetVectorParam = params[1];
-            _sql << sqlIdentifier(alias) << ".vector LIKE encode_vector(";
+            string        tableName = FTSTableName(params[0], true).first;
+            const string& alias     = indexJoinTableAlias(tableName, "vector");
+            Assert(!alias.empty());
+            auto targetVectorParam = params[1];
+            _sql << sqlIdentifier(alias) << ".vector MATCH encode_vector(";
             parseNode(targetVectorParam);
             _sql << ")";
         }
@@ -109,7 +95,7 @@ namespace litecore {
     // Writes the SQL translation of the `vector_distance(...)` call.
     void QueryParser::writeVectorDistanceFn(ArrayIterator& params) {
         string tableName = FTSTableName(params[0], true).first;
-        _sql << indexJoinTableAlias(tableName) << ".distance";
+        _sql << indexJoinTableAlias(tableName, "vector") << ".distance";
     }
 
     // Given the expression to index from a vector index spec, returns the SQL of a
--- LiteCore/tests/LazyVectorQueryTest.cc
+++ LiteCore/tests/LazyVectorQueryTest.cc
@@ -50,9 +50,10 @@ class LazyVectorQueryTest : public VectorQueryTest {
 
         string queryStr = R"(
          ['SELECT', {
-            WHERE:    ['VECTOR_MATCH()', 'factorsindex', ['$target'], 5],
+            WHERE:    ['VECTOR_MATCH()', 'factorsindex', ['$target']],
             WHAT:     [ ['._id'], ['AS', ['VECTOR_DISTANCE()', 'factorsindex'], 'distance'] ],
             ORDER_BY: [ ['.distance'] ],
+            LIMIT:    5
          }] )";
         _query          = store->compileQuery(json5(queryStr), QueryLanguage::kJSON);
         REQUIRE(_query != nullptr);
