bc1664a63427db3d202b10e0491c9061f97ad3fa
Jens Alfke <jens@couchbase.com>
2024-06-25 19:30:14 -0700

    CBL-5925: Regression from table migration during getIndex (#2082)
    If the index table is present at update time, make a schema change in order to modify the index table so that by the time it is used it is either present or not present in the correct definition (but it will not be present and invalid)

--- LiteCore/Storage/SQLiteDataFile.hh
+++ LiteCore/Storage/SQLiteDataFile.hh
@@ -167,8 +167,9 @@ namespace litecore {
 
             WithNewDocs = 400,  // New document/revision storage (CBL 3.0)
 
-            WithDeletedTable = 500,  // Added 'deleted' KeyStore for deleted docs (CBL 3.0?)
-            MaxReadable      = 599,  // Cannot open versions newer than this
+            WithDeletedTable   = 500,  // Added 'deleted' KeyStore for deleted docs (CBL 3.0?)
+            WithIndexesLastSeq = 501,  // Added 'lastSeq' column to 'indexes' table (CBL 3.2)
+            MaxReadable        = 599,  // Cannot open versions newer than this
 
             Current = WithDeletedTable
         };
