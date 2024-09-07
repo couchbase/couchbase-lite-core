01d195bc506f094546405f962b5eff3b473661a3
jianminzhao <76990468+jianminzhao@users.noreply.github.com>
2024-09-06 17:57:45 -0700

    CBL-6189: Annotation of thread-safety, filling in the missing ones after the first pass. (#2129)

--- docs/C/html/search/groups_12.js
+++ docs/C/html/search/groups_12.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['value_20slots_0',['Value Slots',['../group___slots.html',1,'']]],
-  ['values_1',['Values',['../group___f_l_value.html',1,'Fleece Values'],['../group___mutable.html',1,'Mutable Values']]]
+  ['value_20builder_0',['Fleece Formatted Value Builder',['../group__builder.html',1,'']]],
+  ['value_20slots_1',['Value Slots',['../group___slots.html',1,'']]],
+  ['values_2',['Values',['../group___f_l_value.html',1,'Fleece Values'],['../group___mutable.html',1,'Mutable Values']]]
 ];
