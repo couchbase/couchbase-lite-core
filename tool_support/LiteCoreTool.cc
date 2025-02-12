e9809f66cf1c81261c834fe6bff58aeb6d474fca
Jens Alfke <jens@couchbase.com>
2025-02-12 15:31:40 -0800

    tool_support needs to be clang-formatted

--- tool_support/LiteCoreTool.cc
+++ tool_support/LiteCoreTool.cc
@@ -18,68 +18,54 @@
 
 #include "LiteCoreTool.hh"
 #include "FilePath.hh"
-#include "StringUtil.hh"            // for digittoint(), on non-BSD-like systems
+#include "StringUtil.hh"  // for digittoint(), on non-BSD-like systems
 #include <fleece/RefCounted.hh>
 
 using namespace std;
 using namespace litecore;
 
-
-LiteCoreTool::LiteCoreTool(const char* name)
-:Tool(name)
-{
-    if (const char* extPath = getenv("CBLITE_EXTENSION_PATH"))
-        c4_setExtensionPath(slice(extPath));
+LiteCoreTool::LiteCoreTool(const char* name) : Tool(name) {
+    if ( const char* extPath = getenv("CBLITE_EXTENSION_PATH") ) c4_setExtensionPath(slice(extPath));
 }
 
-
 LiteCoreTool::~LiteCoreTool() {
-    if (_shouldCloseDB && _db) {
+    if ( _shouldCloseDB && _db ) {
         C4Error err;
-        bool ok = c4db_close(_db, &err);
-        if (!ok)
-            cerr << "Warning: error closing database: " << c4error_descriptionStr(err) << "\n";
+        bool    ok = c4db_close(_db, &err);
+        if ( !ok ) cerr << "Warning: error closing database: " << c4error_descriptionStr(err) << "\n";
     }
 }
 
+bool LiteCoreTool::isDatabasePath(const string& path) { return !splitDBPath(path).second.empty(); }
 
-bool LiteCoreTool::isDatabasePath(const string &path) {
-    return ! splitDBPath(path).second.empty();
-}
-
-
-pair<string,string> LiteCoreTool::splitDBPath(const string &pathStr) {
+pair<string, string> LiteCoreTool::splitDBPath(const string& pathStr) {
     FilePath path(pathStr);
-    if (path.extension() != kC4DatabaseFilenameExtension)
-        return {"",""};
+    if ( path.extension() != kC4DatabaseFilenameExtension ) return {"", ""};
     return std::make_pair(string(path.parentDir()), path.unextendedName());
 }
 
-
-bool LiteCoreTool::isDatabaseURL(const string &str) {
+bool LiteCoreTool::isDatabaseURL(const string& str) {
     C4Address addr;
-    C4String dbName;
+    C4String  dbName;
     return c4address_fromURL(slice(str), &addr, &dbName);
 }
 
 
 #if COUCHBASE_ENTERPRISE
-static bool setHexKey(C4EncryptionKey *key, slice str) {
-    if (str.size != 2 * kC4EncryptionKeySizeAES256)
-        return false;
-    uint8_t *dst = &key->bytes[0];
-    for (size_t src = 0; src < 2 * kC4EncryptionKeySizeAES256; src += 2) {
-        if (!isxdigit(str[src]) || !isxdigit(str[src+1]))
-            return false;
-        *dst++ = (uint8_t)(16*digittoint(str[src]) + digittoint(str[src+1]));
+static bool setHexKey(C4EncryptionKey* key, slice str) {
+    if ( str.size != 2 * kC4EncryptionKeySizeAES256 ) return false;
+    uint8_t* dst = &key->bytes[0];
+    for ( size_t src = 0; src < 2 * kC4EncryptionKeySizeAES256; src += 2 ) {
+        if ( !isxdigit(str[src]) || !isxdigit(str[src + 1]) ) return false;
+        *dst++ = (uint8_t)(16 * digittoint(str[src]) + digittoint(str[src + 1]));
     }
     key->algorithm = kC4EncryptionAES256;
     return true;
 }
 
-bool LiteCoreTool::setPasswordOrKey(C4EncryptionKey *encryptionKey, slice passwordOrKey) {
+bool LiteCoreTool::setPasswordOrKey(C4EncryptionKey* encryptionKey, slice passwordOrKey) {
     return setHexKey(encryptionKey, passwordOrKey)
-        || c4key_setPassword(encryptionKey, slice(passwordOrKey), kC4EncryptionAES256);
+           || c4key_setPassword(encryptionKey, slice(passwordOrKey), kC4EncryptionAES256);
 }
 #endif
 
@@ -90,76 +76,71 @@ void LiteCoreTool::displayVersion() {
     ::exit(0);
 }
 
-
 void LiteCoreTool::processDBFlags() {
     processFlags({
-        {"--create",    [&]{_dbFlags |= kC4DB_Create; _dbFlags &= ~kC4DB_ReadOnly;}},
-        {"--writeable", [&]{_dbFlags &= ~kC4DB_ReadOnly;}},
-        {"--upgrade",   [&]{_dbFlags &= ~(kC4DB_NoUpgrade | kC4DB_ReadOnly);}},
+        {"--create",
+         [&] {
+             _dbFlags |= kC4DB_Create;
+             _dbFlags &= ~kC4DB_ReadOnly;
+         }},
+                {"--writeable", [&] { _dbFlags &= ~kC4DB_ReadOnly; }},
+                {"--upgrade", [&] { _dbFlags &= ~(kC4DB_NoUpgrade | kC4DB_ReadOnly); }},
 #if LITECORE_API_VERSION >= 300
-        {"--upgrade=vv",[&]{_dbFlags &= ~(kC4DB_NoUpgrade | kC4DB_ReadOnly);
-            _dbFlags |= kC4DB_VersionVectors;}},
+                {"--upgrade=vv",
+                 [&] {
+                     _dbFlags &= ~(kC4DB_NoUpgrade | kC4DB_ReadOnly);
+                     _dbFlags |= kC4DB_VersionVectors;
+                 }},
 #endif
-        {"--encrypted", [&]{_dbNeedsPassword = true;}},
-        {"--version",   [&]{displayVersion();}},
-        {"-v",          [&]{displayVersion();}},
+                {"--encrypted", [&] { _dbNeedsPassword = true; }}, {"--version", [&] { displayVersion(); }},
+                {"-v", [&] { displayVersion(); }},
     });
 }
 
-
-c4::ref<C4Database> LiteCoreTool::openDatabase(string pathStr,
-                                               C4DatabaseFlags dbFlags,
-                                               C4EncryptionKey const& key)
-{
+c4::ref<C4Database> LiteCoreTool::openDatabase(string pathStr, C4DatabaseFlags dbFlags, C4EncryptionKey const& key) {
     fixUpPath(pathStr);
     auto [parentDir, dbName] = splitDBPath(pathStr);
-    if (dbName.empty())
-        fail("Database filename must have a '.cblite2' extension: " + pathStr);
-    C4DatabaseConfig2 config = {slice(parentDir), dbFlags, key};
-    C4Error err;
+    if ( dbName.empty() ) fail("Database filename must have a '.cblite2' extension: " + pathStr);
+    C4DatabaseConfig2   config = {slice(parentDir), dbFlags, key};
+    C4Error             err;
     c4::ref<C4Database> db = c4db_openNamed(slice(dbName), &config, &err);
-    if (!db)
-        fail(stringprintf("Couldn't open database %s", pathStr.c_str()), err);
+    if ( !db ) fail(stringprintf("Couldn't open database %s", pathStr.c_str()), err);
     return db;
 }
-
 
 void LiteCoreTool::openDatabase(string pathStr, bool interactive) {
     assert(!_db);
     fixUpPath(pathStr);
     auto [parentDir, dbName] = splitDBPath(pathStr);
-    if (dbName.empty())
-        fail("Database filename must have a '.cblite2' extension: " + pathStr);
+    if ( dbName.empty() ) fail("Database filename must have a '.cblite2' extension: " + pathStr);
     C4DatabaseConfig2 config = {slice(parentDir), _dbFlags};
-    C4Error err;
-    if (!_dbNeedsPassword) {
+    C4Error           err;
+    if ( !_dbNeedsPassword ) {
         _db = c4db_openNamed(slice(dbName), &config, &err);
     } else {
         // If --encrypted flag given, skip opening db as unencrypted
         err = kEncryptedDBError;
     }
 
-    while (!_db && err == kEncryptedDBError) {
+    while ( !_db && err == kEncryptedDBError ) {
 #ifdef COUCHBASE_ENTERPRISE
         // Database is encrypted
-        if (!interactive && !_dbNeedsPassword) {
+        if ( !interactive && !_dbNeedsPassword ) {
             // Don't prompt for a password unless this is an interactive session
             fail("Database is encrypted (use `--encrypted` flag to get a password prompt)");
         }
         string prompt = "Password (or hex key) for database " + dbName + ":";
-        if (config.encryptionKey.algorithm != kC4EncryptionNone)
-            prompt = "Sorry, try again: ";
+        if ( config.encryptionKey.algorithm != kC4EncryptionNone ) prompt = "Sorry, try again: ";
         string password = readPassword(prompt.c_str());
-        if (password.empty())
-            exit(1);
-        if (!setPasswordOrKey(&config.encryptionKey, password)) {
+        if ( password.empty() ) exit(1);
+        if ( !setPasswordOrKey(&config.encryptionKey, password) ) {
             cout << "Error: Couldn't derive key from password\n";
             continue;
         }
         _db = c4db_openNamed(slice(dbName), &config, &err);
-        if (!_db && err == kEncryptedDBError) {
+        if ( !_db && err == kEncryptedDBError ) {
             cout << "Failed to decrypt database using current method, trying old method..." << endl;
-            if (!c4key_setPasswordSHA1(&config.encryptionKey, slice(password), kC4EncryptionAES256)) {
+            if ( !c4key_setPasswordSHA1(&config.encryptionKey, slice(password), kC4EncryptionAES256) ) {
                 cout << "Error: Couldn't derive key from password\n";
                 continue;
             }
@@ -170,14 +151,14 @@ void LiteCoreTool::openDatabase(string pathStr, bool interactive) {
         fail("Database is encrypted (Enterprise Edition is required to open encrypted databases)");
 #endif
     }
-    
-    if (!_db) {
-        if (err.domain == LiteCoreDomain && err.code == kC4ErrorCantUpgradeDatabase
-                && (_dbFlags & kC4DB_NoUpgrade)) {
+
+    if ( !_db ) {
+        if ( err.domain == LiteCoreDomain && err.code == kC4ErrorCantUpgradeDatabase && (_dbFlags & kC4DB_NoUpgrade) ) {
             fail("The database needs to be upgraded to be opened by this version of LiteCore.\n"
                  "**This will likely make it unreadable by earlier versions.**\n"
                  "To upgrade, add the `--upgrade` flag before the database path.\n"
-                 "(Detailed error message", err);
+                 "(Detailed error message",
+                 err);
         }
         fail(stringprintf("Couldn't open database %s", pathStr.c_str()), err);
     }
@@ -187,62 +168,55 @@ void LiteCoreTool::openDatabase(string pathStr, bool interactive) {
 
 #ifdef COUCHBASE_ENTERPRISE
 c4::ref<C4Cert> LiteCoreTool::readCertFile(string const& certFile) {
-    alloc_slice certData = readFile(certFile);
-    C4Error err;
+    alloc_slice     certData = readFile(certFile);
+    C4Error         err;
     c4::ref<C4Cert> cert = c4cert_fromData(certData, &err);
-    if (!cert)
-        fail("Couldn't read X.509 certificate(s) from " + certFile, err);
+    if ( !cert ) fail("Couldn't read X.509 certificate(s) from " + certFile, err);
     return cert;
 }
-
 
 c4::ref<C4KeyPair> LiteCoreTool::readKeyFile(string const& keyFile) {
     alloc_slice keyData = readFile(keyFile);
     alloc_slice keyPassword;
-    if (keyData.containsBytes("-----BEGIN ENCRYPTED "_sl)) {
+    if ( keyData.containsBytes("-----BEGIN ENCRYPTED "_sl) ) {
         string prompt = "Private key file " + keyFile + " is encrypted; what's the password? ";
-        keyPassword = alloc_slice(readPassword(prompt.c_str()));
+        keyPassword   = alloc_slice(readPassword(prompt.c_str()));
     }
-    C4Error err;
+    C4Error            err;
     c4::ref<C4KeyPair> key = c4keypair_fromPrivateKeyData(keyData, keyPassword, &err);
-    if (!key)
-        fail("Couldn't parse or decrypt private key in file " + keyFile, err);
+    if ( !key ) fail("Couldn't parse or decrypt private key in file " + keyFile, err);
     return key;
 }
 
-
-LiteCoreTool::TLSConfig LiteCoreTool::makeTLSConfig(string const& certFile,
-                                                    string const& keyFile,
-                                                    string const& clientCertFile)
-{
+LiteCoreTool::TLSConfig LiteCoreTool::makeTLSConfig(string const& certFile, string const& keyFile,
+                                                    string const& clientCertFile) {
     TLSConfig tlsConfig{};
 
-    if (certFile.find('/') == string::npos && certFile.find('\\') == string::npos) {
+    if ( certFile.find('/') == string::npos && certFile.find('\\') == string::npos ) {
         // Interpret a string with no path delimiters as a cert name to be looked up in Keychain:
         C4Error error;
         tlsConfig._certificate = c4cert_load(slice(certFile), &error);
-        if (!tlsConfig._certificate) {
-            if (error == C4Error{LiteCoreDomain, kC4ErrorNotFound}) {
+        if ( !tlsConfig._certificate ) {
+            if ( error == C4Error{LiteCoreDomain, kC4ErrorNotFound} ) {
                 fail("no certificate named '" + certFile
                      + "' found in secure store. (If this is a filename, put './' in front of it.)");
-            } else if (error != C4Error{LiteCoreDomain, kC4ErrorUnimplemented}) {
+            } else if ( error != C4Error{LiteCoreDomain, kC4ErrorUnimplemented} ) {
                 fail("failed to read '" + certFile + "' from secure certificate store", error);
             }
             // ... unless cert store is unimplemented; then treat it as a filename
         }
     }
-    if (!tlsConfig._certificate)
-        tlsConfig._certificate = readCertFile(certFile);
+    if ( !tlsConfig._certificate ) tlsConfig._certificate = readCertFile(certFile);
 
-    if (!keyFile.empty()) {
-        tlsConfig._key = readKeyFile(keyFile);
+    if ( !keyFile.empty() ) {
+        tlsConfig._key                     = readKeyFile(keyFile);
         tlsConfig.privateKeyRepresentation = kC4PrivateKeyFromKey;
     } else {
         tlsConfig.privateKeyRepresentation = kC4PrivateKeyFromCert;
     }
 
-    if (!clientCertFile.empty()) {
-        tlsConfig._rootClientCerts = readCertFile(clientCertFile);
+    if ( !clientCertFile.empty() ) {
+        tlsConfig._rootClientCerts   = readCertFile(clientCertFile);
         tlsConfig.requireClientCerts = true;
     }
 
@@ -251,24 +225,22 @@ LiteCoreTool::TLSConfig LiteCoreTool::makeTLSConfig(string const& certFile,
     tlsConfig.rootClientCerts = tlsConfig._rootClientCerts;
     return tlsConfig;
 }
-#endif // COUCHBASE_ENTERPRISE
+#endif  // COUCHBASE_ENTERPRISE
 
 
 void LiteCoreTool::logError(string_view what, C4Error err) {
     std::cerr << "Error";
-    if (!islower(what[0]))
-        std::cerr << ":";
+    if ( !islower(what[0]) ) std::cerr << ":";
     std::cerr << " " << what;
-    if (err.code) {
+    if ( err.code ) {
         fleece::alloc_slice message = c4error_getDescription(err);
         std::cerr << ": " << to_string(message);
     }
     std::cerr << "\n";
 }
 
-void LiteCoreTool::errorOccurred(const std::string &what, C4Error err) {
+void LiteCoreTool::errorOccurred(const std::string& what, C4Error err) {
     logError(what, err);
     ++_errorCount;
-    if (_failOnError)
-        fail();
+    if ( _failOnError ) fail();
 }
--- tool_support/Tool.cc
+++ tool_support/Tool.cc
@@ -26,18 +26,18 @@
 #include <regex>
 
 #if !defined(_MSC_VER)
-#include <unistd.h>
-#include <sys/ioctl.h>
+#    include <unistd.h>
+#    include <sys/ioctl.h>
 #else
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#endif
-#include <windows.h>
-#include <io.h>
-#include <conio.h>
-#define isatty _isatty
-#define STDIN_FILENO _fileno(stdin)
-#define STDOUT_FILENO _fileno(stdout)
+#    ifndef WIN32_LEAN_AND_MEAN
+#        define WIN32_LEAN_AND_MEAN
+#    endif
+#    include <windows.h>
+#    include <io.h>
+#    include <conio.h>
+#    define isatty        _isatty
+#    define STDIN_FILENO  _fileno(stdin)
+#    define STDOUT_FILENO _fileno(stdout)
 #endif
 
 using namespace std;
@@ -46,123 +46,107 @@ using namespace litecore;
 
 static constexpr int kDefaultLineWidth = 100;
 
-static constexpr const char *kHistoryFilePath = "~/.cblite_history";
+static constexpr const char* kHistoryFilePath = "~/.cblite_history";
 
 
 Tool* Tool::instance;
 
-Tool::Tool(const char* name)
-:_name(name)
-{
-    if(!instance) {
-        instance = this;
-    }
+Tool::Tool(const char* name) : _name(name) {
+    if ( !instance ) { instance = this; }
 }
 
 Tool::~Tool() {
-    if (this == instance) {
-        instance = nullptr;
-    }
+    if ( this == instance ) { instance = nullptr; }
 }
 
 
 #ifdef _MSC_VER
-    typedef LONG NTSTATUS, *PNTSTATUS;
-    #define STATUS_SUCCESS (0x00000000)
+typedef LONG NTSTATUS, *PNTSTATUS;
+#    define STATUS_SUCCESS (0x00000000)
 
-    typedef NTSTATUS (WINAPI* RtlGetVersionPtr)(PRTL_OSVERSIONINFOW);
+typedef NTSTATUS(WINAPI* RtlGetVersionPtr)(PRTL_OSVERSIONINFOW);
 
-    RTL_OSVERSIONINFOW GetRealOSVersion() {
-        HMODULE hMod = ::GetModuleHandleW(L"ntdll.dll");
-        if (hMod) {
-            RtlGetVersionPtr fxPtr = (RtlGetVersionPtr)::GetProcAddress(hMod, "RtlGetVersion");
-            if (fxPtr != nullptr) {
-                RTL_OSVERSIONINFOW rovi = { 0 };
-                rovi.dwOSVersionInfoSize = sizeof(rovi);
-                if ( STATUS_SUCCESS == fxPtr(&rovi) ) {
-                    return rovi;
-                }
-            }
+RTL_OSVERSIONINFOW GetRealOSVersion() {
+    HMODULE hMod = ::GetModuleHandleW(L"ntdll.dll");
+    if ( hMod ) {
+        RtlGetVersionPtr fxPtr = (RtlGetVersionPtr)::GetProcAddress(hMod, "RtlGetVersion");
+        if ( fxPtr != nullptr ) {
+            RTL_OSVERSIONINFOW rovi  = {0};
+            rovi.dwOSVersionInfoSize = sizeof(rovi);
+            if ( STATUS_SUCCESS == fxPtr(&rovi) ) { return rovi; }
         }
-        RTL_OSVERSIONINFOW rovi = { 0 };
-        return rovi;
     }
+    RTL_OSVERSIONINFOW rovi = {0};
+    return rovi;
+}
 #endif
 
 
 static bool sOutputIsColor = false;
 
 void Tool::enableColor() {
-    if (getenv("CLICOLOR_FORCE")) {
+    if ( getenv("CLICOLOR_FORCE") ) {
         sOutputIsColor = true;
         return;
     }
-    
-    const char *term = getenv("TERM");
-    if(isatty(STDOUT_FILENO)
-            && term != nullptr
-            && (strstr(term,"ANSI") || strstr(term,"ansi") || strstr(term,"color"))) {
+
+    const char* term = getenv("TERM");
+    if ( isatty(STDOUT_FILENO) && term != nullptr
+         && (strstr(term, "ANSI") || strstr(term, "ansi") || strstr(term, "color")) ) {
         sOutputIsColor = true;
         return;
     }
 
 #ifdef _MSC_VER
-    #ifndef ENABLE_VIRTUAL_TERMINAL_PROCESSING
-    // Sick of this being missing for whatever reason
-    #define ENABLE_VIRTUAL_TERMINAL_PROCESSING  0x0004
-    #endif
+#    ifndef ENABLE_VIRTUAL_TERMINAL_PROCESSING
+// Sick of this being missing for whatever reason
+#        define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
+#    endif
 
     sOutputIsColor = GetRealOSVersion().dwMajorVersion >= 10;
-    if(sOutputIsColor) {
+    if ( sOutputIsColor ) {
         HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-        DWORD consoleMode;
-        if(GetConsoleMode(hConsole, &consoleMode)) {
+        DWORD  consoleMode;
+        if ( GetConsoleMode(hConsole, &consoleMode) ) {
             SetConsoleMode(hConsole, consoleMode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
         }
     }
 #endif
-
 }
 
-string Tool::ansi(const char *command) {
-    if (sOutputIsColor)
-        return stringprintf("\033[%sm", command);
+string Tool::ansi(const char* command) {
+    if ( sOutputIsColor ) return stringprintf("\033[%sm", command);
     else
         return "";
 }
 
-
 int Tool::terminalWidth() {
 #if __APPLE__
     struct ttysize ts;
-    if (ioctl(0, TIOCGSIZE, &ts) == 0 && ts.ts_cols > 0)
-        return ts.ts_cols;
+    if ( ioctl(0, TIOCGSIZE, &ts) == 0 && ts.ts_cols > 0 ) return ts.ts_cols;
 #endif
     return kDefaultLineWidth;
 }
 
+static Tool* sLineReader = nullptr;  // The instance that's currently calling linenoise()
 
-static Tool* sLineReader = nullptr;     // The instance that's currently calling linenoise()
-
-
-bool Tool::readLine(const char *cPrompt) {
+bool Tool::readLine(const char* cPrompt) {
     initReadLine();
 
     string prompt = cPrompt;
-    if (sOutputIsColor)
-        prompt = ansiBold() + prompt + ansiReset();
+    if ( sOutputIsColor ) prompt = ansiBold() + prompt + ansiReset();
 
-    while (true) {
+    while ( true ) {
         sLineReader = this;
-        char* line = linenoise(prompt.c_str());
+        char* line  = linenoise(prompt.c_str());
         sLineReader = nullptr;
-        if (line == nullptr) {
+        if ( line == nullptr ) {
             // EOF (Ctrl-D)
             return false;
-        } else if (*line == 0) {
+        } else if ( *line == 0 ) {
             // No command was entered, so go round again:
             cout << "Please type a command, or Ctrl-D to exit.\n";
-        } else if (*line) {
+        } else if ( *line ) {
             // Got a command!
             // Add line to history so user can recall it later:
             linenoiseHistoryAdd(line);
@@ -176,35 +160,30 @@ bool Tool::readLine(const char *cPrompt) {
     }
 }
 
-
 // Initialize linenoise library:
 void Tool::initReadLine() {
     static bool sLineNoiseInitialized = false;
-    if (sLineNoiseInitialized)
-        return;
+    if ( sLineNoiseInitialized ) return;
 
 #ifdef __APPLE__
     // Prevent linenoise from trying to use ANSI escapes in the Xcode console on macOS,
     // which is a TTY but does not set $TERM. For some reason linenoise thinks a missing $TERM
     // indicates an ANSI-compatible terminal (isUnsupportedTerm() in linenoise.c.)
     // So if $TERM is not set, set it to "dumb", which linenoise does understand.
-    if (isatty(STDIN_FILENO) && getenv("TERM") == nullptr)
-        setenv("TERM", "dumb", false);
+    if ( isatty(STDIN_FILENO) && getenv("TERM") == nullptr ) setenv("TERM", "dumb", false);
 #endif
 
     // Enable UTF-8:
-    linenoiseSetEncodingFunctions(linenoiseUtf8PrevCharLen, linenoiseUtf8NextCharLen,
-                                  linenoiseUtf8ReadCode);
+    linenoiseSetEncodingFunctions(linenoiseUtf8PrevCharLen, linenoiseUtf8NextCharLen, linenoiseUtf8ReadCode);
 
     // Install a command-completion callback that dispatches to the current Tool instance:
-    linenoiseSetCompletionCallback([](const char *line, linenoiseCompletions *lc) {
-        if (sLineReader) {
+    linenoiseSetCompletionCallback([](const char* line, linenoiseCompletions* lc) {
+        if ( sLineReader ) {
             ArgumentTokenizer tokenizer;
             tokenizer.reset(line);
-            if (tokenizer.hasArgument()) {
-                sLineReader->addLineCompletions(tokenizer, [&](const string &completion) {
-                    linenoiseAddCompletion(lc, completion.c_str());
-                });
+            if ( tokenizer.hasArgument() ) {
+                sLineReader->addLineCompletions(
+                        tokenizer, [&](const string& completion) { linenoiseAddCompletion(lc, completion.c_str()); });
             }
         }
     });
@@ -218,27 +197,26 @@ void Tool::initReadLine() {
     sLineNoiseInitialized = true;
 }
 
-
-string Tool::readPassword(const char *prompt) {
+string Tool::readPassword(const char* prompt) {
 #if defined(_MSC_VER)
     cout << prompt;
-    string pass;
-    const char BACKSPACE = 8;
+    string     pass;
+    const char BACKSPACE       = 8;
     const char CARRIAGE_RETURN = 13;
-    const char CTRL_C = 3;
-    int next;
-    while((next = _getch()) != CARRIAGE_RETURN) {
-        if(next == CTRL_C) {
+    const char CTRL_C          = 3;
+    int        next;
+    while ( (next = _getch()) != CARRIAGE_RETURN ) {
+        if ( next == CTRL_C ) {
             pass.clear();
             break;
         }
 
-        if(next == BACKSPACE) {
+        if ( next == BACKSPACE ) {
             pass.resize(pass.length() - 1);
             continue;
         }
 
-        if(next < ' ') {
+        if ( next < ' ' ) {
             // Disregard other non-printables
             continue;
         }
@@ -249,46 +227,41 @@ string Tool::readPassword(const char *prompt) {
     cout << endl;
     return pass;
 #else
-    char *cpass = getpass(prompt);
+    char*  cpass    = getpass(prompt);
     string password = cpass;
-    memset(cpass, 0, strlen(cpass) + 1); // overwrite password at known static location
+    memset(cpass, 0, strlen(cpass) + 1);  // overwrite password at known static location
     return password;
 #endif
 }
 
-
-alloc_slice Tool::readFile(const string &path, bool mustExist) {
+alloc_slice Tool::readFile(const string& path, bool mustExist) {
     FILE* file = fopen(path.c_str(), "rb");
-    if (file) {
-        if (fseek(file, 0, SEEK_END) == 0) {
+    if ( file ) {
+        if ( fseek(file, 0, SEEK_END) == 0 ) {
             auto size = ftell(file);
             fseek(file, 0, SEEK_SET);
             alloc_slice data(size);
-            if (fread((char*)data.buf, 1, size, file) == size) {
+            if ( fread((char*)data.buf, 1, size, file) == size ) {
                 fclose(file);
                 return data;
             }
         }
     }
     int err = errno;
-    if (file)
-        fclose(file);
-    if (!mustExist && err == ENOENT)
-        return nullslice;
+    if ( file ) fclose(file);
+    if ( !mustExist && err == ENOENT ) return nullslice;
     fail("Couldn't read file " + path + ": " + strerror(err));
 }
 
-
-void Tool::writeFile(slice data, const string& path, const char *mode) {
+void Tool::writeFile(slice data, const string& path, const char* mode) {
     FILE* file = fopen(path.c_str(), mode);
-    if (file) {
-        if (fwrite(data.buf, 1, data.size, file) == data.size) {
+    if ( file ) {
+        if ( fwrite(data.buf, 1, data.size, file) == data.size ) {
             fclose(file);
             return;
         }
     }
     int err = errno;
-    if (file)
-        fclose(file);
+    if ( file ) fclose(file);
     fail("Couldn't write file " + path + ": " + strerror(err));
 }
