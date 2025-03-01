d29950acbe127bc4773e226b99beaeff8ea8dd4b
Jens Alfke <jens@couchbase.com>
2025-02-28 17:32:32 -0800

    Copyright notices

--- Networking/P2P/PeerDiscovery+AppleBT.hh
+++ Networking/P2P/PeerDiscovery+AppleBT.hh
@@ -1,5 +1,13 @@
 //
-// Created by Jens Alfke on 2/24/25.
+// PeerDiscovery+AppleBT.hh
+//
+// Copyright 2025-Present Couchbase, Inc.
+//
+// Use of this software is governed by the Business Source License included
+// in the file licenses/BSL-Couchbase.txt.  As of the Change Date specified
+// in that file, in accordance with the Business Source License, use of this
+// software will be governed by the Apache License, Version 2.0, included in
+// the file licenses/APL2.txt.
 //
 
 #pragma once
@@ -11,8 +19,6 @@
 namespace litecore::p2p {
 
     void InitializeBluetoothProvider(string_view serviceType);
-
-    // NOTE: You can call `peer->getPlatformPeer("CBPeripheral")` to get an Obj-C `CBPeripheral*`.
 
 }  // namespace litecore::p2p
 
