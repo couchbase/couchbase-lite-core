c6f514660d1fb186961ab35a3402c7575f9ceb41
Jens Alfke <jens@couchbase.com>
2025-04-14 11:50:07 -0700

    SecureRandomize: Added RandomNumberGenerator class

--- Crypto/SecureRandomize.cc
+++ Crypto/SecureRandomize.cc
@@ -34,6 +34,44 @@ namespace litecore {
         return uniform(e);
     }
 
+#pragma mark - RANDOM NUMBER GENERATOR:
+
+    RandomNumberGenerator& RandomNumberGenerator::defaultInstance() {
+        static RandomNumberGenerator* sRNG = new RandomNumberGenerator;
+        return *sRNG;
+    }
+
+    uint32_t RandomNumberGenerator::randomNumber(uint32_t upperBound) {
+        std::uniform_int_distribution<uint32_t> uniform(0, upperBound - 1);
+        return uniform(*this);
+    }
+
+    double RandomNumberGenerator::randomDouble(double lowerBound, double upperBound) {
+        std::uniform_real_distribution<double> uniform(lowerBound, upperBound);
+        return uniform(*this);
+    }
+
+    double RandomNumberGenerator::randomNormalDouble(double mean, double stdDev) {
+        std::normal_distribution norm(mean, stdDev);
+        return norm(*this);
+    }
+
+    class RepeatableRandomNumberGenerator : public RandomNumberGenerator {
+      public:
+        explicit RepeatableRandomNumberGenerator(uint32_t seed) : _rng(seed) {}
+
+        result_type operator()() override { return _rng(); };
+
+      private:
+        std::mt19937 _rng;
+    };
+
+    std::unique_ptr<RandomNumberGenerator> RandomNumberGenerator::newRepeatable(uint32_t seed) {
+        return std::make_unique<RepeatableRandomNumberGenerator>(seed);
+    }
+
+#pragma mark - SECURE RANDOMIZE:
+
     void SecureRandomize(fleece::mutable_slice s) {
 #ifdef __APPLE__
         // iOS and Mac OS implementation based on system-level CommonCrypto library:
--- Crypto/SecureRandomize.hh
+++ Crypto/SecureRandomize.hh
@@ -26,4 +26,46 @@ namespace litecore {
     /** Returns a random integer in the range [0 .. upperBound-1].
         @warning  On some platforms this RNG is cryptographically secure, on others less so. */
     uint32_t RandomNumber(uint32_t upperBound);
+
+    /** Random number generator class that can be used to abstract the source/type of randomness.
+        Default implementation uses `RandomNumber()`. Can be subclassed to use other generators. */
+    class RandomNumberGenerator {
+      public:
+        // This class conforms to the concept `std::uniform_random_bit_generator`.
+        using result_type = uint32_t;
+
+        static constexpr result_type min() { return 0; }
+
+        static constexpr result_type max() { return UINT_MAX; }
+
+        virtual result_type operator()() { return RandomNumber(); };
+
+        virtual ~RandomNumberGenerator() = default;
+
+        /// The default instance, which uses `std::random_device`.
+        /// @warning  On some platforms this RNG is cryptographically secure, on others less so.
+        static RandomNumberGenerator& defaultInstance();
+
+        /// Creates a RandomNumberGenerator that generates pseudo-random numbers based on a seed.
+        /// The same seed always creates the same sequence of numbers.
+        static std::unique_ptr<RandomNumberGenerator> newRepeatable(uint32_t seed);
+
+        /// Returns a random integer in the range [0 .. UINT32_MAX].
+        uint32_t randomNumber() { return (*this)(); }
+
+        /// Returns a random integer in the range [0 .. upperBound-1].
+        uint32_t randomNumber(uint32_t upperBound);
+
+        /// Returns a random real number in the range [lowerBound .. upperBound).
+        double randomDouble(double lowerBound, double upperBound);
+
+        /// Returns a random real number with a normal/Gaussian distribution.
+        /// <https://en.wikipedia.org/wiki/Normal_distribution>
+        /// About 68% of results will be within mean ± stdDev.
+        /// About 95% will be within ± 2 times stdDev; 99.9% within ± 3 times stdDev.
+        /// @param mean  The mean (average) value to return.
+        /// @param stdDev  The standard deviation.
+        double randomNormalDouble(double mean, double stdDev);
+    };
+
 }  // namespace litecore
