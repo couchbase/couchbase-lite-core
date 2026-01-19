9607d8789d9cb63067b5fe464c0a9e85fa264fbe
Jens Alfke <jens@couchbase.com>
2026-01-19 13:22:05 -0800

    Fixed crash regression closing unopened C4Socket

--- C/c4Socket.cc
+++ C/c4Socket.cc
@@ -99,7 +99,9 @@ const C4SocketFactory C4SocketFactoryImpl::kFactory{
                     C4SliceResult allocatedData) { nativeHandle(socket)->write(alloc_slice(allocatedData)); },
         .completedReceive = [](C4Socket* socket,
                                size_t    byteCount) { nativeHandle(socket)->completedReceive(byteCount); },
-        .close            = [](C4Socket* socket) { nativeHandle(socket)->close(); },
+        .close            = [](C4Socket* socket) {
+            if (auto handle = nativeHandle(socket)) handle->close();
+        },
         .dispose          = [](C4Socket* socket) { release(nativeHandle(socket)); },
         .attached =
                 [](C4Socket* socket) {
